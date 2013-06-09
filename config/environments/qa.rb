65fd4c46fde626662c4e701764a7027f9b51816d
Amol Brid <abrid@collective.com>
2013-06-09 18:12:53 -0400

    Updated 'qa' configuration to generate digest URL for assets.

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -12,6 +12,9 @@ Reachui::Application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
   config.static_cache_control = "public, max-age=3600"
