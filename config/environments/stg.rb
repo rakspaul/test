250c8f6520f80d51831371e2a9c42f590bb74fc4
Amol Brid <abrid@collective.com>
2013-06-09 18:23:53 -0400

    Let rails in stage environment serve static contents. We can change this later when nginx is setup.

--- config/environments/stg.rb
+++ config/environments/stg.rb
@@ -20,7 +20,8 @@ Reachui::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # TODO: Change this when put behind nginx.
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
