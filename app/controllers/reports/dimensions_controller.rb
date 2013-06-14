256b4ef74110e3f905c8d256fb565ae4243d8c2e
Ankur Aute <ankurau@cybage.com>
2013-06-14 11:10:24 -0400

    Chnages for group by order and advertiser

--- app/controllers/reports/dimensions_controller.rb
+++ app/controllers/reports/dimensions_controller.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'json'
+require 'load_dimensions_wrapper'
 
 class Reports::DimensionsController < ApplicationController
   include Authenticator
@@ -10,9 +11,10 @@ class Reports::DimensionsController < ApplicationController
   	from_date = params[:from_date].present? ? params[:from_date] : ""
   	to_date = params[:to_date].present? ? params[:to_date] : ""
   	dimensions = params[:dimensions].present? ? params[:dimensions] : ""
+  	expand_id = params[:expand_id].present? ? params[:expand_id] : ""
 
   	wrapper = LoadDimensionsWrapper.new
-  	csv_results = wrapper.load(current_user.id, dimensions, from_date, to_date, current_network)
+  	csv_results = wrapper.load(current_user.id, dimensions, from_date, to_date, current_network, expand_id)
 
   	respond_with(csv_results)
   end
