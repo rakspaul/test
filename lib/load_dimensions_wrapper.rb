86716db4674214fcf9c7d66bca89e291f87cd962
Ankur Aute <ankurau@cybage.com>
2013-06-26 10:39:11 -0400

    Move the request call to new reporting server

--- lib/load_dimensions_wrapper.rb
+++ lib/load_dimensions_wrapper.rb
@@ -3,81 +3,36 @@ require 'open-uri'
 require 'csv'
 
   class LoadDimensionsWrapper
-
-    def load(user_id, dimensions, from_date, to_date, current_network, expand_id)
-      arr = dimensions
-
-      if arr.length == 1
-        list_type = arr[0]
-        detail_type = arr[0]
-        item = arr[0]
-        grp = arr[0]
-      else
-        list_type = arr[0]
-        detail_type = arr[1]
-        item = arr[1]
-        grp = arr[0]
-        parent_type = arr[0]
-        detail_id = expand_id
-        parent_id = expand_id
-      end  
+    
+    def load(group, cols, filter, start_date, end_date, current_network, current_user)
 
       params = {
-        "calltype" => "data",
-        "viewtype" => "tree",
-        "type" => "est",
-        "universe" => "network",
-        "listtype" => list_type,
-        "detailtype" => detail_type,
-        "item" => item,
-        "grp" => grp,
-        "format" => "csv",
-        "sortField" => user_id,
-        "usage" => "0",
-        "pagesize" => "400",
-        "pagestart" => "0",
-        "sortDirection" => "ascending",
-        "fromdate" => from_date,
-        "todate" => to_date,
-        "userid" => user_id,
-        "tkn" => Digest::MD5.hexdigest("#{current_network.id}:#{user_id}:#{Date.today.strftime('%Y-%m-%d')}")
-      }
+        "instance" => current_network.id,
+        "group" => group,
+        "cols" => cols,
+        "format" => "json",
+        "start_date" => start_date,
+        "end_date" => end_date,
+        "limit" => "50",
+        "user_id" => current_user.id,
+        "filters" => filter
+       }
 
-      if expand_id != ""
-        params = { "detailid" => detail_id, "parentid" => parent_id, "parenttype" => parent_type }.merge(params)
-      end  
-
-      query_string = params.map {|k,v| "#{k}=#{v}"}.join("&")
-
-      cdb_server = "http://cm.qacdb.collective-media.net/delivery/data"
+      query_string = params.map {|k,v| "#{k}=#{v}"}.join("&")      
+      cdb_server = "http://stg-cdb1.collective-media.net/export"
 
       url = "#{cdb_server}?#{query_string}"
-      csv_results = []
-      doc = Nokogiri::XML(open(url, :read_timeout => 3600))
-      data = doc.at_xpath('.//data')
+      puts "URL:-  " + url
 
-      unless data.nil?
-        CSV.parse(data.text.delete('"').strip, {:headers => :true, :quote_char => ','}) do |row|
-          dr = Result.new
-          dr.id = row['id']  
-          dr.name = row['name']
-	   dr.type = row['type']
-          dr.impressions = row['impressions']
-          dr.clicks = row['clicks']
-          dr.ctr = row['ctr']
-          dr.pccr = row['pccr']
-          dr.actions = row['actions']
-          dr.ar = row['ar']
-          dr.gross_rev = row['gross_rev']
-          dr.gross_ecpm = row['gross_ecpm']
-          dr.net_ecpc = row['net_ecpc']
-          dr.net_ecpa = row['net_ecpa']
-
-          csv_results << dr
-        end
+      response = nil
+      open(url, :read_timeout => 3600) do |file|
+        response = file.read
       end
 
-      return csv_results
+      data = ActiveSupport::JSON.decode(response)
+      response = data['records']
+
+      return response
     end
 
  end    
