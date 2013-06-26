2f0ce05ecd578bf17bc50ae6f6e8e45aa75f33d0
Amol Brid <abrid@collective.com>
2013-06-26 17:01:13 -0400

    Validate ad size assigned to lineitem within user's network.

--- app/validators/ad_size_validator.rb
+++ app/validators/ad_size_validator.rb
@@ -2,7 +2,7 @@ class AdSizeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless attribute.nil?
       assignedAdSizes = value.split(',').map(&:strip)
-      validAdSizes = AdSize.where(:size => assignedAdSizes).pluck('size')
+      validAdSizes = AdSize.of_network(record.user.network).where(:size => assignedAdSizes).pluck('size')
 
       invalidAdSizes = assignedAdSizes - validAdSizes
       record.errors.add attribute, "#{invalidAdSizes.join(',')} not found." unless invalidAdSizes.empty?
