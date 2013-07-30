c2c3bfbc80346649b61317f1d238f5a7b65fcc73
Ankur Aute <ankurau@cybage.com>
2013-07-30 02:41:14 -0400

    Changes for report service url creation

--- app/controllers/reports/schedule_reports_controller.rb
+++ app/controllers/reports/schedule_reports_controller.rb
@@ -1,5 +1,6 @@
 class Reports::ScheduleReportsController < ApplicationController
   include Authenticator
+  SUPPORTED_PARAMS = ['group', 'cols', 'start_date', 'end_date', 'filter', 'per_page', 'offset', 'sort', 'limit', 'format']
 
   respond_to :json
 
@@ -17,6 +18,19 @@ class Reports::ScheduleReportsController < ApplicationController
 
   def create
   	@report = ReportSchedule.new(params[:report_schedule])
+
+    params.clone.keep_if {|key, value| SUPPORTED_PARAMS.include? key}
+    params.merge!({
+      "instance" => @current_user.network.id,
+      "user_id" => @current_user.id,
+      "tkn" => build_request_token
+     })
+
+    query_string = params.map {|k,v| "#{k}=#{v}"}.join("&")
+    report_server = Rails.application.config.report_service_uri
+    url = "#{report_server}?#{query_string}"
+
+    @report.url = url
   	
 	  if !@report.save
       render json: { errors: @report.errors }, status: :unprocessable_entity
@@ -44,6 +58,10 @@ class Reports::ScheduleReportsController < ApplicationController
   def destroy
   	@report = ReportSchedule.find(params[:id])
   	@report.destroy
+  end
+
+  def build_request_token
+    Digest::MD5.hexdigest("#{@current_user.network.id}:#{@current_user.id}:#{Date.today.strftime('%Y-%m-%d')}")
   end	  
 
 end  
