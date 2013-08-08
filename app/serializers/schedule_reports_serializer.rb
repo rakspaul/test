59d458938102dca96aa73556e94670060a847226
Ankur Aute <ankurau@cybage.com>
2013-08-08 09:50:07 -0400

    Added required params into serializer

--- app/serializers/schedule_reports_serializer.rb
+++ app/serializers/schedule_reports_serializer.rb
@@ -1,6 +1,8 @@
 class ScheduleReportsSerializer < ActiveModel::Serializer
 
-  attributes :id, :title, :email, :recalculate_dates, :start_date, :end_date, :frequency_value, :frequency_type, :report_start_date, :report_end_date, :status
+  attributes :id, :title, :email, :recalculate_dates, :start_date, :end_date,
+     :url, :last_ran, :frequency_value, :frequency_type, :report_start_date,
+     :report_end_date, :status, :created_at, :updated_at
 
   def start_date
     object.start_date.strftime("%Y-%m-%d")
@@ -16,6 +18,14 @@ class ScheduleReportsSerializer < ActiveModel::Serializer
 
   def report_end_date
     object.report_end_date.nil? ? "" : object.report_end_date.strftime("%Y-%m-%d")
+  end
+
+  def created_at
+    object.created_at.strftime("%Y-%m-%d")
+  end
+
+  def updated_at
+    object.updated_at.strftime("%Y-%m-%d")
   end
 
 end
