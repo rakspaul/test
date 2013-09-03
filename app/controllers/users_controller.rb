2f3cad5e986b741d93fdb29431ad75808d79c958
Amol Brid <abrid@collective.com>
2013-09-03 12:42:04 -0400

    Fix search for sales person and account manager.
    1. Replacing 'concat' function with concatenation operator (||). qa/stage/prod postgres db version is 8.4 which does not support 'concat' function to concatenate string. 
    2. Updated io fixture file with correct email of Eric Burns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       User.where("email ilike ?", "#{params[:search]}%").limit(8)
     elsif params[:search_by] == 'name'
       relation = params[:sales].present? ? User.sales_people : User
-      relation.where("concat(first_name,' ',last_name) ilike ?", "#{params[:search]}%").limit(8)
+      relation.where("(first_name || ' ' || last_name) ilike ?", "#{params[:search]}%").limit(8)
     end
 
     @users.map!{|u| {id: u.id, email: u.email, phone: u.phone_number, name: "#{u.first_name} #{u.last_name}"} }
