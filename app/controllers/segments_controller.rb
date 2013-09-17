5176d6ac4863a84d27e58cf0a7f0cdcc2521b612
Amol Brid <abrid@collective.com>
2013-09-17 12:32:10 -0400

    Case insensitive segments search and refactoring

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,15 +1,15 @@
 class SegmentsController < ApplicationController
   include Authenticator
+
   respond_to :json
 
   def search
     search_query = params[:search]
     networks = Rails.application.config.search_segments_in_network.split(',')
 
-    if search_query.blank?
-      @segments = Segment.of_networks(networks).limit(500).order("name  asc")
-    else
-      @segments = Segment.of_networks(networks).where("name like ? or friendly_name like ?", "%#{search_query}%", "%#{search_query}%").limit(500).order("name  asc")
+    @segments = Segment.of_networks(networks).limit(500).order("name  asc")
+    unless search_query.blank?
+      @segments = @segments.where("lower(name) ilike lower(?) or lower(friendly_name) ilike lower(?)", "%#{search_query}%", "%#{search_query}%")
     end
 
     respond_with(@segments)
