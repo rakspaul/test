1400a4a7198fc667f32f9edd44ece6946a8bcf1b
Dmitrii Samoilov <e2718281828@ya.ru>
2013-10-07 13:38:42 +0300

    refs #78, Missing Search Capability on Key Values

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -7,9 +7,9 @@ class Ad < ActiveRecord::Base
   has_many :ad_assignments
   has_many :creatives, through: :ad_assignments
 
-  has_and_belongs_to_many :zipcodes, join_table: :zipcode_targeting, dependent: :destroy
-  has_and_belongs_to_many :designated_market_areas, join_table: :dma_targeting, association_foreign_key: :dma_id, dependent: :destroy
-  has_and_belongs_to_many :audience_groups, join_table: :ads_reach_audience_groups, association_foreign_key: :reach_audience_group_id, dependent: :destroy
+  has_and_belongs_to_many :zipcodes, join_table: :zipcode_targeting
+  has_and_belongs_to_many :designated_market_areas, join_table: :dma_targeting, association_foreign_key: :dma_id
+  has_and_belongs_to_many :audience_groups, join_table: :ads_reach_audience_groups, association_foreign_key: :reach_audience_group_id
 
   validates :description, uniqueness: { message: "The following Ads have duplicate names. Please ensure the Ad names are unique", scope: :order_id }
 
