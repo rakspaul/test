79b590de0eefc520961b1ca2e1f61f37503e5dd3
Ankur Aute <ankurau@cybage.com>
2013-10-17 06:52:01 -0400

    fixes #67, Changes to removed additional Contexts loaded into search box

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -7,7 +7,8 @@ class ContextsController < ApplicationController
     search_query = params[:search]
     networks = Rails.application.config.search_contexts_in_network.split(',')
 
-    @contexts = Context.of_networks(networks).where(:inactive => false).limit(500).order("name  asc")
+    @contexts = Context.with_counts.of_networks(networks).where(:inactive => false).where.not(:type => ['Pixel', 'InvalidContext']).limit(500).order("name  asc")
+
     unless search_query.blank?
       @contexts = @contexts.where("lower(name) ilike lower(?) or lower(friendly_name) ilike lower(?)", "%#{search_query}%", "%#{search_query}%")
     end
--- app/models/context.rb
+++ app/models/context.rb
@@ -1,5 +1,11 @@
 class Context < ActiveRecord::Base
   belongs_to :network
+  has_many :segments, :foreign_key => 'parent_id'
+
+  scope :with_counts, {:joins => "LEFT JOIN segments ON (segments.parent_id = contexts.id)",
+                       :select => "contexts.*, count(DISTINCT segments.id) as segment_count",
+                       :group => column_names.map {|c| "contexts.#{c}"}.join(', ')
+                      }
 
   def self.of_networks(networks)
     where(:network_id => networks)
