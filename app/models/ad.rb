4130bee06dbb01d0423df1648e38c2eb1acebef7
Dmitrii Samoilov <e2718281828@ya.ru>
2013-10-28 18:02:57 +0200

    refs #190, If you update order start date and LI dates, Ad date is not caught as predating the order

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -78,11 +78,11 @@ class Ad < ActiveRecord::Base
 
   def check_flight_dates_within_li_flight_dates
     if self.lineitem
-      if self.start_date < self.lineitem.start_date
+      if self.start_date.to_date < self.lineitem.start_date.to_date
         self.errors.add(:start_date, "couldn't be before lineitem's start date")
       end
 
-      if self.end_date > self.lineitem.end_date
+      if self.end_date.to_date > self.lineitem.end_date.to_date
         self.errors.add(:start_date, "couldn't be after lineitem's end date")
       end
     end
