40fac4affd3529182b258c91ce1b41020fc703c8
Dmitrii Samoilov <e2718281828@ya.ru>
2013-10-28 16:07:24 +0200

    refs #190, If you update order start date and LI dates, Ad date is not caught as predating the order

--- spec/factories/networks.rb
+++ spec/factories/networks.rb
@@ -1,8 +1,12 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
+  factory :data_source do
+    name "Doubleclick"
+    ident "dart"
+  end
+
   factory :network do
     id 81
     name "Test network"
+    data_source
   end
 end
