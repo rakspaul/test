02189199adf9fd4936b065a7633cc16e9dfa2fae
Vladimir SHulga <vshulga@cogniance.com>
2013-10-29 19:27:38 +0200

    fixes #140, fix frontend bug in ad block height update

--- app/assets/javascripts/utils/core.js
+++ app/assets/javascripts/utils/core.js
@@ -90,7 +90,7 @@ ReachUI.showCondensedTargetingOptions = function() {
 
 // align height of ad's subdivs with the largest one ('.name')
 ReachUI.alignAdsDivs = function() {
-  var highest_div = _.max(_.map($('.ad > div[class^="pure-u-"]'), function(el) { return $(el).outerHeight() } ));
+  var highest_div = _.max(_.map($('.ad > div[class^="pure-u-"]'), function(el) { return $(el).height() } ));
   _.each($('.ad > div[class^="pure-u-"]'), function(el) {
     var padding = $(el).css('box-sizing') == 'border-box' ? parseInt($(el).css('padding-top')) : 0;
     $(el).css('height', (highest_div + padding + 'px') ) 
