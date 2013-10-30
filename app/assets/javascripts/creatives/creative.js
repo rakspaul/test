fb857b06c104961fd6c35891df53605d658820ed
Dmitrii Samoilov <e2718281828@ya.ru>
2013-10-30 14:35:41 +0200

    refs #207, Creatives Error Bug

--- app/assets/javascripts/creatives/creative.js
+++ app/assets/javascripts/creatives/creative.js
@@ -57,7 +57,8 @@
 
     updateLiCreative: function() {
       var view = this,
-          update_creative_model_from_li = true;
+          update_creative_model_from_li = true,
+          current_creative = view.model;
 
       // only update Creative on LI level if in another Ads there is no such Creative
       var this_ad = this.options.parent_view.model;
@@ -73,48 +74,25 @@
               }
             });
 
-            // collect all creative attributes from LI's Ads except current one
-            var creatives_urls = [], start_dates = [], end_dates = [], ad_sizes = [];
-            _.each(ads_except_current, function(ad) { 
-              _.each(ad.get('creatives').models, function(c) {
-                creatives_urls.push(c.get('redirect_url'));
-                start_dates.push(c.get('start_date'));
-                end_dates.push(c.get('end_date'));
-                ad_sizes.push(c.get('ad_size'));
-              });
+            var clone_creative_to_li = true;
+            _.each(this_li.get('creatives').models, function(c) {
+              if(c.get('id') && current_creative.get('id') && c.get('id') == current_creative.get('id')) {
+                // creatives are persisted and one of them was changed
+                // then copy new attributes to LI level creative
+                c.attributes = current_creative.attributes;
+                this_li_view.renderCreatives();
+                clone_creative_to_li = false;               
+              } else if (c.cid == current_creative.get('parent_cid')) { // not persisted creative
+                c.attributes = current_creative.attributes;
+                this_li_view.renderCreatives();
+                clone_creative_to_li = false;
+              }
             });
 
-            // if all attributes are already present among existing ones creatives then do nothing
-            if(_.contains(creatives_urls, view.model.get('redirect_url')) && _.contains(start_dates, view.model.get('start_date')) && _.contains(end_dates, view.model.get('end_date')) && _.contains(ad_sizes, view.model.get('ad_size'))) {
-              update_creative_model_from_li = false;
-            }
-
-            if(update_creative_model_from_li) {
-              var clone_creative_to_li = false;
-              _.each(this_li.get('creatives').models, function(c) {
-
-                if(c.get('id') && view.model.get('id')) {
-                  if(c.get('id') == view.model.get('id')) {
-                    c.attributes = view.model.attributes;
-                    this_li_view.renderCreatives();
-                    return;
-                  } else {
-                    clone_creative_to_li = true;
-                  }
-                } else if (c.cid == view.model.get('parent_cid')) { // not persisted creative
-                  c.attributes = view.model.attributes;
-                  this_li_view.renderCreatives();
-                  return;
-                } else {
-                  clone_creative_to_li = true;
-                }
-              });
-
-              // create new one with new attributes (copy-on-write)
-              if(clone_creative_to_li) {
-                view.options.parent_view.options.parent_view.model.get('creatives').add(view.model);
-                this_li_view.renderCreatives();
-              }
+            // create new one with new attributes (copy-on-write)
+            if(clone_creative_to_li) {
+              view.options.parent_view.options.parent_view.model.get('creatives').add(current_creative);
+              this_li_view.renderCreatives();
             }
           }
         }
@@ -238,6 +216,8 @@
         var this_li = this.options.parent_view.model;  
         this._markCreativeForDeletion(this_li);
       }
+
+      this.updateLiCreative();
     }
   });
 
