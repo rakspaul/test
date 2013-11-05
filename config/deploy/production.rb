83cd506e805a242cca5d1ba93884c8438312bca6
Amol Brid <amolbrid@gmail.com>
2013-11-05 00:12:42 -0500

    reverting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,11 +5,9 @@ set :rails_env, "production" # set for capistrano asset pipeline precompilation
 set :hipchat_env, "Production"
 
 set :app1, "ampapp1.collective-media.net"
-set :db1, "ampdb1.collective-media.net"
 
 role :web, app1
 role :app, app1
-role :db,  db1, :primary => true
 
 set :default_environment, 'JAVA_HOME' => "/usr/lib/jvm/java-6-sun-1.6.0.26"
 #set :default_environment, 'JAVA_HOME' => '`readlink -f /usr/bin/java | sed "s:bin/java::"`'
