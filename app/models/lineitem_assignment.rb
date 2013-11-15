08a73426c05162be17e020f700f996eb8e7275b0
Dmitrii Samoilov <e2718281828@ya.ru>
2013-11-15 17:03:25 +0200

    refs #256, Error pushing should have been caught by UI

--- app/models/lineitem_assignment.rb
+++ app/models/lineitem_assignment.rb
@@ -3,6 +3,7 @@ class LineitemAssignment < ActiveRecord::Base
   belongs_to :creative
 
   before_validation :check_flight_dates_within_li_flight_dates
+  before_validation :check_end_date_after_start_date
 
 private
 
@@ -13,6 +14,12 @@ private
 
     if self.end_date.to_date > self.lineitem.end_date.to_date
       self.errors.add(:end_date, "couldn't be after lineitem's end date")
+    end
+  end
+
+  def check_end_date_after_start_date
+    if self.end_date.to_date < self.start_date.to_date
+      self.errors.add(:end_date, "couldn't be before start date")
     end
   end
 end
