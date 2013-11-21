40de38583b5e86e43a5b293c4cb1df17bf7ecf45
Vladimir SHulga <vshulga@cogniance.com>
2013-11-21 19:10:47 +0200

    fixes #256, fix bug in errors

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -589,11 +589,11 @@ private
       end
 
       if creative[:end_date].to_date < parent.start_date.to_date
-        creative_errors[:start_date] = "couldn't be before #{type}'s start date"
+        creative_errors[:end_date] = "couldn't be before #{type}'s start date"
       end
 
       if creative[:start_date].to_date > parent.end_date.to_date
-        creative_errors[:end_date] = "couldn't be after #{type}'s end date"
+        creative_errors[:start_date] = "couldn't be after #{type}'s end date"
       end
 
       if creative[:end_date].to_date < creative[:start_date].to_date
