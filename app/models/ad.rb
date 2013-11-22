8561acd1c082079d506f7d63e281ceff69cb74f1
Vladimir SHulga <vshulga@cogniance.com>
2013-11-22 13:28:02 +0200

    fixes #256, fix bug in end dates validation

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -92,7 +92,7 @@ class Ad < ActiveRecord::Base
       end
 
       if self.end_date.to_date > self.lineitem.end_date.to_date
-        self.errors.add(:start_date, "couldn't be after lineitem's end date")
+        self.errors.add(:end_date, "couldn't be after lineitem's end date")
       end
     end
   end
