b9940f573d61a0c86a2984b4670cda1377a7688e
Ankur Aute <ankurau@cybage.com>
2013-11-28 23:38:40 -0500

    Added test case to only load ADVERTISER type of Advertiser.

--- spec/models/advertiser_spec.rb
+++ spec/models/advertiser_spec.rb
@@ -5,8 +5,13 @@ describe Advertiser do
   let(:collective_network) { Network.create name: 'Collective', :data_source => data_source }
 
   before do
-    advertiser_type = AdvertiserType.create name: 'ADVERTISER', network: collective_network
-    @adv_one = Advertiser.create network_id: collective_network.id, name: "adv1", source_id: '12345', advertiser_type: advertiser_type
+    advertiser_type1 = AdvertiserType.create name: 'ADVERTISER', network: collective_network
+    advertiser_type2 = AdvertiserType.create name: 'HOUSEADVERTISER', network: collective_network
+    advertiser_type3 = AdvertiserType.create name: 'AGENCY', network: collective_network
+
+    @adv_one = Advertiser.create network_id: collective_network.id, name: "adv1", source_id: '12345', advertiser_type: advertiser_type1
+    adv_two = Advertiser.create network_id: collective_network.id, name: "adv2", source_id: '23456', advertiser_type: advertiser_type2
+    adv_three = Advertiser.create network_id: collective_network.id, name: "adv3", source_id: '34567', advertiser_type: advertiser_type3
   end
 
   it { should belong_to(:advertiser_type) }
@@ -15,6 +20,12 @@ describe Advertiser do
     adv = Advertiser.find_by_name("adv1")
     adv.advertiser_type.name.should == 'ADVERTISER'
     adv.should eq @adv_one
+  end
+
+  it 'return only ADVERTISER type of Advertisers' do
+    adv = Advertiser.all
+    adv.size == 1
+    adv.first.advertiser_type.name == 'ADVERTISER'
   end
 
 end
