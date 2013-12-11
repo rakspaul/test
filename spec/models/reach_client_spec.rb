441cf3444adc594250a5d18f26080b300e69e4f0
Kashinath <kslagdive@gmail.com>
2013-12-11 07:33:59 -0500

    fixes #293, test cases added for uniqueness of name and abbr.

--- spec/models/reach_client_spec.rb
+++ spec/models/reach_client_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 describe ReachClient do
+
   it { should belong_to(:media_contact) }
   it { should belong_to(:billing_contact) }
   it { should belong_to(:account_manager) }
   it { should belong_to(:sales_person) }
 
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:abbr) }
+  context "Validations" do
+    before do
+      reach_client = FactoryGirl.create(:reach_client)
+    end
 
-  it { should validate_presence_of(:network_id) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:abbr) }
+    it { should validate_presence_of(:network_id) }
+    it { should validate_uniqueness_of(:name).scoped_to(:network_id).case_insensitive  }
+    it { should validate_uniqueness_of(:abbr).scoped_to(:network_id).case_insensitive  }
+  end
 end
