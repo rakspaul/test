532f07163cf6b900a7a8aac80a295a28a0e8abc4
Dmitrii Samoilov <e2718281828@ya.ru>
2013-12-11 14:44:24 +0200

    refs #193, Import Cox Creative

--- app/models/parsers/base.rb
+++ app/models/parsers/base.rb
@@ -4,6 +4,7 @@ class Parsers::Base
   def initialize(file, order_id)
     @tempfile = File.new(File.join(Dir.tmpdir, 'IO_asset' + Time.current.to_i.to_s), 'w+')
     @tempfile.write File.read(file.path)
+    @tempfile.flush
     @order_id = order_id
   end
 end
