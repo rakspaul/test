1225e52be785a5ec4fd0f3141637db4e9eee8111
Dmitrii Samoilov <e2718281828@ya.ru>
2013-12-16 14:04:16 +0200

    refs #282, Support to email IO note

---
+++ config/environments/test.rb
@@ -0,0 +1,48 @@
+Reachui::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Stores cached data
+  config.cache_store = :dalli_store
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  ## Custom application configurations
+
+  # audience group segment search
+  # comma separated string of network ids
+  config.search_segments_in_network = '6,176'
+
+  # audience group context search
+  # comma separated string of network ids
+  config.search_contexts_in_network = '6,176'
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :test
+end
---
+++ spec/controllers/notes_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe OrderNotesController do
+  setup :activate_authlogic
+
+  let(:user) { FactoryGirl.create :user }
+  let(:advertiser) { FactoryGirl.create :advertiser, network: user.network }
+
+  before :each do
+    account = FactoryGirl.create(:account)
+    AccountSession.create(account)
+  end
+
+  context "new note" do
+    before do
+      @order = Order.create user_id: user.id, network: user.network, network_advertiser_id: advertiser.id, start_date: (Time.current + 1.day), end_date: (Time.current + 12.days), name: "Rodenbaugh's on Audience Network / TWCC (10/3 - 12/29/13) - 788977"
+      @user_to_notify = FactoryGirl.create :user, first_name: "Dmitrii", last_name: "Samoilov", email: "dsamoilov@cogniance.com"
+      @note_body = "test"
+    end
+
+    it "creates new note successfully" do
+      expect {
+        xhr :post, :create, {note: @note_body, username: "Amol Brid", order_id: @order.id, order_note: {note: "test"}}
+      }.to change(OrderNote, :count).by(1)
+    end
+
+    it "creates new note and notifies user" do
+      expect {
+        expect {
+          xhr :post, :create, {note: @note_body, username: "Amol Brid", notify_users: [@user_to_notify.id], order_id: @order.id, order_note: {note: "test"}}
+        }.to change(OrderNote, :count).by(1)
+      }.to change(ActionMailer::Base.deliveries, :count).by(1)
+
+      email = ActionMailer::Base.deliveries.last
+      email.to.should == [@user_to_notify.email]
+      email.subject.should match(/New note added to the order #\d+ from the user/)
+      email.body.should match(Regexp.new(@note_body))
+    end
+  end
+end
