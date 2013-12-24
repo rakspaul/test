9410d5a53c6fc4370b30f4a29e632c2f6814552a
Kashinath <kslagdive@gmail.com>
2013-12-24 10:05:06 -0500

    fixes #305, media and billing controller tests.

---
+++ spec/controllers/admin/billing_contacts_controller_spec.rb
@@ -0,0 +1,104 @@
+require 'spec_helper'
+
+describe Admin::BillingContactsController do
+  setup :activate_authlogic
+  let(:billing_contact) { FactoryGirl.create(:billing_contact) }
+
+  before :each do
+    account = FactoryGirl.create(:account)
+    AccountSession.create(account)
+  end
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index', :id => billing_contact.id, :format => 'json'
+      response.should be_success
+    end
+  end
+
+  describe "POST 'create'" do
+    it "creates new billing contact" do
+      # expect{
+      #   post :create, valid_params
+      #   }.to change(BillingContact,:count).by(1)
+    end
+
+    it "checks validation errors" do
+      post :create, invalid_params
+      data = json_parse(response.body)
+
+      expect(data[:errors]).to include(:name)
+      expect(data[:errors]).to include(:email)
+    end
+  end
+
+  describe "PUT 'update'" do
+    context "updates with diff params" do
+      let(:params) { diff_params }
+
+      before do
+        params[:id] = billing_contact.id
+      end
+
+      it "checks billing contact update" do
+        put :update, params
+
+        assigns(:billing_contact).name.should eq(params[:billingContact][:name])
+        assigns(:billing_contact).phone.should eq(params[:billingContact][:phone])
+        assigns(:billing_contact).email.should eq(params[:billingContact][:email])
+      end
+    end
+
+    context "updates with invalid params" do
+      let(:params) { invalid_params }
+
+      before do
+        params[:id] = billing_contact.id
+      end
+
+      it "checks validation errors" do
+        put :update, params
+        data = json_parse(response.body)
+
+        expect(data[:errors]).to include(:name)
+        expect(data[:errors]).to include(:email)
+      end
+    end
+  end
+
+  private
+    def valid_params
+      params = {
+        billingContact: {
+          name: "Bryan Snyder",
+          phone: billing_contact.phone,
+          email: billing_contact.email,
+          reach_client_id: billing_contact.reach_client_id
+        }
+      }
+      { :format => 'json' }.merge params
+    end
+
+    def invalid_params
+      params = {
+        billingContact: {
+          name: '',
+          phone: '',
+          email: '',
+          reach_client_id: ''
+        }
+      }
+      { :format => 'json' }.merge params
+    end
+
+    def diff_params
+      params = {
+        billingContact: {
+          name: "Bryan Snyder",
+          phone: "7049737346",
+          email: "digital.services@twcable.com",
+        }
+      }
+      { :format => 'json' }.merge params
+    end
+end
