9262072f3678f435fa0d08dd89f0e927deffe90d
Dmitrii Samoilov <e2718281828@ya.ru>
2013-12-27 20:14:24 +0200

    refs #282, Support to email IO note
    Conflicts:
    	app/views/layouts/application.html.haml

--- app/controllers/order_notes_controller.rb
+++ app/controllers/order_notes_controller.rb
@@ -14,24 +14,24 @@ class OrderNotesController < ApplicationController
   def create
     order = Order.find(params[:order_id])
     p = params.require(:order_note).permit(:note)
-    users_ids = params[:notify_users]
+    users_emails = params[:notify_users]
 
     @note = OrderNote.new(p)
     @note.order = order
     @note.user = current_user
     @note.save
 
-    notify_users(users_ids) if !users_ids.blank?
+    notify_users(users_emails) if !users_emails.blank?
 
     respond_with(@note, location: nil)
   end
 
 private
 
-  def notify_users(users_ids)
-    users = User.find(users_ids)
-    users.each do |user|
-      NotificationsMailer.notification_email(user, @note).deliver
+  def notify_users(users_emails)
+    users_emails.each do |user_email|
+      u = User.find_by(email: user_email)
+      NotificationsMailer.notification_email(u, @note).deliver if u
     end
   end
 end
