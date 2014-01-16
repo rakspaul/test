346c383c8c7135f3e58544605e63a1689caf8e2a
Dmitrii Samoilov <e2718281828@ya.ru>
2014-01-16 18:24:55 +0200

    refs #359, Email display changes

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -13,7 +13,8 @@ describe OrderNotesController do
 
   context "new note" do
     before do
-      @order = Order.create user_id: user.id, network: user.network, network_advertiser_id: advertiser.id, start_date: (Time.current + 1.day), end_date: (Time.current + 12.days), name: "Rodenbaugh's on Audience Network / TWCC (10/3 - 12/29/13) - 788977"
+      @order_name = "Rodenbaugh's on Audience Network / TWCC (10/3 - 12/29/13) - 788977"
+      @order = Order.create user_id: user.id, network: user.network, network_advertiser_id: advertiser.id, start_date: (Time.current + 1.day), end_date: (Time.current + 12.days), name: @order_name
       @user_to_notify = FactoryGirl.create :user, first_name: "Dmitrii", last_name: "Samoilov", email: "dsamoilov@cogniance.com"
       @note_body = "test"
     end
@@ -27,13 +28,13 @@ describe OrderNotesController do
     it "creates new note and notifies user" do
       expect {
         expect {
-          xhr :post, :create, {note: @note_body, username: "Amol Brid", notify_users: [@user_to_notify.id], order_id: @order.id, order_note: {note: "test"}}
+          xhr :post, :create, {note: @note_body, username: "Amol Brid", notify_users: [@user_to_notify.email], order_id: @order.id, order_note: {note: "test"}}
         }.to change(OrderNote, :count).by(1)
       }.to change(ActionMailer::Base.deliveries, :count).by(1)
 
       email = ActionMailer::Base.deliveries.last
       email.to.should == [@user_to_notify.email]
-      email.subject.should match(/New note added to the order #\d+ from the user/)
+      email.subject.should match(Regexp.escape(@order_name))
       email.body.should match(Regexp.new(@note_body))
     end
   end
