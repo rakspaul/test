7e6f9aa489b1589d3f8ee3170e8d6d1d90c49030
Dmitrii Samoilov <e2718281828@ya.ru>
2014-01-17 14:15:15 +0200

    refs #359, Email display changes

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationsMailer < ActionMailer::Base
-  def notification_email(email, note)
+  def notification_email(emails, note)
     @note  = note
-    mail(to: email, from: @note.user.email, subject: "#{IoDetail::STATUS[@note.order.io_detail.try(:state).try(:to_sym)]} | ##{@note.order.name} | #{@note.order.start_date.to_date} - #{@note.order.end_date.to_date}")
+    mail(to: emails, from: @note.user.email, subject: "#{IoDetail::STATUS[@note.order.io_detail.try(:state).try(:to_sym)]} | ##{@note.order.name} | #{@note.order.start_date.to_date} - #{@note.order.end_date.to_date}")
   end
 end
