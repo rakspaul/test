8fc6b42486d9d9f123c46057bcfe97c03a3c8981
Amol Brid <amolbrid@gmail.com>
2014-02-06 12:51:40 -0500

    fixing li_assignment_id value in ads.json

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -8,7 +8,7 @@ class AdsController < ActionController::Base
               :zipcodes,
               :designated_market_areas,
               :audience_groups,
-              :creatives
+              {:creatives => :lineitem_assignment}
             ).where(["order_id = ? AND io_lineitem_id IS NOT NULL", params[:order_id].to_i])
     respond_to do |format|
       format.json
