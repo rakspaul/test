db78f8b20676353ab524149e27ab54be9d45cd4c
Amol Brid <amolbrid@gmail.com>
2014-02-07 16:14:31 -0500

    select only targetable cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,5 +3,5 @@ class City < ActiveRecord::Base
   has_and_belongs_to_many :lineitems, join_table: :cities_lineitems, foreign_key: :city_id
 
   scope :in_us, -> { where('country_code = ?', 'US')}
-  scope :xfp_present, -> { where('xfp_id IS NOT NULL') }
+  scope :xfp_present, -> { where('xfp_id IS NOT NULL and xfp_targetable = true') }
 end
