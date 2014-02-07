5468feb6a89dbcc65117e3779fc101228ca82c78
maheshkh <maheshkh@cybage.com>
2014-02-07 07:50:06 -0500

    added rspec for agency controller and model.

--- spec/controllers/agency_controller_spec.rb
+++ spec/controllers/agency_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe AgencyController do
+  setup :activate_authlogic
 
+  before :each do
+    account = FactoryGirl.create(:account)
+    AccountSession.create(account)
+  end
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index', { format: "json" }
+      response.should be_success
+    end
+  end
 end
