f3b62c9ddda3d7e489f63cd9ce1dc9b04bd1acb9
maheshkh <maheshkh@cybage.com>
2014-02-11 08:02:15 -0500

    login_pages_spec changes

--- spec/features/login_pages_spec.rb
+++ spec/features/login_pages_spec.rb
@@ -12,7 +12,9 @@ describe "Login" do
     end
 
     it "should redirect if session is active" do
+      role = FactoryGirl.create(:role)
       valid_user = FactoryGirl.create(:account)
+      role.users << valid_user.user
 
       visit root_path
       fill_in "Username", :with => valid_user.login
@@ -26,12 +28,29 @@ describe "Login" do
   end
 
   describe "POST /" do
+
+    before :each do
+      @role = FactoryGirl.create(:role)
+
+      @agency = FactoryGirl.create(:agency)
+      @agency_user = FactoryGirl.create(:user, client_type: "Agency")
+      @agency.users << @agency_user
+
+      @reach_client = FactoryGirl.create(:reach_client, agency: @agency)
+
+      @account = FactoryGirl.create(:account)
+      @agency_account = FactoryGirl.create(:account, user: @agency_user)
+
+      @role.users << @account.user
+      @role.users << @agency_account.user
+    end
+
     it "should login valid user" do
       valid_user = FactoryGirl.create(:account)
 
       visit root_path
-      fill_in "Username", :with => valid_user.login
-      fill_in "Password", :with => valid_user.password
+      fill_in "Username", :with => @account.login
+      fill_in "Password", :with => @account.password
       click_button "Login"
       page.should have_content 'Signout'
     end
