3df71cb0f9fcabb184d1dd246ee04545a47aa012
Ankur Aute <ankurau@cybage.com>
2014-02-12 08:58:39 -0500

    Initial changes of models and controllers for block violations.

--- app/controllers/admin/block_violations_controller.rb
+++ app/controllers/admin/block_violations_controller.rb
@@ -4,6 +4,8 @@ class Admin::BlockViolationsController < ApplicationController
   layout "admin"
 
   def index
+    block_violations = BlockViolations.all.order("#{sort_column} #{sort_direction}")
+    @block_violations = Kaminari.paginate_array(block_violations).page(params[:page]).per(50);
   end
 
 end
--- app/models/block_violations.rb
+++ app/models/block_violations.rb
@@ -1,4 +1,8 @@
 class BlockViolations < ActiveRecord::Base
   self.table_name = "reach_block_violations"
 
+  belongs_to :site, :primary_key => 'source_id', :class_name => "Site"
+  belongs_to :ad, :primary_key => 'source_id', :class_name => "Ad"
+  belongs_to :advertiser
+
 end
