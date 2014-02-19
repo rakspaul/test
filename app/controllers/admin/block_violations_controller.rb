bc9b104fcb7272da9ec948246adc51a532996fb6
Ankur Aute <ankurau@cybage.com>
2014-02-19 07:57:38 -0500

    Updated UI changes with sorting

--- app/controllers/admin/block_violations_controller.rb
+++ app/controllers/admin/block_violations_controller.rb
@@ -2,10 +2,23 @@ class Admin::BlockViolationsController < ApplicationController
   include Authenticator
 
   layout "admin"
+  respond_to :html
 
   def index
-    block_violations = BlockViolations.all.order("#{sort_column} #{sort_direction}")
-    @block_violations = Kaminari.paginate_array(block_violations).page(params[:page]).per(50);
-  end
+    sort_direction = params[:sort_direction] ? params[:sort_direction] : "desc"
+    sort_column = params[:sort_column] ? params[:sort_column] : "reach_block_violations.created_at"
 
+    if sort_column == "site"
+      sort_column = "sites.name"
+    elsif sort_column == "advertiser"
+      sort_column = "network_advertisers.name"
+    elsif sort_column == "ad"
+      sort_column = "ads.description"
+    elsif sort_column == "job_ran_date"
+      sort_column = "reach_block_violations.created_at"
+    end
+
+    block_violations = BlockViolations.includes(:advertiser).joins(:site, :ad).order("#{sort_column} #{sort_direction}")
+    @block_violations = Kaminari.paginate_array(block_violations).page(params[:page]).per(100);
+  end
 end
--- app/models/block_violations.rb
+++ app/models/block_violations.rb
@@ -1,8 +1,7 @@
 class BlockViolations < ActiveRecord::Base
   self.table_name = "reach_block_violations"
 
-  belongs_to :site, :primary_key => 'source_id', :class_name => "Site"
-  belongs_to :ad, :primary_key => 'source_id', :class_name => "Ad"
+  belongs_to :site
+  belongs_to :ad
   belongs_to :advertiser
-
 end
