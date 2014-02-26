edddfabf1850c929249f53845e1e67ad10fa9f9a
Vladimir SHulga <vshulga@cogniance.com>
2014-02-26 09:36:38 +0200

    refs #524, remove test case

--- app/assets/javascripts/lineitems/lineitems.js
+++ app/assets/javascripts/lineitems/lineitems.js
@@ -664,7 +664,7 @@
         }
       });
 
-      if(_.include(["Pushed", "Failure", "Incomplete Push", "Draft"], this.collection.order.get('order_status')) ||
+      if(_.include(["Pushed", "Failure", "Incomplete Push"], this.collection.order.get('order_status')) ||
         (lineitemsWithoutAds.length > 0)) {
         var dialog = $('#push-confirmation-dialog');
         var liList = dialog.find('.li-without-ads');
