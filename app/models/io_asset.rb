4331b2428c4723cdc8bddea4559ab8a8e127b370
Vladimir SHulga <vshulga@cogniance.com>
2014-03-14 16:24:39 +0200

    refs #460, add creative txt saving

--- app/models/io_asset.rb
+++ app/models/io_asset.rb
@@ -1,4 +1,8 @@
 class IoAsset < ActiveRecord::Base
+  default_scope { where(asset_type: 'io') }
+
+  scope :io_creatives, -> { where(asset_type: 'creatives') }
+
   belongs_to :order
 
   validates :asset_upload_name, :asset_path, presence: true
