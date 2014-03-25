e13f233998831912cc9a8541c2e194d5feae19c4
Amol Brid <amolbrid@gmail.com>
2014-03-25 23:44:43 +0530

    remove column name ambiguity by prepending table name

--- app/models/order.rb
+++ app/models/order.rb
@@ -35,7 +35,7 @@ class Order < ActiveRecord::Base
   scope :filterByAM, lambda { |am| where("io_details.account_manager_id = '#{am}'") unless am.blank? }
   scope :filterByTrafficker, lambda { |trafficker| where("io_details.trafficking_contact_id = '#{trafficker}'") unless trafficker.blank? }
   scope :filterByLoggingUser, lambda { |user, orders_by_user| where("orders.user_id = '#{user.id}'") unless orders_by_user.blank? || orders_by_user == "all_orders" }
-  scope :filterByIdOrNameOrAdvertiser, lambda {|query| where("orders.id::text ILIKE ? or name ILIKE ? or source_id ILIKE ? or io_details.client_advertiser_name ILIKE ?",
+  scope :filterByIdOrNameOrAdvertiser, lambda {|query| where("orders.id::text ILIKE ? or orders.name ILIKE ? or orders.source_id ILIKE ? or io_details.client_advertiser_name ILIKE ?",
                                                              "%#{query}%", "%#{query}%","%#{query}%","%#{query}%") unless query.blank? }
   scope :for_agency, lambda {|agency, is_agency| where("io_details.reach_client_id IN (?)", agency.try(:reach_clients).pluck(:id)) if is_agency}
   scope :filterByReachClient, lambda { |rc| where("reach_clients.name = '#{rc}'") unless rc.blank?  }
