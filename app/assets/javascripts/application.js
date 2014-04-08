37af4a0ade35cc3203a9d35399855e64c09ffa68
Vladimir SHulga <vshulga@cogniance.com>
2014-04-08 16:16:51 +0300

    refs #465, add bootstrap multi select dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,7 @@
 //= require twitter/bootstrap/dropdown
 //= require bootstrap-popup-tooltip.js
 //= require bootstrap-editable.js
+//= require bootstrap-select.js
 //= require select2.min.js
 //= require jquery-fileupload/basic
 //= require jquery.autosize
