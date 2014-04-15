db9d4e85876b57061a0298343d5421b15004ba39
Dmitrii Samoilov <e2718281828@ya.ru>
2014-04-15 17:49:14 +0300

    refs #622, Cox creative not matching - parsing removing a space from the PDF in line break

--- app/models/parsers/cox_creative.rb
+++ app/models/parsers/cox_creative.rb
@@ -1,8 +1,10 @@
+require 'diff/lcs'
+
 class Parsers::CoxCreative < Parsers::Base
 
   CREATIVES_SEPARATOR = /^==+\R/
   CREATIVE_NAME       = /Placement Name:(.*)\R/
-  CREATIVE_HTML_CODE  = /JavaScript code:(.*)\r\n\r\n/m
+  CREATIVE_HTML_CODE  = /JavaScript code:(.*)?(\r\n)?(\r\n)?/m
   CREATIVE_START_DATE = /Placement Start Date:(.*)/
   CREATIVE_END_DATE   = /Placement End Date:(.*)/
   
@@ -13,6 +15,7 @@ class Parsers::CoxCreative < Parsers::Base
 
     creatives = content.split(CREATIVES_SEPARATOR).delete_if(&:empty?)
     creatives.shift if creatives[0].match(/Cox Digital Solutions Ad Tags/m) # remove metainfirmation
+
     @creatives_errors = []
     @creatives = []
     @old_li_creatives = {}
@@ -27,7 +30,7 @@ class Parsers::CoxCreative < Parsers::Base
     end
 
     if @creatives_errors.empty?
-      order = Order.find_by_id @order_id
+      order = Order.find_by(id: @order_id)
       file = File.open @tempfile
       writer = IOFileWriter.new("file_store/io_imports", file, @original_filename, order, 'creatives')
       writer.write
@@ -61,7 +64,6 @@ class Parsers::CoxCreative < Parsers::Base
       placement_name_match  = creative_txt.match(CREATIVE_NAME)
 
       return if placement_name_match.nil?
-
       placement_name  = placement_name_match[1].to_s.strip
       start_date      = start_date(creative_txt)
       end_date        = end_date(creative_txt)
@@ -74,16 +76,34 @@ class Parsers::CoxCreative < Parsers::Base
       return
     end
 
-    if li = Lineitem.find_by(name: placement_name, order_id: @order_id)
-      Creative.transaction do
-        creative = Creative.create name: li.ad_name(start_date, ad_size), network_advertiser_id: li.order.network_advertiser_id, size: ad_size, width: width, height: height, creative_type: CREATIVE_TYPE, redirect_url: "", html_code: javascript_code, network_id: li.order.network_id, data_source_id: 1
+    lis = Order.find(@order_id).lineitems
+    if !lis.blank?
+      matched_li = nil
+      lis.each do |li| 
+        diff = Diff::LCS.diff(li.name, placement_name)
+        # if there is no difference between LI name and creative placement name
+        if diff.empty?
+          matched_li = li
+          break
+        # or there is additional or no blankspace 
+        elsif diff.length == 1 && diff.last.try(:last).try(:element) == " "
+          matched_li = li
+          break
+        end
+      end
 
-        li_assignment = LineitemAssignment.create lineitem: li, creative: creative, start_date: start_date, end_date: end_date, network_id: li.order.network_id, data_source_id: li.order.network.try(:data_source_id)
+      # then we found LI to replace creatives
+      if matched_li
+        Creative.transaction do
+          creative = Creative.create name: matched_li.ad_name(start_date, ad_size), network_advertiser_id: matched_li.order.network_advertiser_id, size: ad_size, width: width, height: height, creative_type: CREATIVE_TYPE, redirect_url: "", html_code: javascript_code, network_id: matched_li.order.network_id, data_source_id: 1
 
-        if !li_assignment.errors.messages.blank?
-          @creatives_errors << "#{index+1}: "+li_assignment.errors.full_messages.join('; ')
-        else
-          @creatives << creative
+          li_assignment = LineitemAssignment.create lineitem: matched_li, creative: creative, start_date: start_date, end_date: end_date, network_id: matched_li.order.network_id, data_source_id: matched_li.order.network.try(:data_source_id)
+
+          if !li_assignment.errors.messages.blank?
+            @creatives_errors << "#{index+1}: "+li_assignment.errors.full_messages.join('; ')
+          else
+            @creatives << creative
+          end
         end
       end
     else
