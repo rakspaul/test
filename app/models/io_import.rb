adfb64d4af15541d9abf2c3d403aa28e4f0d9c6d
Dmitrii Samoilov <e2718281828@ya.ru>
2014-04-22 16:36:16 +0300

    refs #459, Revisions - Import Revisions to Existing Orders and Push; fixing order notes

--- app/models/io_import.rb
+++ app/models/io_import.rb
@@ -197,7 +197,7 @@ class IoImport
 
     def read_notes
       # array with hash in it, because OrdersController#show also use this format
-      @notes = [{note: @reader.find_notes, created_at: Time.current.to_s(:db), username: @current_user.try(:full_name) }]
+      @notes = [{note: @reader.find_notes, created_at: Time.current.to_s(:db), username: @current_user.try(:full_name) }]    
     end
 
     def find_sales_person
