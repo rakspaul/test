5129fd8570b8230b6e07beb76666c54e7151ad19
Dmitrii Samoilov <e2718281828@ya.ru>
2014-05-09 18:53:31 +0300

    refs #662, Revisions Changes; fixed bug w/ duplicating order name

--- app/models/order.rb
+++ app/models/order.rb
@@ -21,7 +21,7 @@ class Order < ActiveRecord::Base
   validates :start_date, :end_date, presence: true
   validates :network_advertiser_id, :user_id, :network_id, presence: true, numericality: { only_integer: true}
   validate :validate_start_date, on: :create
-  validates :name, uniqueness: { message: "The order name is already used.", scope: :network_id }, presence: true, on: :create
+  validates :name, uniqueness: { message: "The order name is already used.", scope: :network_id }, presence: true
   validate :validate_advertiser_id, :validate_network_id, :validate_user_id, :validate_end_date_after_start_date
 
   before_create :create_random_source_id, :make_order_inactive
