f35cc0062e6f06673215ffb8d7a1c5cb7caded8b
mmrajuk <mmrajuk@gmail.com>
2014-05-30 14:24:22 +0530

    Fixed the flickering issue in task details region

--- app/assets/javascripts/activities_tasks/tasks/list/list_view.js
+++ app/assets/javascripts/activities_tasks/tasks/list/list_view.js
@@ -1,5 +1,8 @@
 ReachActivityTaskApp.module("ActivitiesTasks.Tasks.List",function(List,ReachActivityTaskApp,Backbone, Marionette, $, _,JST){
 
+
+    List.currentTaskId = undefined;
+
     List.Task = Marionette.Layout.extend({
       tagName: 'div',
       template: JST['templates/activities_tasks/tasks/task_list_item'],
@@ -9,26 +12,36 @@ ReachActivityTaskApp.module("ActivitiesTasks.Tasks.List",function(List,ReachActi
         'click': 'showTaskView'
       },
 
-      showTaskView: function() {
-        // Create region from container within this row
-        var taskDetailsRegion = this.addRegion("task_" + this.model.id + "_region", "#task_" + this.model.id);
+      showTaskView: function(e) {
 
-        // Close previously opened region
-        if (this.model.collection.shownRegion && (this.model.collection.shownRegion != taskDetailsRegion)) {
-          this.model.collection.shownRegion.close();
-        }
-        // track the reference to currently shown region
-        this.model.collection.shownRegion = taskDetailsRegion;
+           //This piece of login helps in not refreshing continually task details region when click happens on task or task details region.
+           //On close of task details region, the value will be undefined for the current task id.
+           if(List.currentTaskId != undefined){
+                if(List.currentTaskId == this.model.id){
+                    return;
+                }
+            }
+            // Create region from container within this row
+            var taskDetailsRegion = this.addRegion("task_" + this.model.id + "_region", "#task_" + this.model.id);
+
+            // Close previously opened region
+            if (this.model.collection.shownRegion && (this.model.collection.shownRegion != taskDetailsRegion)) {
+              this.model.collection.shownRegion.close();
+            }
+            // track the reference to currently shown region
+            this.model.collection.shownRegion = taskDetailsRegion;
 
 
-        if(this.model.collection.selectedTask) {
-          this.model.collection.selectedTask.$el.removeClass('task-selected');
-        }
+            if(this.model.collection.selectedTask) {
+              this.model.collection.selectedTask.$el.removeClass('task-selected');
+            }
 
-        this.model.collection.selectedTask = this;
-        this.$el.addClass('task-selected');
+            this.model.collection.selectedTask = this;
+            this.$el.addClass('task-selected');
 
-        ReachActivityTaskApp.trigger("include:taskDetails", {task: this.model, aRegion: taskDetailsRegion});
+            ReachActivityTaskApp.trigger("include:taskDetails", {task: this.model, aRegion: taskDetailsRegion});
+
+            List.currentTaskId = this.model.id;
       }
     });
 
@@ -68,6 +81,7 @@ ReachActivityTaskApp.module("ActivitiesTasks.Tasks.List",function(List,ReachActi
         e.stopPropagation();
         this.close();
         this.options.parentRegion.close();
+        List.currentTaskId = undefined;
 //        ReachActivityTaskApp.trigger("include:activities");
       },
 
