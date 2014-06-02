5808d1693f343bafe64ed5df08fd332ae6d3664c
Dmitrii Samoilov <e2718281828@ya.ru>
2014-06-02 16:35:00 +0300

    refs #617, Pull in DFP Orders into OM; create lineitem_assignments and lineitem_video_assignments for DFP-pulled orders

--- spec/controllers/lineitems_controller_spec.rb
+++ spec/controllers/lineitems_controller_spec.rb
@@ -15,10 +15,18 @@ describe LineitemsController do
 
     before do
       @order = FactoryGirl.create :dfp_pulled_order
+      creative = FactoryGirl.create :creative
+      creative2 = FactoryGirl.create :creative
+      video_creative = FactoryGirl.create :video_creative
+      video_creative2 = FactoryGirl.create :video_creative
       ad1 = FactoryGirl.create :ad, order_id: @order.id, description: "Test ad #1"
       ad2 = FactoryGirl.create :ad, order_id: @order.id, description: "Test ad #2"
       FactoryGirl.create :ad_pricing, ad_id: ad1.id
       FactoryGirl.create :ad_pricing, ad_id: ad2.id
+      FactoryGirl.create :ad_assignment, ad: ad1, creative: creative, start_date: ad1.start_date, end_date: ad1.end_date
+      FactoryGirl.create :ad_assignment, ad: ad2, creative: creative2, start_date: ad2.start_date, end_date: ad2.end_date
+      FactoryGirl.create :video_ad_assignment, ad: ad1, video_creative: video_creative, start_date: ad1.start_date, end_date: ad1.end_date
+      FactoryGirl.create :video_ad_assignment, ad: ad2, video_creative: video_creative2, start_date: ad2.start_date, end_date: ad2.end_date
       AdSize.create size: "160x600", width: 160, height: 600, network_id: user.network.id
     end
 
@@ -27,6 +35,22 @@ describe LineitemsController do
       expect {
         xhr :get, :index, {order_id: @order.id}
       }.to change{@order.lineitems.count}.by(2)
+    end
+
+    it "creates lineitem_assignments for creatives" do
+      xhr :get, :index, {order_id: @order.id}
+
+      @order.lineitems.map do |li|
+        expect(li.creatives.count).to eq(1)
+      end
+    end
+
+    it "creates lineitem_video_assignments for creatives" do
+      xhr :get, :index, {order_id: @order.id}
+
+      @order.lineitems.map do |li|
+        expect(li.video_creatives.count).to eq(1)
+      end
     end
   end
 end
