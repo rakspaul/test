27b8ae0127b29c29f5e10c7a644c07215156602f
Ankur Aute <ankurau@cybage.com>
2014-06-09 10:11:17 -0400

    #828, Initial refactored code for reach_clients_controller

--- app/controllers/admin/reach_clients_controller.rb
+++ app/controllers/admin/reach_clients_controller.rb
@@ -11,9 +11,11 @@ class Admin::ReachClientsController < ApplicationController
 
   def index
     sort_direction = params[:sort_direction] ? params[:sort_direction] : "asc"
-    sort_column = params[:sort_column] ? params[:sort_column] : "name"
+    sort_column = params[:sort_column] ? params[:sort_column] : "reach_clients.name"
 
-    if sort_column == "media_contact"
+    if sort_column == "name"
+      sort_column = "reach_clients.name"
+    elsif sort_column == "media_contact"
       sort_column = "media_contacts.name"
     elsif sort_column == "billing_contact"
       sort_column = "billing_contacts.name"
@@ -23,8 +25,11 @@ class Admin::ReachClientsController < ApplicationController
       sort_column = "account_managers_reach_clients.first_name"
     end
 
-    reach_clients = ReachClient.includes(:media_contact, :billing_contact).joins(:sales_person, :account_manager,).of_network(current_network).order(sort_column + " " + sort_direction)
-    @reach_clients = Kaminari.paginate_array(reach_clients).page(params[:page]).per(50);
+    @reach_clients = ReachClient.includes(includes_params)
+                                .of_network(current_network)
+                                .references(includes_params)
+                                .order("#{sort_column} #{sort_direction}")
+                                .page(params[:page]).per(100);
   end
 
   def new
@@ -39,8 +44,7 @@ class Admin::ReachClientsController < ApplicationController
   end
 
   def create
-    p = params.require(:reachClient).permit(:name, :abbr, :sales_person_id, :account_manager_id, :agency_id, :client_buffer)
-    @reach_client = ReachClient.new(p)
+    @reach_client = ReachClient.new(reach_client_params)
     @reach_client.network_id = current_network.id
     @reach_client.user_id = current_user.id
     @reach_client.save
@@ -59,10 +63,18 @@ class Admin::ReachClientsController < ApplicationController
 
   def update
     @reach_client = ReachClient.find(params[:id])
-    p = params.require(:reachClient).permit(:name, :abbr, :sales_person_id, :account_manager_id, :media_contact_id, :billing_contact_id, :agency_id, :client_buffer)
-    @reach_client.update_attributes(p)
+    @reach_client.update_attributes(reach_client_params)
     @reach_client.save
 
     respond_with(@reach_client)
   end
+
+  private
+    def reach_client_params
+      params.require(:reachClient).permit(:name, :abbr, :sales_person_id, :account_manager_id, :media_contact_id, :billing_contact_id, :agency_id, :client_buffer)
+    end
+
+    def includes_params
+      return :media_contact, :billing_contact, :sales_person, :account_manager
+    end
 end
