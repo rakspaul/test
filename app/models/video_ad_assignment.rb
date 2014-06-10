15e5139d0058d59ddf40f394498e855f3b4477da
Dmitrii Samoilov <e2718281828@ya.ru>
2014-06-10 17:38:34 +0300

    refs #814, Store start and end dates in ET timezone

--- app/models/video_ad_assignment.rb
+++ app/models/video_ad_assignment.rb
@@ -20,7 +20,10 @@ private
   # temporary fix [https://github.com/collectivemedia/reachui/issues/814]
   def set_est_flight_dates
     if self[:start_date]
-      self[:start_date] = read_attribute_before_type_cast('start_date').to_date.to_s+" 00:00:00"
+      start_date = read_attribute_before_type_cast('start_date').to_date
+      current = "%.2i:%.2i:%.2i" % [Time.current.hour, Time.current.min, Time.current.sec]
+      start_time = start_date.today? ? current : "00:00:00"
+      self[:start_date] = "#{start_date} #{start_time}"
     end
     if self[:end_date]
       self[:end_date] = read_attribute_before_type_cast('end_date').to_date.to_s+" 23:59:59"
