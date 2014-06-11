6de1b116dba0c415fa2fb1df0fa18912b1458af5
Dmitrii Samoilov <e2718281828@ya.ru>
2014-06-11 20:53:05 +0300

    refs #617, Pull in DFP Orders into OM; added Country to li/ad targeting

--- app/models/lineitem.rb
+++ app/models/lineitem.rb
@@ -24,6 +24,7 @@ class Lineitem < ActiveRecord::Base
   has_and_belongs_to_many :zipcodes, join_table: :lineitem_geo_targetings, class_name: GeoTarget::Zipcode, association_foreign_key: :geo_target_id
   has_and_belongs_to_many :cities, join_table: :lineitem_geo_targetings, class_name: GeoTarget::City, association_foreign_key: :geo_target_id
   has_and_belongs_to_many :states, join_table: :lineitem_geo_targetings, class_name: GeoTarget::State, association_foreign_key: :geo_target_id
+  has_and_belongs_to_many :countries, join_table: :lineitem_geo_targetings, class_name: GeoTarget::Country, association_foreign_key: :geo_target_id
 
   has_and_belongs_to_many :audience_groups, join_table: :lineitems_reach_audience_groups, association_foreign_key: :reach_audience_group_id
 
