6a253549af5100c95ded441f182afa6fd4317fc4
Vladimir SHulga <vshulga@cogniance.com>
2014-06-23 17:00:12 +0300

    refs #779, add creatives spec

--- spec/javascripts/views/creatives/creative_spec.js
+++ spec/javascripts/views/creatives/creative_spec.js
@@ -2,9 +2,7 @@ describe('Creative view', function() {
   describe('ReachUI.Creatives.CreativeView', function() {
 
     beforeEach(function() {
-      this.creative = new ReachUI.Creatives.Creative({
-        name: 'Test'
-      });
+      this.creative = BackboneFactory.create('creative');
 
       this.view = new ReachUI.Creatives.CreativeView({ model: this.creative });
     });
@@ -16,5 +14,32 @@ describe('Creative view', function() {
     it('can be instantiated', function() {
       expect(this.view).not.toBeNull();
     });
+
+    describe('creative row', function() {
+      beforeEach(function() {
+        this.lineitem = BackboneFactory.create('lineitem');
+        this.liView = new ReachUI.LineItems.LineItemView({ model: this.lineitem });
+
+        this.view = new ReachUI.Creatives.CreativeView({
+          model:       this.creative,
+          parent_view: this.liView
+        });
+        var el = this.view.render().$el;
+        $('body').append(el);
+        this.deleteBtnContainer = this.view.$el.find('.delete-btn-container');
+      });
+
+      xit('show delete button for not pushed creative', function() {
+        //console.log(this.deleteBtnContainer);
+        console.log('ACTUAL');
+        console.log($(this.deleteBtnContainer).html());
+        console.log('END');
+        //expect($('<div><div class="delete-btn"></div></div>')).toContainElement('div.delete-btn');
+        //expect($(this.deleteBtnContainer)).toContainElement('div.delete-btn');
+        
+        //expect(this.view.$el.)
+        //creative.get('source_id')
+      });
+    })
   });
 });
