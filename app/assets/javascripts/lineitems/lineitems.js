d0a45a1129ad53ad5d203c2a7416aec7e7012b15
Vladimir SHulga <vshulga@cogniance.com>
2014-06-27 18:11:10 +0300

    refs #946, fix impressions formating issue

--- app/assets/javascripts/lineitems/lineitems.js
+++ app/assets/javascripts/lineitems/lineitems.js
@@ -232,7 +232,7 @@
       '.volume-editable': {
         observe: 'volume',
         onGet: function(val) {
-          return accounting.formatNumber(val, 2);
+          return accounting.formatNumber(val, '');
         }
       },
       '.rate-editable': {
@@ -425,7 +425,7 @@
           collection._recalculateLiImpressionsMediaCost();
         },
         display: function(value) {
-          return accounting.formatNumber(value, 2);
+          return accounting.formatNumber(value, '');
         }
       });
 
