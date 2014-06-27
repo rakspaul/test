090c2bf7e0b3795045d3562e3259957f3df9bfc6
Dmitrii Samoilov <e2718281828@ya.ru>
2014-06-27 17:41:53 +0300

    refs #936, Start Date cannot be in the past message - when choosing today

--- app/assets/javascripts/lineitems/lineitems.js
+++ app/assets/javascripts/lineitems/lineitems.js
@@ -279,6 +279,15 @@
           collection.order.set('start_date', minDate);
 
           view._changeEditable($(this), newValue);
+
+          if(moment(ReachUI.currentTimeWithOffset("-5h")).format("YYYY-MM-DD") > date) {
+            var error_message = 'Start date cannot be in the past';
+            view.$el.find('.start-date').addClass('field_with_errors');
+          } else {
+            var error_message = '';
+            view.$el.find('.start-date').removeClass('field_with_errors');
+          }
+          view.$el.find('.start-date .errors_container').html(error_message);
         },
         datepicker: {
           startDate: ReachUI.initialStartDate(model.get('start_date'))
@@ -297,6 +306,15 @@
           collection.order.set("end_date", maxDate);
 
           view._changeEditable($(this), newValue);
+
+          if(moment(ReachUI.currentTimeWithOffset("-5h")).format("YYYY-MM-DD") > date) {
+            var error_message = 'End date cannot be in the past';
+            view.$el.find('.end-date').removeClass('field_with_errors');
+          } else {
+            var error_message = '';
+            view.$el.find('.end-date').removeClass('field_with_errors');
+          }
+          view.$el.find('.end-date .errors_container').html(error_message);
         },
         datepicker: {
           startDate: moment().format("YYYY-MM-DD")
