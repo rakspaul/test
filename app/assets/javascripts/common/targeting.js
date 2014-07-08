cc40073c2d1c43195cb6627d35e7d4618d1d0ba9
Vladimir SHulga <vshulga@cogniance.com>
2014-07-08 10:16:08 +0300

    refs #582, fix merge bugs

--- app/assets/javascripts/common/targeting.js
+++ app/assets/javascripts/common/targeting.js
@@ -433,12 +433,12 @@
           $apply_ads_dialog.find('.apply-revisions-txt').html('Apply the new targeting to ads');
           $apply_ads_dialog.find('.noapply-btn').click(function() {
             $apply_ads_dialog.modal('hide');
-            //self.options.parent_view._toggleTargetingDialog();
-            self._renderSelectedTargetingOptions();
 
-            this.options.parent_view._hideTargetingDialog();
-            this.options.parent_view.onTargetingDialogToggle();
-            this.$el.parent().hide('slow');
+            self.options.parent_view._hideTargetingDialog();
+            self.options.parent_view.onTargetingDialogToggle();
+            self.$el.parent().hide('slow');
+
+            self._renderSelectedTargetingOptions();
           });
           $apply_ads_dialog.find('.apply-btn').click(function() {
             var targeting = _.pick(self.model.attributes, 'selected_geos', 'frequency_caps', 'selected_key_values', 'selected_zip_codes', 'keyvalue_targeting');
@@ -448,15 +448,12 @@
           });
           $apply_ads_dialog.modal('show');
         } else {
-          this.options.parent_view._toggleTargetingDialog();
+          this.options.parent_view._hideTargetingDialog();
+          this.options.parent_view.onTargetingDialogToggle();
+          this.$el.parent().hide('slow');
+
           this._renderSelectedTargetingOptions();
         }
-        // TODO finish resolve conflicts
-        /*this._renderSelectedTargetingOptions();
-
-        this.options.parent_view._hideTargetingDialog();
-        this.options.parent_view.onTargetingDialogToggle();
-        this.$el.parent().hide('slow');*/
       }
     },
 
