2e0f03eeaf63841637e0ba3ec3be91a8d10cb697
Vladimir SHulga <vshulga@cogniance.com>
2014-07-09 18:57:16 +0300

    refs #682, hide missing geo caution symbol after coping geo targeting

--- app/assets/javascripts/utils/core.js
+++ app/assets/javascripts/utils/core.js
@@ -361,6 +361,7 @@ ReachUI.deselectAllItems = function(options, scope) {
         item.renderTargetingDialog();
         if (type == 'ad') {
           ReachUI.showCondensedTargetingOptions.apply(item);
+          item.toggleMissingGeoCaution();
         }
       }
     });
