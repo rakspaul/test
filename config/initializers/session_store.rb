172e6d6e1878fa803e512ddb7a50f70787e7996c
Venkat G <venkateswararao.g@gmail.com>
2014-08-28 15:16:13 +0530

    [CD-87]configure the cookie host name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Reachui::Application.config.session_store :cookie_store, key: '_reachui_session'
+Reachui::Application.config.session_store :cookie_store, key: '_reachui_session', domain: :all, tld_length: 3
+
+cookie_config = YAML.load_file(File.join(Rails.root, 'config', 'cookie.yml')).deep_symbolize_keys
+
+Reachui::Application.config.secret_key_base = cookie_config[:secret_key_base]
+Reachui::Application.config.session_store(:cookie_store,
+                                          :key   => cookie_config[:key],
+                                          :domain => cookie_config[Rails.env.to_sym][:domain])
