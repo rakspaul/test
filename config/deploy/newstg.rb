2fbddca92244b08c0f3e2820db00c5c21f7ce307
abhishekkrsingh1 <abhisheks@collective.com>
2014-09-03 09:08:29 +0000

    adding new staging env

---
+++ config/deploy/newstg.rb
@@ -0,0 +1,14 @@
+set :branch, 'cdesk_p1'
+
+set :rails_env, "newstg" # set for capistrano asset pipeline precompilation
+
+set :hipchat_env, "Stage"
+
+set :app1, "104.131.251.80"
+
+role :web, app1
+role :app, app1
+
+
+
+set :default_environment, 'JAVA_HOME' => "/usr/lib/jvm/java-6-sun-1.6.0.26"
