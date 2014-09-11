443f515eacf1918f62764c324d48d947d42a46ce
Venkat G <venkateswararao.g@gmail.com>
2014-09-11 19:46:17 +0530

    campaign list - separated the js & css only for this page

---
+++ app/assets/javascripts/cdesk_application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+
+//= require jquery
+//= require jquery-ui-1.10.3.custom.min
+//= require bootstrap.min
+//= require angular.min
+//= require angular-cache-2.3.7
+//= require angular-cookies
+//= require angular-resource.min
+//= require angular-route.min
+//= require highcharts-ng
+//= require highcharts
+
+//= require cdesk/app
+//= require cdesk/charts/pacing
+//= require cdesk/charts/line
+//= require cdesk/controllers/campaign_controller
+//= require cdesk/models/campaign
+//= require cdesk/services/dataservice
+//= require cdesk/utils
