d2803502aea1a509fbb6cc1d4ddf552c3c447717
roydon <roydon@techjini.com>
2014-09-19 17:56:54 +0530

    moved style to css #CD-353

--- app/assets/javascripts/cdesk/charts/line.js
+++ app/assets/javascripts/cdesk/charts/line.js
@@ -26,7 +26,7 @@
                     tooltip: {
                         useHTML:true,
                         formatter: function () {
-                            return '<div style="background:#fff;padding:5px;border:1px solid #ccc"> <b>' + this.series.name  + '</b><br/>' + this.x + ': ' + this.y+'</div>';
+                            return '<div class="chart-tool-tip" > <b>' + this.series.name  + '</b><br/>' + this.x + ': ' + this.y+'</div>';
                         }
                     },
                     title: {
