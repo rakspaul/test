82fbe6c5f1a000d0ee3b63c8278d6ebf251f1099
gauravs-collective <gauravs@collective.com>
2014-10-20 12:54:30 +0530

    Optimization for line items controller #CDESK-142

--- app/controllers/desk/lineitems_controller.rb
+++ app/controllers/desk/lineitems_controller.rb
@@ -8,19 +8,23 @@ class Desk::LineitemsController < LineitemsController
 
   def set_lineitem_status(lineitems)
     lineitems.each do |lineitem|
-      ads_count = lineitem.ads.size
+      lineitem.ads.load
+
+      ads_count = lineitem.ads.inject(0) {|count| count = count + 1; count}
+      puts ads_count
       if  ads_count > 0
-        if lineitem.ads.where(:status => Ad::DELIVERING).first
+
+        if lineitem.ads.select{|ad| ad.status == Ad::DELIVERING}.first
           lineitem.li_status = Ad::STATUS[:delivering]
-        elsif lineitem.ads.where(:status => Ad::READY).first
+        elsif lineitem.ads.select{|ad| ad.status == Ad::READY}.first
           lineitem.li_status = Ad::STATUS[:draft]
-        elsif lineitem.ads.where(:status => Ad::COMPLETED).size == ads_count
+        elsif lineitem.ads.select{|ad| ad.status == Ad::COMPLETED}.count == ads_count
           lineitem.li_status = Ad::STATUS[:completed]
-        elsif lineitem.ads.where(:status => Ad::CANCELED).size == ads_count
+        elsif lineitem.ads.select{|ad| ad.status == Ad::CANCELED}.count == ads_count
           lineitem.li_status = Ad::STATUS[:draft]
-        elsif lineitem.ads.where(:status => Ad::PAUSED).size == ads_count
+        elsif lineitem.ads.select{|ad| ad.status == Ad::PAUSED}.count == ads_count
           lineitem.li_status = Ad::STATUS[:draft]
-        elsif lineitem.ads.where(:status => Ad::PAUSED_INVENTORY_RELEASED).size == ads_count
+        elsif lineitem.ads.select{|ad| ad.status == Ad::PAUSED_INVENTORY_RELEASED}.count == ads_count
           lineitem.li_status = Ad::STATUS[:draft]
           # as per Amber's request changed 'Paused Inventory Released' status to 'Paused'
         else
