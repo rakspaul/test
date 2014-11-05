293f6c50ffee31d6948cacce1831f2c8485bf878
Richa <richag@collective.com>
2014-11-05 14:34:12 +0530

    basic angular integration for inventory tab

--- app/assets/javascripts/cdesk/services/inventoryservice.js
+++ app/assets/javascripts/cdesk/services/inventoryservice.js
@@ -4,27 +4,19 @@
         $http.defaults.headers.common['Authorization'] = "CollectiveAuth token=" + user_id + ":" + auth_token + " realm=\"reach-ui\"";
         return {
             getStrategiesForCampaign: function(campaingId) {
-               var url='http://localhost:3000/assets/cdesk/tmp/strategy_selected.json';
-               // var url = apiPaths.apiSerivicesUrl + '/campaigns/' + campaingId + '/strategies/meta';
+                var url = apiPaths.apiSerivicesUrl + '/campaigns/' + campaingId + '/strategies/meta';
                 return this.fetch(url);
             },
 
             getCampaingsForUser: function() {
-
-
-
                 var url ='/assets/cdesk/tmp/campaings.json';
                 // var url =  apiPaths.apiSerivicesUrl + '/campaigns/meta?user_id='+user_id;
-
                 return this.fetch(url);
 
             },
 
             getCategoryDataForStrategy: function (param) {
-
-
                 var url= apiPaths.apiSerivicesUrl +'/campaigns/'+ param.campaign_id+ '/strategies/'+param.strategyId+'/inventory/'+param.domain+'?kpi_type='+param.kpi_type + '&date_filter='+ param.time_filter;
-
                 return this.fetch(url);
             },
 
@@ -56,5 +48,6 @@
             }
 
         };
+        
     });
 }());
