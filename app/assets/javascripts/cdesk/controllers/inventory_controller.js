3b082f8d6e0fee5f031ff25d68e6c5e5a8b89a75
ravi <ravi.m@techjini.com>
2014-11-05 20:05:05 +0530

    charts modifications

--- app/assets/javascripts/cdesk/controllers/inventory_controller.js
+++ app/assets/javascripts/cdesk/controllers/inventory_controller.js
@@ -7,7 +7,9 @@ var angObj = angObj || {};
             console.log("Inside init method on inventroyController");
             $scope.selectedCampaign = {
                 id: '401459',
-                name: 'RE SZ WDBJ Piedmont Eye Center Q114'
+                name: 'RE SZ WDBJ Piedmont Eye Center Q114',
+               strategyId: '22281',
+                kpiType:'CPA'
             };
 
         $scope.campaignlist();
@@ -28,12 +30,23 @@ var angObj = angObj || {};
                 inventoryService.getStrategiesForCampaign(campaignId).then(function(result){
                     $scope.strategies = result.data.data;
                 });
-              //  $scope.selectedStrategy = $scope.strategies[0];
+
+                $scope.getStrategyChart({campaign_id: campaignId, strategyId: $scope.selectedCampaign.strategyId, kpi_type:$scope.selectedCampaign.kpiType});
+
             };
 
+        $scope.getStrategyChart = function(param){
+            inventoryService.getCategoryDataForStrategy(param).then(function(result){
+                //Chart
+                $scope. inventoryChart = columnline.highChart(result.data.data[0].inv_metrics, $scope.selectedCampaign.kpiType);
+
+            });
+        };
 
 
-        //Chart
+        //$scope. inventoryChart= columnline.highChart();
+
+
        $scope. inventoryChart= columnline.highChart();
         //Hot fix to show the campaign tab selected
         $("ul.nav:first").find('.active').removeClass('active').end().find('li:contains(Reports)').addClass('active');
