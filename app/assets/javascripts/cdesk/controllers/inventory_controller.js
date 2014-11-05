a0960047f93e2ab21ca03b870f5870d400f18331
Richa <richag@collective.com>
2014-11-05 14:34:12 +0530

    basic angular integration for inventory tab

---
+++ app/assets/javascripts/cdesk/controllers/inventory_controller.js
@@ -0,0 +1,32 @@
+var angObj = angObj || {};
+(function () {
+    'use strict';
+    angObj.controller('InventoryController', function ($scope, inventoryService, $http) {
+
+        $scope.init = function () {
+            console.log("Inside init method on inventroyController");
+            $scope.selectedCampaign = {
+                id: '401459',
+                name: 'RE SZ WDBJ Piedmont Eye Center Q114'
+            };
+
+        $scope.strategylist($scope.selectedCampaign.id);
+
+        };
+
+            $scope.strategylist = function(campaingId) {
+                console.log("inventroy_controller strategyList function ")
+
+                inventoryService.getStrategiesForCampaign(campaingId).then(function (result) {
+                    console.log("Strategies data is ");
+                    console.log(result.data);
+
+                    $scope.strategies = result.data;
+
+                })
+            };
+
+        //Hot fix to show the campaign tab selected
+        $("ul.nav:first").find('.active').removeClass('active').end().find('li:contains(Reports)').addClass('active');
+    });
+}());
