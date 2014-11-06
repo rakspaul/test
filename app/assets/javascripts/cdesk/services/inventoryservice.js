7c03f3f3ebcdc4544f7f3d96978addd26d6e8628
Richa <richag@collective.com>
2014-11-06 10:44:22 +0530

    strategy drop down changes

--- app/assets/javascripts/cdesk/services/inventoryservice.js
+++ app/assets/javascripts/cdesk/services/inventoryservice.js
@@ -9,6 +9,7 @@
             },
 
             getCampaingsForUser: function() {
+
                 var url ='/assets/cdesk/tmp/campaings.json';
                 // var url =  apiPaths.apiSerivicesUrl + '/campaigns/meta?user_id='+user_id;
                 return this.fetch(url);
