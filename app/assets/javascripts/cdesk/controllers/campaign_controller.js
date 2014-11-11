927276c35ae326c4683ada85a16ed6221a36a4f4
ravi <ravi.m@techjini.com>
2014-11-11 13:59:31 +0530

    highlight campaign tab

--- app/assets/javascripts/cdesk/controllers/campaign_controller.js
+++ app/assets/javascripts/cdesk/controllers/campaign_controller.js
@@ -317,5 +317,6 @@
 
         return Campaigns;
     });
-
+    //Hot fix to show the campaign tab selected
+    //$("ul.nav:first").find('.active').removeClass('active').end().find('li:first').addClass('active');
 }());
