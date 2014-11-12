7a75eb9208f21d0e6038f6900c323770eac6abb3
ravi <ravi.m@techjini.com>
2014-11-12 18:36:23 +0530

    chart impression labesl

--- app/assets/javascripts/cdesk/charts/columnline.js
+++ app/assets/javascripts/cdesk/charts/columnline.js
@@ -4,24 +4,25 @@
 
 
         var  getRepString = function(x) {
-            if(isNaN(x)) return x;
+            //if(isNaN(x)) return x;
+            var y = Math.abs(x);
 
-            if(x < 9999) {
+            if(y < 9999) {
                 return x;
             }
 
-            if(x < 1000000) {
+            if(y < 1000000) {
                 return Math.round(x/1000) + "K";
             }
-            if( x < 10000000) {
-                return (x/1000000) + "M";
+            if( y < 10000000) {
+                return (x/1000000).toFixed(2) + "M";
             }
 
-            if(x < 1000000000) {
+            if(y < 1000000000) {
                 return Math.round((x/1000000)) + "M";
             }
 
-            if(x < 1000000000000) {
+            if(y < 1000000000000) {
                 return Math.round((x/1000000000)) + "B";
             }
 
