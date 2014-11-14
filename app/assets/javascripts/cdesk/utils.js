a0b20b8347fec848f8648f01c359fe16f680f1c9
ravi <ravi.m@techjini.com>
2014-11-14 21:34:53 +0530

    util changes for common filters

--- app/assets/javascripts/cdesk/utils.js
+++ app/assets/javascripts/cdesk/utils.js
@@ -25,12 +25,50 @@
         var goToLocation = function (url) {
             $location.url(url);
         };
-
+        var reportsDropDowns = function(){
+                return {
+                    'tabs' : [
+                        {
+                            href:'optimization',
+                            title: 'Optimization Impact'
+                        },
+                        {
+                            href:'#',
+                            title: 'Cost'
+                        },
+                        {
+                            href:'#',
+                            title: 'Performance'
+                        },
+                        {
+                            href:'inventory',
+                            title: 'Inventory'
+                        },
+                        {
+                            href:'viewability',
+                            title: 'Viewability'
+                        }
+                    ],
+                    'duration' : [
+                        {value: 'weekly', text: 'Last 7 days'},
+                        {value: 'monthly', text: 'Last month'},
+                        {value: 'life_time', text: 'Life Time'}
+                    ],
+                    'kpiTypes' : [
+                        {value: 'CPA', text: 'CPA'},
+                        {value: 'CPC', text: 'CPC'},
+                        {value: 'CPM', text: 'CPM'},
+                        {value: 'CTR', text: 'CTR'},
+                        {value: 'action_rate', text: 'Action Rate'}
+                    ]
+                }
+        };
         return {
             formatDate: formatDate,
             makeTitle: makeTitle,
             roundOff: roundOff,
-            goToLocation: goToLocation
+            goToLocation: goToLocation,
+            reportsDropDowns: reportsDropDowns
         };
     }]);
 
