f299d75dded39957c421d93a7c76b694b4e8437a
Richa <richag@collective.com>
2014-11-22 21:00:29 +0530

    empty search string will show complete list

--- app/assets/javascripts/cdesk/controllers/viewability_controller.js
+++ app/assets/javascripts/cdesk/controllers/viewability_controller.js
@@ -243,7 +243,7 @@ var angObj = angObj || {};
                 '</ul>',
             link: function ($scope, element, attrs) {
 
-
+               $scope.originalCampaingList = $scope.$parent.campaigns;
 
                 //Check the status and load the function accordingly for the campaigns list
                 if(attrs.showSearchBtn === "true") {
@@ -314,7 +314,8 @@ var angObj = angObj || {};
                         }
                         if(name.length == 0){
                             console.log("length is zero ");
-                            $scope.listColumns = $scope.$parent.campaigns;
+                            $scope.listColumns = $scope.originalCampaingList ;
+                            // $scope.$parent.campaigns;
                             console.log($scope.listColumns);
                             //$scope.campaingns = $scope.$parent.campaignFullList;
                         }
