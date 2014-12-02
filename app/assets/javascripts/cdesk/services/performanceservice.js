3bb41142dae83c8d25b723703e34c51e880ee7b0
Richa <richag@collective.com>
2014-12-02 14:59:47 +0530

    removed hard coded user id

--- app/assets/javascripts/cdesk/services/performanceservice.js
+++ app/assets/javascripts/cdesk/services/performanceservice.js
@@ -16,7 +16,7 @@
 
             getCampaingsForUser: function () {
                 //  var url = '/assets/cdesk/tmp/campaings.json';
-                var url =  apiPaths.apiSerivicesUrl + '/campaigns/meta?user_id=11127'; //+user_id;
+                var url =  apiPaths.apiSerivicesUrl + '/campaigns/meta?user_id=' +user_id ;
                 return this.fetch(url);
 
             },
