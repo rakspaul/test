6fcf8091350b50c3f73378cfa16a3becef3a6319
Richa <richag@collective.com>
2014-12-08 12:03:08 +0530

    removing user id for cost service page.

--- app/assets/javascripts/cdesk/services/costservice.js
+++ app/assets/javascripts/cdesk/services/costservice.js
@@ -14,7 +14,7 @@
             },
 
             getCampaingsForUser: function () {
-                var url =  apiPaths.apiSerivicesUrl + '/campaigns/meta?user_id=11127'; // + user_id ;
+                var url =  apiPaths.apiSerivicesUrl + '/campaigns/meta?user_id='+ user_id ;
                 return this.fetch(url);
 
             },
