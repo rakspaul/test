4cb9851e8eeec2e6f369beb63cd549690d186dca
raji-collective <raji@collective.com>
2014-12-22 14:56:11 +0530

    incorrectly commited a file. reverting

--- app/helpers/domain_helper.rb
+++ app/helpers/domain_helper.rb
@@ -3,7 +3,7 @@ module DomainHelper
     return @domain_desk if defined?(@domain_desk)
     # invoking this method from decorators where we don't have request
     @domain_desk =  if Thread.current[:request].host.match(/localhost/)
-                      true#ENV['DEPLOY_ENV'] == Role::CDESK
+                      ENV['DEPLOY_ENV'] == Role::CDESK
                     else
                       Thread.current[:request].host.match(/desk/)
                     end
@@ -12,7 +12,7 @@ module DomainHelper
   def domain_reach?
     return @domain_reach if defined?(@domain_reach)
     @domain_reach = if Thread.current[:request].host.match(/localhost/)
-                      false#ENV['DEPLOY_ENV'] == Role::REACH_UI
+                      ENV['DEPLOY_ENV'] == Role::REACH_UI
                     else
                       Thread.current[:request].host.match(/reach/)
                     end
