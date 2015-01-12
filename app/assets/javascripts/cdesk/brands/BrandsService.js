91cdff8bfa723ae61fbef3ed965089d99ebe6163
Richa <richag@collective.com>
2015-01-12 15:43:14 +0530

    moved brands filter api to scala from rails

--- app/assets/javascripts/cdesk/brands/BrandsService.js
+++ app/assets/javascripts/cdesk/brands/BrandsService.js
@@ -1,7 +1,8 @@
 //Data fetching in service.
-brandsModule.service("brandsService", ['$http', 'dataService', function ($http, dataService) {
+brandsModule.service("brandsService", ['$http', 'dataService','api','apiPaths', function ($http, dataService,  api, apiPaths ) {
   this.fetchBrands = function () {
-    var url = '/desk/advertisers.json';
+    //  var url = '/desk/advertisers.json';
+     var url = apiPaths.apiSerivicesUrl + '/brands?user_id=' + user_id ;
     return dataService.fetch(url);
   }
 }]);
