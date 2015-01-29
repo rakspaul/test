818277d3e11f4804f90496ccdc1646962aa852a5
gauravs-collective <gauravs@collective.com>
2015-01-29 14:30:23 +0530

    New gauge UI.

--- app/assets/javascripts/cdesk/common/controllers/GaugeController.js
+++ app/assets/javascripts/cdesk/common/controllers/GaugeController.js
@@ -2,8 +2,7 @@
   'use strict';
   commonModule.controller('gaugeController', function ($scope, gauge, gaugeModel, constants) {
     gauge.createGauge();
-    gauge.setMessage(constants.GAUGE_PERFORMANCE, '% are On Track');
-    gauge.drawPointer(constants.GAUGE_PERFORMANCE);
+    gauge.setMessage(constants.GAUGE_PERFORMANCE, '%');
     $scope.$on(constants.EVENT_BRAND_CHANGED, function(event, args) {
       getGaugeData();
     });
