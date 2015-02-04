d5424b41425c941b92658c482f765dadce0077be
Roydon DSouza <roydon@techjini.com>
2015-02-04 22:48:19 +0530

    gantt chart integration

--- app/assets/javascripts/cdesk/common/controllers/GanttChartController.js
+++ app/assets/javascripts/cdesk/common/controllers/GanttChartController.js
@@ -1,6 +1,22 @@
 (function () {
     'use strict';
-    commonModule.controller('ganttChartController', function ($scope, loginModel, $cookieStore, $location, loginService) {
+    commonModule.controller('ganttChartController', function ($scope, $location, ganttChart) {
+
+    	
+    	$scope.init = function(){
+            $scope.calendarBusy = true;
+           	ganttChart.newCalendar();
+
+        };
+        $scope.add = function(){
+        	console.log('adding');
+        	  	ganttChart.addTask();
+        }
+         $scope.prev = function(){
+        	console.log('adding');
+        	  	ganttChart.prev();
+        }
+        $scope.init();
 
     });
 }());
--- app/assets/javascripts/cdesk/common/d3/ganttChart.js
+++ app/assets/javascripts/cdesk/common/d3/ganttChart.js
@@ -4,6 +4,531 @@
         this.createGanttChart = function() {
 
         };
+
+		d3.gantt = function() {
+		    var FIT_TIME_DOMAIN_MODE = "fit";
+		    var FIXED_TIME_DOMAIN_MODE = "fixed";
+		    var CAMPAIGN_HEIGHT = 30;
+
+		    var CALENDAR_HEIGHT = 600;
+		    var CALENDAR_WIDTH = 1050;
+
+		    var margin = {
+		        top: 20,
+		        right: 40,
+		        bottom: 20,
+		        left: 50
+		    };
+		    var timeDomainStart = d3.time.day.offset(new Date(), -300);
+		    var timeDomainEnd = d3.time.hour.offset(new Date(), +300);
+		    var timeDomainMode = FIT_TIME_DOMAIN_MODE; // fixed or fit
+		    var taskTypes = [];
+		    var taskStatus = [];
+		    var height = CALENDAR_HEIGHT - margin.top - margin.bottom - 5;
+		    var width = CALENDAR_WIDTH  - margin.right - margin.left - 5;
+
+		    var tickFormat = "%d"; //%b
+
+
+		    var keyFunction = function(d) {
+		        console.log("key" + d.startDate);
+		        return d.startDate + d.taskName + d.endDate;
+		    };
+
+		    var rectTransform = function(d) {
+		        console.log("transform" + d);
+		        return "translate(" + x(d.startDate) + "," + y(d.taskName) + ")";
+		    };
+
+		    var x = d3.time.scale().domain([timeDomainStart, timeDomainEnd]).range([0, width]).clamp(true);
+		    var y = d3.scale.ordinal().domain(taskTypes).rangeBands([0, 500]);
+
+		    var xAxis = d3.svg.axis()
+		        .scale(x).orient("top")
+		        .tickFormat(d3.time.format(tickFormat))
+		        .ticks(d3.time.days, 1)
+		        .tickSize(height - margin.top, height - margin.top)
+		        .tickPadding(-15);
+
+		    var yAxis = d3.svg.axis().scale(y).orient("right").tickFormat("").tickSize(0);
+
+		    var initTimeDomain = function(tasks) {
+		        if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
+		            if (tasks === undefined || tasks.length < 1) {
+		                timeDomainStart = d3.time.day.offset(new Date(), -3);
+		                timeDomainEnd = d3.time.hour.offset(new Date(), +3);
+		                return;
+		            }
+		            tasks.sort(function(a, b) {
+		                return a.endDate - b.endDate;
+		            });
+		            timeDomainEnd = tasks[tasks.length - 1].endDate;
+		            tasks.sort(function(a, b) {
+		                return a.startDate - b.startDate;
+		            });
+		            timeDomainStart = tasks[0].startDate;
+		        }
+		    };
+
+		    var initAxis = function() {
+		        x = d3.time.scale().domain([timeDomainStart, timeDomainEnd]).range([0, width]).clamp(true);
+		        y = d3.scale.ordinal().domain(taskTypes).rangeBands([0, 500]);
+
+		        xAxis = d3.svg.axis()
+		            .scale(x).orient("top")
+		            .tickFormat(d3.time.format(tickFormat))
+		            .ticks(d3.time.days, 1)
+		            .tickSize(height - margin.top, height - margin.top)
+		            .tickPadding(-15); //modified from 8
+
+		        yAxis = d3.svg.axis().scale(y).orient("right").tickFormat("").tickSize(0);
+		    };
+
+		    function gantt(tasks) {
+
+		        initTimeDomain(tasks);
+		        initAxis();
+
+		        var svg = d3.select("#calendar_widget")
+		            .append("svg")
+		            .attr("class", "chart")
+		            .attr("width", width + margin.left + margin.right)
+		            .attr("height", height + margin.top + margin.bottom)
+		            .append("g")
+		            .attr("class", "gantt-chart")
+		            .attr("width", width + margin.left + margin.right)
+		            .attr("height", height + margin.top + margin.bottom)
+		            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+
+		        svg.append("g")
+		            .attr("class", "x axis")
+		            .attr("transform", "translate(0, " + (height - margin.top - margin.bottom) + ")")
+		            .transition()
+		            .call(xAxis)
+		            .selectAll(".tick text").attr("style", "font-family:sans-serif;font-size:12pt;").attr("x", function(d) {
+		                return 10
+		            });
+
+		        svg.append("g").attr("class", "y axis").transition().call(yAxis);
+
+//temporary *****
+		        var box =svg.append("rect")
+		        .attr("x",39)
+		        .attr("y",50)
+		            .attr("width",300)
+		            .attr("height",25)
+		            .attr("stroke","#ccc")
+		            .attr("fill","#ccc");
+
+		          box.transition().duration(1000)
+		          .attr("width",621)
+//temporary *****		          
+
+		        return gantt;
+
+		    };
+
+		    gantt.redraw = function(tasks) {
+
+		        initTimeDomain(tasks);
+		        initAxis();
+
+		        var svg = d3.select("svg");
+
+		        var ganttChartGroup = svg.select(".gantt-chart");
+
+		        var rectData = ganttChartGroup.selectAll(".node").data(tasks, keyFunction);
+		        var rect = rectData.enter();
+		        var rectGroup = rect.append("g").attr("class", "node").on("click", function(d) {
+		            alert('click captured' + d.name);
+		        });
+
+
+		        rectGroup.append("rect")
+		            .attr("title", function(d) {
+		                return "campaign";
+		            })
+		            .attr("id", function(d, i) {
+		                return "campaign-" + i;
+		            })
+		            .attr("class", function(d) {
+		                if (taskStatus[d.status] == null) {
+		                    return "bar campaigns";
+		                }
+		                return taskStatus[d.status] + " campaigns";
+		            })
+		            .attr("width", function(d) {
+		                return (x(d.endDate) - x(d.startDate));
+		            })
+		            .attr("height", function(d) {
+		                return CAMPAIGN_HEIGHT
+		            })
+		            .transition()
+		            .attr("transform", rectTransform);
+
+		        //top bar 
+		        rectGroup.append("rect")
+		            .attr("class", "header")
+		            .attr("fill", "green")
+		            .attr("width", function(d) {
+		                return (x(d.endDate) - x(d.startDate));
+		            })
+		            .attr("height", 3)
+		            .transition()
+		            .attr("transform", rectTransform);
+
+		        rectGroup.append("text")
+		            .attr("class", "campaigns_name")
+		            .attr("x", 30)
+		            .attr("y", CAMPAIGN_HEIGHT / 2)
+		            .attr("dy", ".35em")
+		            .attr("style", function(d) {
+		                if ((x(d.endDate) - x(d.startDate)) != 0) {
+		                    return "";
+		                } else {
+		                    return "display:none";
+		                }
+		            })
+		            .text(function(d) {
+		                return d.taskName;
+		            })
+		            .transition()
+		            .attr("transform", rectTransform);
+
+		        rectGroup.append("image")
+		            .attr("class", "icon")
+		            .attr("x", CAMPAIGN_HEIGHT / 3)
+		            .attr("y", CAMPAIGN_HEIGHT / 5)
+		            .attr("height", CAMPAIGN_HEIGHT - 10)
+		            .attr("width", function(d) {
+		                if ((x(d.endDate) - x(d.startDate)) != 0) {
+		                    return 15;
+		                } else {
+		                    return 0;
+		                }
+		            })
+		            .attr("xlink:href", "https://qa-desk.collective.com/assets/statusWidget/active_icon-062230a04a602d1f17ea262b34a0198f.png")
+		            .transition()
+		            .attr("transform", rectTransform);
+
+		        //incase of a tooltip
+		        //   	rectGroup.on("mouseover", function(d) {
+		        //     var g = d3.select(this); // The node
+		        //     // The class is used to remove the additional text later
+		        //     var info = g.append('text')
+		        //        .classed('info', true)
+		        //        .attr('x', 20)
+		        //        .attr('y', 10)
+		        //        .attr("transform", rectTransform)
+		        //        .text('More info');
+		        // })
+		        // .on("mouseout", function() {
+		        //     // Remove the info text on mouse out.
+		        //     d3.select(this).select('text.info').remove();
+		        // });
+
+		        var node = ganttChartGroup.selectAll(".node").data(tasks, keyFunction);
+		        var campaignBody = ganttChartGroup.selectAll(".campaigns").data(tasks, keyFunction);
+		        var campaignText = ganttChartGroup.selectAll(".campaigns_name").data(tasks, keyFunction);
+		        var campaignTopStroke = ganttChartGroup.selectAll(".header").data(tasks, keyFunction);
+		        var campaignsStatusIcon = ganttChartGroup.selectAll(".icon").data(tasks, keyFunction);
+
+
+
+
+		        var translateVisualElements = function(a) {
+		            a.transition()
+		                .delay(0)
+		                .attr("transform", rectTransform)
+		                .attr("width", function(d) {
+		                    return (x(d.endDate) - x(d.startDate));
+		                });
+		        };
+
+		        var translateGraphicElements = function(a) {
+		            a.transition().delay(0)
+		                .attr("transform", rectTransform)
+		                .attr("width", function(d) {
+		                    if ((x(d.endDate) - x(d.startDate)) != 0) {
+		                        return 15;
+		                    } else {
+		                        return 0;
+		                    }
+		                })
+		                .attr("style", function(d) {
+		                    if ((x(d.endDate) - x(d.startDate)) >= 40) {
+		                        return "";
+		                    } else {
+		                        return "display:none";
+		                    }
+		                });
+		        };
+
+		        translateVisualElements(node);
+		        translateVisualElements(campaignBody);
+		        translateVisualElements(campaignTopStroke);
+		        translateGraphicElements(campaignText);
+		        translateGraphicElements(campaignsStatusIcon);
+
+		        rectData.exit().remove();
+
+
+		        svg.select(".x").transition().call(xAxis)
+		            .selectAll(".tick text").attr("style", "font-family:sans-serif;font-size:12pt").attr("x", function(d) {
+		                return 10
+		            });
+		        svg.select(".y").transition().call(yAxis);
+
+		        return gantt;
+		    };
+
+		    gantt.margin = function(value) {
+		        if (!arguments.length)
+		            return margin;
+		        margin = value;
+		        return gantt;
+		    };
+
+		    gantt.timeDomain = function(value) {
+		        if (!arguments.length)
+		            return [timeDomainStart, timeDomainEnd];
+		        timeDomainStart = +value[0], timeDomainEnd = +value[1];
+		        return gantt;
+		    };
+
+		    /**
+		     * @param {string}
+		     *                vale The value can be "fit" - the domain fits the data or
+		     *                "fixed" - fixed domain.
+		     */
+		    gantt.timeDomainMode = function(value) {
+		        if (!arguments.length)
+		            return timeDomainMode;
+		        timeDomainMode = value;
+		        return gantt;
+
+		    };
+
+		    gantt.taskTypes = function(value) {
+		        if (!arguments.length)
+		            return taskTypes;
+		        taskTypes = value;
+		        return gantt;
+		    };
+
+		    gantt.taskStatus = function(value) {
+		        if (!arguments.length)
+		            return taskStatus;
+		        taskStatus = value;
+		        return gantt;
+		    };
+
+		    gantt.width = function(value) {
+		        if (!arguments.length)
+		            return width;
+		        width = +value;
+		        return gantt;
+		    };
+
+		    gantt.height = function(value) {
+		        if (!arguments.length)
+		            return height;
+		        height = +value;
+		        return gantt;
+		    };
+
+		    gantt.tickFormat = function(value) {
+		        if (!arguments.length)
+		            return tickFormat;
+		        tickFormat = value;
+		        return gantt;
+		    };
+
+		    return gantt;
+		};//end of gantt
+
+	
+var gantt;
+var tasks;
+var taskNames ;
+var taskStatus;
+		var maxDate ;
+		var minDate ;
+		var format ;
+		var timeDomainString;
+
+		function prev() {
+		    format = "%d";
+		    gantt.timeDomain([d3.time.day.offset(getEndDate(), -7), getEndDate()]);
+		    gantt.tickFormat(format);
+		    gantt.redraw(tasks);
+
+		}
+		function addTask() {
+console.log('here');
+		    var lastEndDate = getEndDate();
+		    var taskStatusKeys = Object.keys(taskStatus);
+		    var taskStatusName = taskStatusKeys[Math.floor(Math.random() * taskStatusKeys.length)];
+		    var taskName = taskNames[Math.floor(Math.random() * taskNames.length)];
+		    var name = "test";
+
+		    tasks.push({
+		        "startDate": d3.time.day.offset(lastEndDate, Math.ceil(Math.random(10))),
+		        "endDate": d3.time.day.offset(lastEndDate, (Math.ceil(Math.random(74))) + 1),
+		        "taskName": taskName,
+		        "status": taskStatusName,
+		        "name": name
+		    });
+
+		    changeTimeDomain(timeDomainString);
+		    gantt.redraw(tasks);
+		};
+
+
+		function getEndDate() {
+		    var lastEndDate = Date.now();
+		    if (tasks.length > 0) {
+		        lastEndDate = tasks[tasks.length - 1].endDate;
+		    }
+
+		    return lastEndDate;
+		}
+function next() {
+		    format = "%d";
+		    gantt.timeDomain([getEndDate(), d3.time.day.offset(getEndDate(), +7)]);
+		    gantt.tickFormat(format);
+		    gantt.redraw(tasks);
+
+		}
+
+		function changeTimeDomain(timeDomainString) {
+		   // this.timeDomainString = timeDomainString;
+		    switch (timeDomainString) {
+		        case "1hr":
+		            format = "%H:%M:%S";
+		            gantt.timeDomain([d3.time.hour.offset(getEndDate(), -1), getEndDate()]);
+		            break;
+		        case "3hr":
+		            format = "%H:%M";
+		            gantt.timeDomain([d3.time.hour.offset(getEndDate(), -3), getEndDate()]);
+		            break;
+
+		        case "6hr":
+		            format = "%H:%M";
+		            gantt.timeDomain([d3.time.hour.offset(getEndDate(), -6), getEndDate()]);
+		            break;
+
+		        case "1day":
+		            format = "%H:%M";
+		            gantt.timeDomain([d3.time.day.offset(getEndDate(), -1), getEndDate()]);
+		            break;
+
+		        case "1week":
+		            format = "%d";
+		            gantt.timeDomain([d3.time.day.offset(getEndDate(), -14), getEndDate()]);
+		            break;
+
+		        case "1month":
+		            format = "%d";
+		            gantt.timeDomain([d3.time.day.offset(getEndDate(), -30), getEndDate()]);
+		            break;
+		        default:
+		            format = "%H:%M"
+
+		    }
+		    gantt.tickFormat(format);
+		    gantt.redraw(tasks);
+		}
+
+	
+
+
+		//********************** IMPLEMENTATION
+function newCalendar(){
+
+
+		tasks = [{
+		    "startDate": new Date("Tue Dec 25 00:00:01 EST 2012"),
+		    "endDate": new Date("Tue Jan 08 23:59:59 EST 2013"),
+		    "taskName": "Sierra",
+		    "status": "RUNNING",
+		    "name": "Campaign Name - X34234 34530453"
+		}, {
+		    "startDate": new Date("Thu Dec 20 00:00:01 EST 2012"),
+		    "endDate": new Date("Thu Jan 10 23:59:59 EST 2013"),
+		    "taskName": "TWC",
+		    "status": "FAILED",
+		    "name": "TWC Auto 2345234 234234SDFS"
+		}, {
+		    "startDate": new Date("Thu Dec 20 00:00:01 EST 2012"),
+		    "endDate": new Date("Thu Jan 10 23:59:59 EST 2013"),
+		    "taskName": "TWC",
+		    "status": "FAILED",
+		    "name": "TWC Auto 2345234 234234SDFS"
+		}, {
+		    "startDate": new Date("Tue Jan 01 00:00:01 EST 2013"),
+		    "endDate": new Date("Sat Jan 05 23:59:59 EST 2013"),
+		    "taskName": "TWC",
+		    "status": "FAILED",
+		    "name": "TWC Auto 2345234 234234SDFS"
+		}];
+
+		taskStatus = {
+		    "SUCCEEDED": "bar",
+		    "FAILED": "bar-failed",
+		    "RUNNING": "bar-running",
+		    "KILLED": "bar-killed"
+		};
+
+		taskNames = ["TWC", "Corolla", "Sierra", "Piedmont Eye Care"];
+
+		tasks.sort(function(a, b) {
+		    return a.endDate - b.endDate;
+		});
+		maxDate = tasks[tasks.length - 1].endDate;
+		tasks.sort(function(a, b) {
+		    return a.startDate - b.startDate;
+		});
+		minDate = tasks[0].startDate;
+
+		format = "%d";
+		timeDomainString = "1week";
+
+
+
+		
+
+		
+
+	//this.gantt=gantt;
+	console.log("new chart");
+	
+		 gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format); //.height(450).width(800);;
+	   
+		var margin = {
+		    top: 20,
+		    right: 40,
+		    bottom: 20,
+		    left: 50
+		};
+		gantt.margin(margin);
+
+		gantt.timeDomainMode("fixed");
+		changeTimeDomain(timeDomainString);
+
+		gantt(tasks);
+
+};
+//expose this function to public
+		this.newCalendar = newCalendar ;
+		this.addTask = addTask;
+		
+	this.prev = prev;
+
+
+
+
     })
 }());
 
