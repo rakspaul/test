11b9aabaa4b7b80ed2324e5643bf6bb4b0e8e2eb
raji-collective <raji@collective.com>
2015-02-24 14:21:54 +0530

    changed unicorn working folder

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,24 +21,24 @@ worker_processes 4
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/home/amp/reachui/current" # available in 0.94.0+
+working_directory "/home/amp/crpt-ui/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/reachui.sock", :backlog => 64
+listen "/tmp/crpt-ui.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 60
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/home/amp/reachui/shared/pids/unicorn.pid"
+pid "/home/amp/crpt-ui/shared/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/amp/reachui/shared/log/unicorn.stderr.log"
-stdout_path "/home/amp/reachui/shared/log/unicorn.stdout.log"
+stderr_path "/home/amp/crpt-ui/shared/log/unicorn.stderr.log"
+stdout_path "/home/amp/crpt-ui/shared/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
