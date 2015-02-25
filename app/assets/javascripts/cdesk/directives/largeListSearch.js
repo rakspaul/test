2576b3b26982aca958d5a996ba77123ab0333267
Saudamini M <saudamini@techjini.com>
2015-02-25 13:05:49 +0530

    added campaign and strategy title

--- app/assets/javascripts/cdesk/directives/largeListSearch.js
+++ app/assets/javascripts/cdesk/directives/largeListSearch.js
@@ -8,22 +8,24 @@
             },
             controller: 'directiveController',
             template: '' +
-                ' <div class="pull-left dropdown dropdown_type1">' +
-                '<span class="dropdown_ul_text" data-toggle="dropdown">' +
-                '<ul class="nav navbar-nav">' +
-                '<li class="dropdown" >' +
-                '<div id="campaignsDropdownDiv" style="display:inline;">' +
-                '<input style="width: 600px;" class="dropdown-toggle inactive dd_txt" ng-model="selectedObj.name" id="campaignDropdown" title="{{selectedObj.name}}" placeholder="{{selectedObj.name }}" data-ng-click="filterDropDown()" />' +
-                '<span class="sort-image-inactive" id="#campaignsDropdownDiv"></span>' +
-                '<span class="campaign_name_length">{{selectedObj.name}}</span>' +
-                '</div>' +
-                '<ul class="dropdown-menu" role="menu"  id="campaigns_list">' +
-                '<li ng-repeat="campaign in campaigns" class="campaigns_list_li" value="{{campaign.campaign_id}}" _kpi="{{campaign.kpi_type}}" title="{{campaign.name}}" >{{campaign.name }}</li>' +
-                '</ul>' +
-                '</li>' +
-                '</ul>' +
-                '<span id="campaign_arrow_img" class="arrrow_img"></span>' +
-                '</span>',
+            '<div class="clearfix dropdown_type1_holder pull-left">' +
+                '<span class="report_heading_dropdown_txt">Campaign</span>' +
+                '<div class="pull-left dropdown dropdown_type1">' +
+                 '<span class="dropdown_ul_text" data-toggle="dropdown">' +
+                    '<ul class="nav navbar-nav">' +
+                     '<li class="dropdown" >' +
+                        '<div id="campaignsDropdownDiv" style="display:inline;">' +
+                            '<input style="width: 600px;" class="dropdown-toggle inactive dd_txt" ng-model="selectedObj.name" id="campaignDropdown" title="{{selectedObj.name}}" placeholder="{{selectedObj.name }}" data-ng-click="filterDropDown()" />' +
+                            '<span class="sort-image-inactive" id="#campaignsDropdownDiv"></span>' +
+                            '<span class="campaign_name_length">{{selectedObj.name}}</span>' +
+                         '</div>' +
+                         '<ul class="dropdown-menu" role="menu"  id="campaigns_list">' +
+                             '<li ng-repeat="campaign in campaigns" class="campaigns_list_li" value="{{campaign.campaign_id}}" _kpi="{{campaign.kpi_type}}" title="{{campaign.name}}" >{{campaign.name }}</li>' +
+                         '</ul>' +
+                       '</li>' +
+                    '</ul>' +
+                    '<span id="campaign_arrow_img" class="arrrow_img"></span>' +
+                '</span></div></div><!-- dropdown_type1_holder -->',
             link: function ($scope, element, attrs) {
 
                 // $scope.originalAllCampaingList = $scope.allCampaigns ;
