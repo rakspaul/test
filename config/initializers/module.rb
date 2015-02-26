40e23f506c230341b92cb1a16625591333c975cc
Ankit Tandon <ankit.tandon@yahoo.co.in>
2015-02-26 13:17:25 +0530

    removed unused rails folders

--- config/initializers/module.rb
+++
@@ -1,5 +0,0 @@
-class Module
-  def const_values
-    constants.map {|const| const_get(const)}
-  end
-end
--- config/initializers/yajl_template_handler.rb
+++
@@ -1,3 +0,0 @@
-require './lib/yajl_template_handler'
-
-ActionView::Template.register_template_handler('yajl', YajlTemplateHandler )
