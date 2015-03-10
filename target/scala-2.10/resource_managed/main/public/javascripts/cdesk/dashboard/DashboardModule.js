8a59dfdd321317c32dae339b3ebf3d44719abb56
Richa <richag@collective.com>
2015-03-10 17:36:46 +0530

    removed targed folder files

--- target/scala-2.10/resource_managed/main/public/javascripts/cdesk/dashboard/DashboardModule.js
+++
@@ -1,9 +0,0 @@
-
-var dashboardModule = angular.module('dashboardModule', ['timePeriodModule'])
-
-  .config(function($routeProvider) {
-    $routeProvider.when('/dashboard', {
-      templateUrl: '/assets/html/dashboard.html',
-      controller: 'dashboardController'
-    })
-  })
