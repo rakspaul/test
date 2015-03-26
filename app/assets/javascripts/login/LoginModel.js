b0b987fa50503f394ad7d06835a58dc75418fd54
Abhimanyu <abhimanyus@collective.com>
2015-03-26 21:06:33 +0530

    functional issue..(related to “data not available”  or loading)

--- app/assets/javascripts/login/LoginModel.js
+++ app/assets/javascripts/login/LoginModel.js
@@ -46,11 +46,11 @@
         );
         localStorage.setItem( 'selectedKpi', 'ctr');
         localStorage.setItem('isNavigationFromCampaigns','false');
-        localStorage.setItem('selectedStrategy',JSON.stringify({
+        /*localStorage.setItem('selectedStrategy',JSON.stringify({
             id: '-1',
             name : 'Loading...'
         })
-        );
+        );*/
     },
 
     getLoginName : function() {
