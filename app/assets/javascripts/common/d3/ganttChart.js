cff4752869ee477ad2a05de3a363808631d270af
Roydon DSouza <roydon@techjini.com>
2015-04-09 03:22:38 +0530

    inconsistent end date representation now consistent #CRPT-1922

--- app/assets/javascripts/common/d3/ganttChart.js
+++ app/assets/javascripts/common/d3/ganttChart.js
@@ -814,6 +814,13 @@
                                         return 0;
                                 }
                             })
+                             .attr("x", function(d){
+                                if(isPast(tdEdges[0], d.startDate)) {
+                                        return 2;
+                                } else {
+                                    return 0;
+                                }
+                            })
                             .attr("y", function(d) {
                                 return y(d.taskName) + 2;
                             })
@@ -840,7 +847,7 @@
                             })
                             .attr("x", function(d){
                             	if(isPast(tdEdges[0], d.startDate)) {
-		                    			return -2;
+		                    			return 0;
 		                    	} else {
 		                    		return 0;
 		                    	}
