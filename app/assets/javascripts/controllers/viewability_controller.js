ccf7935408e89e196f75b5a4629827f84adcff86
mmohan <mmrajuk@gmail.com>
2015-04-08 11:55:30 +0530

    Uncomented strategy call during KPI change for inventory & viewability tabs.#CRPT-1923

--- app/assets/javascripts/controllers/viewability_controller.js
+++ app/assets/javascripts/controllers/viewability_controller.js
@@ -116,9 +116,9 @@ var angObj = angObj || {};
         };
 
         $scope.$on(constants.EVENT_KPI_CHANGED, function(e) {
-
             $scope.selected_filters.kpi_type = kpiSelectModel.getSelectedKpi();
             //$scope.$apply();
+            $scope.callBackStrategyChange();
         });
 
 
