2c1b729c776daf6871d630d19679b3696a727233
Ankit Tandon <ankitt@collective.com>
2015-04-17 15:04:12 +0530

    Update crptuibeta.sh

--- scripts/crptuibeta.sh
+++ scripts/crptuibeta.sh
@@ -26,7 +26,7 @@ then
    /sbin/sv t crpt-ui-beta
 elif [ $CDESK_ENV == "ewr_stg" ]
 then
-   echo "Its EWR Production environment !!!"
+   echo "Its EWR Staging environment !!!"
    cp ${CONF_EWR_STG_FILE} ${CONF_FILE}
    cp /tmp/crpt-ui-1.0-SNAPSHOT/scripts/crpt-ui-beta-runit.sh  /tmp/crpt-ui-1.0-SNAPSHOT/bin/crpt-ui-beta-runit.sh
    cp -r /tmp/crpt-ui-1.0-SNAPSHOT/* /home/amp/crpt-ui-beta/
