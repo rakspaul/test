8f050ccce3dae5465a8307cd94de1d800d92b32e
Abhimanyu <abhimanyus@collective.com>
2015-04-27 21:32:23 +0530

    After cache is cleared , and user opens Collective Reporting . the home page is displayed for a moment before login screen

--- app/assets/javascripts/common/controllers/HeaderController.js
+++ app/assets/javascripts/common/controllers/HeaderController.js
@@ -1,34 +1,32 @@
 (function () {
   'use strict';
-    commonModule.controller('headerController', function ($scope, $rootScope, loginModel, $cookieStore, $location, loginService, constants) {
+    commonModule.controller('headerController', function ($scope, $rootScope, $http, loginModel, $cookieStore, $location) {
+
         $scope.user_name = loginModel.getUserName();
         $scope.version = version;
+
         $scope.showProfileMenu = function() {
             $("#profileDropdown").toggle();
             $("#brandsList").hide();
             $(".page_filters").find(".filter_dropdown_open").removeClass("filter_dropdown_open");
             $("#cdbDropdown").hide();
         };
+
         $scope.removeUserData = function() {
             $cookieStore.remove('cdesk_session');
+            $http.defaults.headers.common.Authorization = '';
             localStorage.clear();
             loginModel.deleteData();
         };
 
         $scope.logout = function() {
             $scope.removeUserData();
-            loginService.logoutAction(function(response) {
-                if(response.status == "success"){
-                    /*
-                        //CRPT-2111
-                        var locationPath =  $location.path();
-                        $cookieStore.put(constants.COOKIE_REDIRECT, locationPath);
-                    */
-                    $location.url('/login');
-                }
-            });
+            $location.url('/login');
         };
+
+        $rootScope.logout = $scope.logout; //setting logout function on $rootscope so that i can use the same function on app.js
         $rootScope.dashboard = {};
         $rootScope.dashboard.isNetworkUser = loginModel.getIsNetworkUser();
+
     });
 }());
