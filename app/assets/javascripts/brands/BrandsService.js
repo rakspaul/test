7bc61d1f88fcbd9d0e7f2d19693dbacf49e40680
Abhimanyu <abhimanyus@collective.com>
2015-04-28 13:18:11 +0530

    Selected brand name is not shown on the title of dashboard(CRPT-2142) Brand name is not changing on the title of the dashboard page(CRPT-2144)

--- app/assets/javascripts/brands/BrandsService.js
+++ app/assets/javascripts/brands/BrandsService.js
@@ -1,15 +1,26 @@
 //Data fetching in service.
-brandsModule.service("brandsService", ['$http', 'dataService','api','apiPaths','_', function ($http, dataService,  api, apiPaths,_ ) {
-  //default values
-  this.fetchBrands = function (searchCriteria) {
-    //  var url = '/desk/advertisers.json';
-    var url = apiPaths.apiSerivicesUrl + '/brands?' ;
-    if(!_.isNull(searchCriteria.key) && !_.isEmpty(searchCriteria.key))
-      url += "key="+searchCriteria.key+"&";
-    if(!_.isNull(searchCriteria.limit) && !isNaN(searchCriteria.limit))
-      url +=  "limit="+searchCriteria.limit+"&";
-    if(!_.isNull(searchCriteria.offset) && !isNaN(searchCriteria.offset))
-      url +=  "offset="+searchCriteria.offset;
-    return dataService.fetch(url);
-  }
-}]);
+brandsModule.service("brandsService", function ($rootScope, $http, dataService,  api, constants, apiPaths,_ ) {
+    //default values
+    var service = {};
+
+    service.fetchBrands = function (searchCriteria) {
+        var url = apiPaths.apiSerivicesUrl + '/brands?' ;
+        if(!_.isNull(searchCriteria.key) && !_.isEmpty(searchCriteria.key))
+          url += "key="+searchCriteria.key+"&";
+        if(!_.isNull(searchCriteria.limit) && !isNaN(searchCriteria.limit))
+          url +=  "limit="+searchCriteria.limit+"&";
+        if(!_.isNull(searchCriteria.offset) && !isNaN(searchCriteria.offset))
+          url +=  "offset="+searchCriteria.offset;
+        return dataService.fetch(url);
+    };
+
+    service.preForBrandBroadcast = function(brand) {
+        this.brand = brand;
+        this.broadcastItem();
+    };
+
+    service.broadcastItem = function() {
+        $rootScope.$broadcast(constants.EVENT_BRAND_CHANGED);
+    };
+    return service;
+});
