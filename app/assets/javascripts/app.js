0e7cccc954ace3aef2175d932e511d5d5e88b494
Saudamini M <saudamini@techjini.com>
2015-05-20 11:57:20 +0530

    some more  indentation

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -23,13 +23,12 @@ var angObj = '';
   );
 
 
-angular.module( 'ui.bootstrap.carousel' ,  [ 'ui.bootstrap.transition' ]) 
-    .controller ( 'CarouselController' ,  [ '$scope' ,  '$timeout' ,  '$transition' ,  '$q' ,  function  ( $scope ,  $timeout ,  $transition ,  $q )  { 
-    }]).directive ( 'carousel' ,  [ function ()  { 
-        return  { 
-
-        } 
-}]);
+    angular.module( 'ui.bootstrap.carousel' ,  [ 'ui.bootstrap.transition' ]) 
+        .controller ( 'CarouselController' ,  [ '$scope' ,  '$timeout' ,  '$transition' ,  '$q' ,  function  ( $scope ,  $timeout ,  $transition ,  $q )  { 
+        }]).directive ( 'carousel' ,  [ function ()  { 
+            return  { 
+            } 
+    }]);
 
     angObj.config(function ($routeProvider, $httpProvider) {
         $routeProvider
