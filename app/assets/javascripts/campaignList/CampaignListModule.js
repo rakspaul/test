69efbd0420e634c2376f2b2a38bd29ae3c6c2610
Saudamini M <saudamini@techjini.com>
2015-05-20 12:15:40 +0530

    removed carousel code from app to campaign module

--- app/assets/javascripts/campaignList/CampaignListModule.js
+++ app/assets/javascripts/campaignList/CampaignListModule.js
@@ -7,3 +7,10 @@ var campaignListModule = angular.module('campaignListModule', ['commonModule','u
       controller: 'campaignListController'
     })
   })
+
+angular.module( 'ui.bootstrap.carousel' ,  [ 'ui.bootstrap.transition' ]) 
+    .controller ( 'CarouselController' ,  [ '$scope' ,  '$timeout' ,  '$transition' ,  '$q' ,  function  ( $scope ,  $timeout ,  $transition ,  $q )  { 
+    }]).directive ( 'carousel' ,  [ function ()  { 
+        return  { 
+        } 
+}]);
