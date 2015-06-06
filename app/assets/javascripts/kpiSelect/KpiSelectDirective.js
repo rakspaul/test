67b2f7dff1346cfe5702a16ff95b8164245bf801
Robert Mauger <rmauger@collective.com>
2015-06-06 18:34:46 -0400

    changes related to dropdown to show  hidden columns if video quartile data is present

--- app/assets/javascripts/kpiSelect/KpiSelectDirective.js
+++ app/assets/javascripts/kpiSelect/KpiSelectDirective.js
@@ -4,9 +4,12 @@
         return {
             restrict:'EAC',
             controller: 'kpiSelectController',
-            scope: {type : '='},
+            scope: {type : '=',
+                changedrpval :'='
+            },
             templateUrl: assets.html_kpi_drop_down,
             link: function(scope, element, attrs) {
+                scope.whichCaller = attrs;
                 scope.$watch('type', function(newValue, oldValue) {
                     if(newValue !== oldValue) {
                         scope.setSelectedKpi(newValue)//selected_filters.kpi_type = newValue;
