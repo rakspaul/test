4c76589af17b15ee496d301dab5e22f1210f2d1c
Ankit Tandon <ankit.tandon@yahoo.co.in>
2015-07-24 09:56:40 +0530

    adding ewr dev settings

--- scripts/crptui.sh
+++ scripts/crptui.sh
@@ -14,6 +14,7 @@ echo "CRPT UI SnapShot unzipped."
 
 CONF_FILE=/tmp/crpt-ui-1.0-SNAPSHOT/conf/application.conf
 CONF_QA_FILE=/tmp/crpt-ui-1.0-SNAPSHOT/conf/application_qa.conf
+CONF_EWR_DEV_FILE=/tmp/crpt-ui-1.0-SNAPSHOT/conf/application_ewr_dev.conf
 CONF_EWR_QA_FILE=/tmp/crpt-ui-1.0-SNAPSHOT/conf/application.ewr_qa.conf
 CONF_EWR_STG_FILE=/tmp/crpt-ui-1.0-SNAPSHOT/conf/application.ewr_stg.conf
 CONF_EWR_PROD_FILE=/tmp/crpt-ui-1.0-SNAPSHOT/conf/application.ewr_prod.conf
@@ -25,6 +26,17 @@ then
   echo "Its QA environment !!!"
   echo "Changing copying application_qa.conf to application.conf"
   cp ${CONF_QA_FILE} ${CONF_FILE} 
+  cp -r /tmp/crpt-ui-1.0-SNAPSHOT /home/amp/crpt-ui/
+  sudo cp /tmp/crpt-ui /etc/init.d/crpt-ui
+  sudo chmod 755 /etc/init.d/crpt-ui
+  sudo chkconfig --add crpt-ui
+  sudo /etc/init.d/crpt-ui start
+elif [$CDESK_ENV == "dev"]
+then
+  sudo /etc/init.d/crpt-ui stop
+  echo "Its Dev environment !!!"
+  echo "Changing copying application_qa.conf to application.conf"
+  cp ${CONF_EWR_DEV_FILE} ${CONF_FILE} 
   cp -r /tmp/crpt-ui-1.0-SNAPSHOT /home/amp/crpt-ui/
   sudo cp /tmp/crpt-ui /etc/init.d/crpt-ui
   sudo chmod 755 /etc/init.d/crpt-ui
