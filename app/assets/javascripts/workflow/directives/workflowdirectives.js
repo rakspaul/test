607947fbdf249c46740bf1f0997aa83ab187a597
Abhimanyu <abhimanyus@collective.com>
2015-08-12 00:30:18 +0530

    changes related to filter drop down on creative list and add page.

--- app/assets/javascripts/workflow/directives/workflowdirectives.js
+++ app/assets/javascripts/workflow/directives/workflowdirectives.js
@@ -2,19 +2,69 @@
     'use strict';
     angObj.directive('creativeDropDown', function (utils, constants) {
         return {
-            controller: function($scope, $cookieStore, $location){
+            controller: function($scope, $window, $routeParams, constants, workflowService, $timeout, utils, $location){
+                $scope.creativeFilterData = {};
+                var creativeFilter = {
+                    clients :  function() {
+                        workflowService.getClients().then(function (result) {
+                            if (result.status === "OK" || result.status === "success") {
+                                var responseData = result.data.data;
+                                $scope.creativeFilterData['clients'] =  _.sortBy(responseData, 'name');
+                                $scope.defaultClient  = $scope.creativeFilterData['clients'][0];
+                                creativeFilter.fetchAdvertisers($scope.defaultClient.id)
+                            }
+                            else{
+                                creativeFilter.errorHandler(result);
+                            }
+                        }, creativeFilter.errorHandler);
+                    },
+
+                    fetchAdvertisers :  function(clientId) {
+                        workflowService.getAdvertisers(clientId).then(function (result) {
+                            if (result.status === "OK" || result.status === "success") {
+                                var responseData = result.data.data;
+                                $scope.creativeFilterData['advertisers'] =  _.sortBy(responseData, 'name');
+                                $scope.defaultAdvertiser = $scope.creativeFilterData['advertisers'][0];
+                                $scope.$parent.prarentHandler(clientId, $scope.defaultAdvertiser.id);
+                            }
+                            else{
+                                creativeFilter.errorHandler(result);
+                            }
+                        }, creativeFilter.errorHandler);
+                    },
+
+                    errorHandler : function(errData) {
+                        console.log(errData);
+                    }
+
+                }
+
+                creativeFilter.clients();
+
+                $scope.clientsDropdown = function () {
+                    $("#clientsList").toggle();
+                };
+
+                $scope.advertisersDropdown = function () {
+                    $("#advertisersList").toggle();
+                };
+
+                $scope.selectClient = function(client) {
+                    $("#client_name_selected").text(client.name);
+                    $scope.clientId = client.id;
+                    creativeFilter.fetchAdvertisers(client.id);
+                };
+
+
+                $scope.selectAdvertisers = function(advertiser) {
+                    $("#advertiser_name_selected").text(advertiser.name);
+                    $scope.$parent.prarentHandler($scope.clientId, advertiser.id);
+                };
             },
             scope: {},
             restrict:'EAC',
             templateUrl: assets.html_creative_drop_down,
             link: function($scope, element, attrs) {
-                $scope.data = JSON.parse(attrs.data);
-                $scope.dropdownType = attrs.type;
-                console.log("directive");
-                console.log($scope.data);
-
-                //element.find('.clientsList_ul').show();
-
             }
         };
     });
