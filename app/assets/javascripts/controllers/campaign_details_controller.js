8314c671eb966064bfde162e7540a64d694bcfac
Saudamini M <saudamini@techjini.com>
2015-08-20 13:04:44 +0530

    Campaigns tab is highlighted even when the user is on Reports Overview page and moved inline css

--- app/assets/javascripts/controllers/campaign_details_controller.js
+++ app/assets/javascripts/controllers/campaign_details_controller.js
@@ -24,7 +24,10 @@
         $scope.activityLogFilterByStatus = true;
 
         //Hot fix to show the campaign tab selected
-        $(".main_navigation").find('.active').removeClass('active').end().find('#campaigns_nav_link').addClass('active');
+        $(".main_navigation").find('.active').removeClass('active').end().find('#reports_nav_link').addClass('active');
+        $(".main_navigation").find(".header_tab_dropdown").removeClass('active_tab');
+        $(".main_navigation").find("#reports_overview_tab").addClass("active_tab") ;
+
         $scope.campaigns = new Campaigns();
         $scope.is_network_user = loginModel.getIsNetworkUser();
         var campaignList = [];
