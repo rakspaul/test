4387d8b9d720a017845060ef37c6460645370473
Priya Harish <priyah@collective.com>
2015-08-21 14:59:50 +0530

    script validation-- CW-751,CW-791 view tag popup and update: CW-782 hide "no creative "when data is found in ads create

--- app/assets/javascripts/workflow/controllers/creative_list_controller.js
+++ app/assets/javascripts/workflow/controllers/creative_list_controller.js
@@ -9,21 +9,15 @@ var angObj = angObj || {};
         $scope.adData.screenTypes =[];
         $scope.creativeListLoading = true;
         $scope.creativesNotFound = false;
+        $scope.showViewTagPopup=false;
+        $scope.edittrue=true;
+
+        //$scope.popUpData.tag="";
 
         $scope.getAdFormatIconName = function(adFormat) {
             adFormat =  adFormat || 'display';
             var adFormatMapper = {'display' : 'picture', 'video' : 'film', 'rich media' : 'paperclip', 'social' : 'user' }
             return adFormatMapper[adFormat.toLowerCase()];
-        }
-        $scope.ShowHideTag = function (event) {
-            var elem = $(event.target); 
-            if( elem.siblings(".script_tag").is(":visible") ) {
-                elem.siblings(".script_tag").hide() ;
-                elem.text("View Tag") ;
-            } else {
-                elem.siblings(".script_tag").show() ; 
-                elem.text("Hide Tag") ;
-            }
         }
         var creativeList = {
 
@@ -64,10 +58,70 @@ var angObj = angObj || {};
                 $scope.creativesNotFound = true;
             }
         }
-        
         // Success Message Show/Hide
         // If tag was added successfully run this
-        $(".successMessage").delay( 300 ).animate({opacity: 1}, 'slow').delay( 1500 ).animate({opacity: 0}, 'slow');
+        //$(".successMessage").delay( 300 ).animate({opacity: 1}, 'slow').delay( 1500 ).animate({opacity: 0}, 'slow');
+
+
+        $scope.ShowHideTag=function(obj){
+                        $scope.showViewTagPopup=true;
+                        $scope.popUpData=obj;
+                        //console.log(obj);
+        }
+
+        $scope.updateTag=function(context){ //console.log($scope.popUpData);
+
+            var PatternOutside = new RegExp(/<script.*>.*(https:).*<\/script>.*/);
+            var PatternInside =  new RegExp(/<script.*(https:).*>.*<\/script>.*/);
+            var tagLower=$scope.popUpData.tag.toLowerCase().replace(' ', '').replace(/(\r\n|\n|\r)/gm,'');
+            console.log(tagLower);
+            if (tagLower.match(PatternOutside)) {
+                    $scope.updateCreative();
+            }else if (tagLower.match(PatternInside)) {
+                    $scope.updateCreative();
+            }else{
+                context.IncorrectTag = true;
+                context.incorrectTagMessage = "You have entered an invalid Javascript tag.Please review carefully and try again";
+                console.log("Incorrect tag");
+          }
+
+        }
+        $scope.updateCreative=function(){
+             var putCrDataObj = {};
+             putCrDataObj.name = $scope.popUpData.name;
+             putCrDataObj.tag = $scope.popUpData.tag;
+             putCrDataObj.sizeId = $scope.popUpData.size.id;
+             putCrDataObj.creativeFormat = $scope.popUpData.creativeFormat;
+             putCrDataObj.creativeType = $scope.popUpData.creativeType;
+             putCrDataObj.sslEnable = "true";
+             putCrDataObj.updatedAt= $scope.popUpData.updatedAt;
+             console.log("data after forming json:");
+             console.log(putCrDataObj);
+
+             workflowService.updateCreative($scope.popUpData.clientId ,$scope.popUpData.advertiserId,$scope.popUpData.id,putCrDataObj).then(function (result) {
+                    if (result.status === "OK" || result.status === "success") {
+                        console.log("creative updated");
+                        $scope.showViewTagPopup=false;
+                        $scope.edittrue=true;
+
+                    }
+                    else {
+                        $scope.IncorrectTag = true;
+                        $scope.incorrectTagMessage = "unable to update creative";
+                        console.log(result);
+                    }
+             });
+
+        }
+        $scope.editTrue=function(){
+            $scope.edittrue=false;
+        }
+
+        $scope.cancelPopup=function(){
+            $scope.showViewTagPopup=false;
+            $scope.edittrue=true;
+        }
+
     });
 
 })();
