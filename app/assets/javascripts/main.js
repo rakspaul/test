148bf5c689d7a59d5af13704a7861935b562ed74
jpatel19 <JPatel@JPATEL-MBP15.local>
2015-08-27 15:07:54 -0400

    Online help icon and functionality updates.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -133,6 +133,8 @@
 
       'controllers-campaign_details_controller':{deps: ['login-LoginController']},
       'controllers-campaign_create_controller':{deps: ['controllers-campaign_details_controller']},
+      'controllers-help_controller':{deps: ['controllers-campaign_details_controller']},
+      'controllers-campaign_overview_controller': {deps: ['controllers-help_controller']},
       'controllers-campaign_overview_controller': {deps: ['controllers-campaign_create_controller']},
       'controllers-campaign_adcreate_controller': {deps: ['controllers-campaign_overview_controller']},
       'controllers-creative_controller': {deps: ['controllers-campaign_adcreate_controller']},
@@ -314,6 +316,7 @@
 
 
       'controllers-campaign_details_controller':'controllers/campaign_details_controller',
+      'controllers-help_controller':'controllers/help_controller',
       'controllers-campaign_create_controller':'workflow/controllers/campaign_create_controller',
       'controllers-campaign_overview_controller':'workflow/controllers/campaign_overview_controller',
       'controllers-campaign_adcreate_controller':'workflow/controllers/campaign_adcreate_controller',
@@ -484,6 +487,7 @@
            'login-LoginController',
 
            'controllers-campaign_details_controller',
+           'controllers-help_controller',
            'controllers-campaign_create_controller',
            'controllers-campaign_overview_controller',
            'controllers-campaign_adcreate_controller',
