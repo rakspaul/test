dd8b314bd8cfd625949cc09b13c600ede84d5f21
Abhimanyu <abhimanyus@collective.com>
2015-09-10 13:01:10 +0530

    Cancel functionality is not working on geography targeting type window

--- app/assets/javascripts/campaignList/CampaignListModule.js
+++ app/assets/javascripts/campaignList/CampaignListModule.js
@@ -4,8 +4,12 @@ var campaignListModule = angular.module('campaignListModule', ['commonModule','u
 .config(function($routeProvider) {
     $routeProvider.when('/campaigns', {
       templateUrl: assets.html_campaign_list,
-      controller: 'campaignListController'
+      controller: 'campaignListController',
     })
+        $routeProvider.when('/campaigns/list', {
+            templateUrl: assets.html_campaign_list,
+            controller: 'campaignListController',
+        })
   })
 
 angular.module( 'ui.bootstrap.carousel' ,  [ 'ui.bootstrap.transition' ]) 
