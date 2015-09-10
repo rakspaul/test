4b1eab8a7024a7063204165a24bbaf29449c1031
Saudamini M <saudamini@techjini.com>
2015-09-10 17:36:20 +0530

    removed console

--- app/assets/javascripts/campaignList/CampaignListTableController.js
+++ app/assets/javascripts/campaignList/CampaignListTableController.js
@@ -3,7 +3,6 @@
 
     campaignListModule.controller('campaignListTableController', function($scope, dataService) {
         $scope.campaignData;
-        console.log("hello");
         fetchAllCampaigns();
 
         $scope.utc = function (date) {
@@ -12,8 +11,6 @@
          function fetchAllCampaigns(){
              dataService.fetch("http://dev-workflow002.ewr004.collective-media.net:9009/api/wf/v2/campaigns").then(function(res){
                  $scope.campaignData = res.data.data;
-                 console.log($scope.campaignData);
-
             });
 
         }
