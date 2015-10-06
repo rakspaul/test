4f02d2d3fd81aa62158b5876edc4f0c15f05cd13
Priya Harish <priyah@collective.com>
2015-10-06 16:16:56 +0530

    CW-1184  EDIT Campaign .

--- app/assets/javascripts/workflow/controllers/CampaignListController.js
+++ app/assets/javascripts/workflow/controllers/CampaignListController.js
@@ -3,6 +3,7 @@
 
     campaignListModule.controller('workFlowCampaignListController', function($scope, workflowService) {
         $(".main_navigation").find('.active').removeClass('active').end().find('#campaigns_nav_link').addClass('active');
+        localStorage.setItem('campaignData','');
 
         $scope.utc = function (date) {
             return moment(date).utc().valueOf()
@@ -15,6 +16,13 @@
         }
 
         fetchAllCampaigns();
+
+        $scope.editCampaign=function(campaign){  console.log(campaign);
+
+            window.location.href = '/campaign/'+campaign.id+'/edit';
+            localStorage.setItem('campaignData',JSON.stringify(campaign));
+            console.log(localStorage.getItem('campaignData'));
+        }
     });
 
 }());
