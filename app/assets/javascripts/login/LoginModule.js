ca84b9258a46d551b810cb79bad872cf0c8ee49d
Abhimanyu <abhimanyus@collective.com>
2015-10-09 12:40:11 +0530

    using network/workflow user common method for check

--- app/assets/javascripts/login/LoginModule.js
+++ app/assets/javascripts/login/LoginModule.js
@@ -1,10 +1,3 @@
 var loginModule = angular.module('loginModule', ['commonModule'])
 
-.config(function($routeProvider) {
-    $routeProvider.when('/login', {
-      templateUrl: assets.html_reports_login,
-      controller: 'loginController'
-    }).otherwise({redirectTo: 'campaigns'});
-  })
-
 
