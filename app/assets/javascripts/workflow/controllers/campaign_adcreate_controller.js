01e86f3f957177a61a80fe6615366899bd7e00a7
Shrujan Shetty <shrujan@Collectives-MacBook-Pro-4.local>
2015-10-28 15:32:28 +0530

    fixed bug

--- app/assets/javascripts/workflow/controllers/campaign_adcreate_controller.js
+++ app/assets/javascripts/workflow/controllers/campaign_adcreate_controller.js
@@ -1048,7 +1048,10 @@ var angObj = angObj || {};
         var dmasListArray = [];
         var regionListSortOrder = 'asc';
         var cityListSortOrder = 'asc';
-        var dmaListSortOrder = 'asc';
+        var dmaListSortOrder = 'asc'
+        var regionInitialLoad = false;
+        var cityInitialLoad = false;
+        var dmasInitialLoad = false;
         var storedResponse = {};
         $scope.isRegionSelected = true;
         $scope.citiesIncluded = true;
@@ -1075,7 +1078,7 @@ var angObj = angObj || {};
                     $scope.excludeSelectedItems();
 
                 $scope.adData.geoTargetingData = $scope.geoTargetingData.selected;
-                //$scope.listCities();
+                $scope.listCities();
 
             }
         }
@@ -1094,12 +1097,14 @@ var angObj = angObj || {};
                     if(index != -1)
                         $scope.sync(true, flatArr[index], 'cities')
                 })
-
+                cityInitialLoad = true;
+                $scope.selectedTab = 'regions';
+                $scope.showSwitch = true;
                 //// toggle switch based on region settings
-                //if(storedResponse.targets.geoTargets.CITY.isIncluded)
-                //    $scope.includeSelectedItems();
-                //else
-                //    $scope.excludeSelectedItems();
+                if(!storedResponse.targets.geoTargets.CITY.isIncluded)
+                    $scope.includeSelectedItems();
+                else
+                    $scope.excludeSelectedItems();
             }
         }
 
@@ -1116,6 +1121,7 @@ var angObj = angObj || {};
                     if(index != -1)
                         $scope.sync(true, flatArr[index], 'dmas')
                 })
+                dmasInitialLoad = true;
 
             }
 
@@ -1483,7 +1489,7 @@ var angObj = angObj || {};
                      return item.id;
                  });
 
-                if($scope.mode === 'edit'){
+                if($scope.mode === 'edit' && !dmasInitialLoad){
                     $scope.dmasEdit(flatArr);
                 }
 
@@ -1556,7 +1562,7 @@ var angObj = angObj || {};
                     return item.id;
                 });
 
-                if($scope.mode === 'edit' ){
+                if($scope.mode === 'edit' && !cityInitialLoad){
                     $scope.cityEdit(flatArr);
                     $scope.listDmas();
                 }
