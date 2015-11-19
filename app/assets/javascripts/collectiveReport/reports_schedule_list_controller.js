9d4adcf15b1d67fdd4c8a55a90091a668cf55ab1
Sapna <sapna@techjini.com>
2015-11-19 18:52:21 +0530

    sort frequency - view all report

--- app/assets/javascripts/collectiveReport/reports_schedule_list_controller.js
+++ app/assets/javascripts/collectiveReport/reports_schedule_list_controller.js
@@ -1,7 +1,11 @@
 
 (function() {
     'use strict';
-    collectiveReportModule.controller('ReportsScheduleListController', function(loginModel,collectiveReportModel, $scope, $modal, domainReports, dataService, urlService,campaignSelectModel,constants, $filter,dataStore , $timeout,utils ) {
+    collectiveReportModule.controller('ReportsScheduleListController', function($scope,$timeout,$filter,collectiveReportModel) {
+
+        $scope.noOfSchldInstToShow = 3;
+        $scope.scheduleInstCount = [];
+        $scope.sort = {descending:true};
 
         //close messages in 3 seconds
         $scope.timeoutReset = function(){
@@ -13,6 +17,22 @@
 
         }
 
+        var scheduleReportListSucc = function(schdReportList) {
+           console.log('succesfully got data');
+            $scope.schdReportList = schdReportList;
+            $scope.sortSchdlReport();
+            for(var i = 0 ; i < $scope.schdReportList.length;i++){
+                $scope.scheduleInstCount[i] = $scope.noOfSchldInstToShow;
+            }
+            console.log('-----');
+            console.log($scope.schdReportList);
+        }
+
+        var scheduleReportListError = function() {
+            console.log('error occured');
+        }
+
+        collectiveReportModel.getScheduleReportList(scheduleReportListSucc,scheduleReportListError);
 
 
         $scope.close_msg_box = function(event) {
@@ -20,13 +40,26 @@
             elem.closest(".top_message_box").hide() ;
             $scope.flashMessage = {'message':'','isErrorMsg':''};
         };
-         $scope.open_second_dimension = function(event) {
+         $scope.open_second_dimension = function(event,index) {
             var elem = $(event.target);
+             if(!elem.closest(".row").hasClass("open")) {
+                 $scope.scheduleInstCount[index] = $scope.noOfSchldInstToShow;
+             }
             elem.closest(".row").toggleClass("open") ;
             elem.closest(".row").find(".inner-row").toggle() ;
             
         };
 
+        $scope.setScheduleInstCount = function(index,count) {
+            $scope.scheduleInstCount[index] = count;
+            console.log('index:',index);
+            console.log('count:',count);
+        }
+
+        $scope.sortSchdlReport = function() {
+            $scope.schdReportList = $filter('orderBy')($scope.schdReportList, 'frequency',$scope.sort.descending);
+            $scope.sort.descending = !$scope.sort.descending;
+        }
 
         //$scope.sortReport($scope.sort.column);
 
