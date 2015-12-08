46753c5b2c464364820e313dfe7f20705e406131
Shrujan Shetty <shrujan@Collectives-MacBook-Pro-4.local>
2015-12-08 15:04:16 +0530

    fixed custim fields

--- app/assets/javascripts/workflow/controllers/campaign_adcreate_controller.js
+++ app/assets/javascripts/workflow/controllers/campaign_adcreate_controller.js
@@ -916,6 +916,9 @@ var angObj = angObj || {};
                     $scope.postPlatformDataObj = $scope.workflowData['adsData'].adPlatformCustomInputs;
                 }
             }
+            if($scope.mode == 'edit')
+                localStorage.setItem('adPlatformCustomInputs', JSON.stringify($scope.postPlatformDataObj));
+
         };
 
 
