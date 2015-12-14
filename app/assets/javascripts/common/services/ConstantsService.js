92011f1faa5cb5a609cd7874189237a70619d002
sanjsrik <ssrikonda@collective.com>
2015-12-14 08:32:51 -0500

    Added space in the WF_INVALID_CREATIVE_TAG...
    Added a space after the original error message in Add Creative Tag validation message:
    
    You have entered an invalid Javascript tag.Please review carefully and try again
    Now reads:
    
    You have entered an invalid Javascript tag. Please review carefully and try again
    
    Also updated the following information message:
    
    This tag already exists in your Creative Tag Library. Save a Creative anyway?
    
    TO
    
    This tag already exists in your Creative Tag Library. Save the Creative anyway?

--- app/assets/javascripts/common/services/ConstantsService.js
+++ app/assets/javascripts/common/services/ConstantsService.js
@@ -386,9 +386,9 @@
     this.YESTERDAY = "Yesterday";
     this.WF_NOT_SET="Not Set";
 
-    this.WF_CREATIVE_TAG_ALREADY_EXISTS = "This tag already exists in your Creative Tag Library. Save a Creative anyway?";
+    this.WF_CREATIVE_TAG_ALREADY_EXISTS = "This tag already exists in your Creative Tag Library. Save the Creative anyway?";
     this.WF_DUPLICATE_TAG = 'Duplicate Tag';
-    this.WF_INVALID_CREATIVE_TAG= 'You have entered an invalid Javascript tag.Please review carefully and try again';
+    this.WF_INVALID_CREATIVE_TAG= 'You have entered an invalid Javascript tag. Please review carefully and try again.';
     this.WF_INVALID_CREATIVE_TAG_TRACKER="Please include the %%TRACKER%% macro to save the tag";
     this.WF_CREATIVE_TAG_UPDATE_ERROR = 'Unable to update creative';
     this.WF_CREATIVE_FORCESAVE = 'Unable to forceSave creative';
