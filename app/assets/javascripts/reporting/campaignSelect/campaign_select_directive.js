61c4629184237f30cbe0a80a1729e6e5220d14c9
Saudamini M <saudamini@techjini.com>
2016-01-05 15:54:45 +0530

    Campaign name is not highlighted in the campaign drop down

--- app/assets/javascripts/reporting/campaignSelect/campaign_select_directive.js
+++ app/assets/javascripts/reporting/campaignSelect/campaign_select_directive.js
@@ -44,10 +44,10 @@
                          if(inputValue) {
                             $('#campaignDropdown').attr('placeholder', inputValue);
                             $('#campaignDropdown').val('');
-                            $scope.selectedObj.name = inputValue;
-                            if($scope.allCampaign == "true") {
+                            //if($scope.allCampaign == "true") {
                                 $('#campaign_name_selected').val(inputValue);
-                             }
+                                $scope.selectedObj.name = inputValue;
+                             //}
                          }
                     } else {
                         var target = $(event.target);
