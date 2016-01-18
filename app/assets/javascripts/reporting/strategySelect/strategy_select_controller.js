237f033e1fede7f75fa7ae3a679033f521472611
Abhimanyu <abhimanyus@collective.com>
2016-01-18 15:32:56 +0530

    showing platform data and other minor issues

--- app/assets/javascripts/reporting/strategySelect/strategy_select_controller.js
+++ app/assets/javascripts/reporting/strategySelect/strategy_select_controller.js
@@ -2,69 +2,73 @@
     'use strict';
     strategySelectModule.controller('StrategySelectController', function ($scope, $rootScope , campaignSelectModel ,strategySelectModel ,apiPaths, constants , loginModel, analytics,utils ) {
 
-        var loadStrategies = true;
-
         $scope.strategyData = {
-            strategies: [angular.copy(constants.ALL_STRATEGIES_OBJECT.clone)],
-            selectedStrategy: angular.copy(constants.ALL_STRATEGIES_OBJECT)
+            strategies : {},
+            selectedStrategy :  {
+                id: -1,
+                name : 'Loading...'
+            }
         };
-
         $scope.$parent.strategyLoading =  true;
+        //$scope.$parent.isFetchStrategiesCalled = false;
         $scope.isStrategyDropDownShow = true;
-        $scope.$parent.isStrategyDropDownShow = true;
 
 
         // On this event, only fetch list of strategyies and retain selectedStrategy (done from outside).
         var eventCampaignStrategyChangedFunc =  $rootScope.$on(constants.EVENT_CAMPAIGN_STRATEGY_CHANGED, function() {
-            $scope.reset();
-            $scope.setStrategy($scope.strategyData.selectedStrategy);
+            strategySelectModel.getStrategyObj().strategies = {} ;// reset strategy list
+            $scope.fetchStrategies();// fetch strategies
         });
 
-        $scope.reset = function() {
-            loadStrategies = true;
-            $scope.isStrategyDropDownShow = true;
+        $scope.reset= function(){
             strategySelectModel.reset();// clean up models
             $scope.strategyData.strategies = strategySelectModel.getStrategyObj().strategies ;
             $scope.strategyData.selectedStrategy = strategySelectModel.getStrategyObj().selectedStrategy ;
             $scope.$watch(function(scope) { return $scope.strategyData.selectedStrategy });
         };
-
         $scope.add_active_to_strategy = function() {
             $(".dropdown_type1_holder").removeClass("active");
             $(".dropdown_type2").addClass("active") ;
         };
 
-        $scope.setStrategy = function(strategy) {
+        $scope.setStrategy = function(strategy){
             strategySelectModel.setSelectedStrategy(strategy);
-            $scope.strategyData.selectedStrategy.id = strategy.id;
-            $scope.strategyData.selectedStrategy.name = strategy.name;
+            if (strategySelectModel.getStrategyCount() === 1)  {
+                $scope.$parent.isStrategyDropDownShow = false;
+            } else {
+                $scope.$parent.isStrategyDropDownShow = true;
+            }
+            $scope.strategyData.selectedStrategy.id =(strategy.id == undefined) ? (strategy.lineitemId == undefined ? strategy.strategyId : strategy.lineitemId): strategy.id ;
+            $scope.strategyData.selectedStrategy.name = (strategy.name == undefined) ? strategy.strategy_name  : strategy.name ;
             $rootScope.$broadcast(constants.EVENT_STRATEGY_CHANGED, strategy);
             if($scope.strategyData.selectedStrategy.id !== -1 && $scope.strategyData.selectedStrategy.id !== -99)
                 $scope.$parent.strategyLoading = false;
         };
 
-        $scope.setStrategy($scope.strategyData.selectedStrategy);
 
-        function fetchStrategies() {
-            if(campaignSelectModel.getSelectedCampaign().id == -1) {
-              return;
-            }
-            if(loadStrategies) {
-                loadStrategies = false;
-                strategySelectModel.getStrategies(campaignSelectModel.getSelectedCampaign().id).then(function(result) {
+        $scope.fetchStrategies = function(){
+            $scope.isStrategyDropDownShow = false;
+            if(campaignSelectModel.getSelectedCampaign().id != -1){
+                strategySelectModel.getStrategies(campaignSelectModel.getSelectedCampaign().id).then(function(result){
                     var strategyObj = strategySelectModel.getStrategyObj();
-                    $scope.strategyData.strategies = (strategyObj.strategies == undefined ? angular.copy(constants.ALL_STRATEGIES_OBJECT) : strategyObj.strategies);
+                    $scope.strategyData.strategies = (strategyObj.strategies == undefined)? {} : strategyObj.strategies ;
                     $scope.setStrategy(strategyObj.selectedStrategy);
                 });
+            } else {
+                //$scope.$parent.isFetchStrategiesCalled =  false;
+                $scope.$parent.strategyLoading = true;
+                $scope.strategyData.strategies = {};
+                $scope.strategyData.selectedStrategy.id = -1 ;
+                $scope.strategyData.selectedStrategy.name = constants.NO_ADGROUPS_FOUND ;
+
             }
         };
 
-        //Function called when the user clicks on the strategy dropdown
-        $scope.strategiesDropdownClicked = function() {
-            $scope.$parent.strategyLoading = true;
-            fetchStrategies();
-        };
+        //if(!$scope.$parent.isFetchStrategiesCalled) {
+        $scope.fetchStrategies();
+        //}
 
+        //Function called when the user clicks on the strategy dropdown
         $('#strategies_list').click(function (e) {
             $scope.$parent.strategyLoading = true;
             var selectedStrategy = {
@@ -72,17 +76,13 @@
                 name:  $(e.target).text()
             };
             $scope.setStrategy(selectedStrategy);
+
         });
 
         $scope.$on('$destroy', function() {
             eventCampaignStrategyChangedFunc();
         });
 
-        $scope.$on(constants.EVENT_CAMPAIGN_CHANGED, function() {
-            $scope.reset();
-            $scope.setStrategy($scope.strategyData.selectedStrategy);
-        });
-
-
     });
 }());
+
