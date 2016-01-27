40819ecd0b80d7af17c2fe4a540af90c0add4484
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2016-01-27 11:05:33 +0530

    minor changes

--- app/assets/javascripts/workflow/controllers/creative_controller.js
+++ app/assets/javascripts/workflow/controllers/creative_controller.js
@@ -3,9 +3,49 @@ var angObj = angObj || {};
     'use strict';
     angObj.controller('CreativeController', function ($scope,$rootScope, $window, $routeParams, constants,
                                                       workflowService, $timeout, utils, $location) {
+
+        var creatives = {
+            /*Function to get creatives sizes*/
+            getCreativeSizes: function () {
+                var responseData;
+
+                workflowService
+                    .getCreativeSizes()
+                    .then(function (result) {
+                        if (result.status === 'OK' || result.status === 'success') {
+                            responseData = result.data.data;
+                            $scope.creativeSizeData.creativeSize = responseData;
+                        } else {
+                            console.log('failed to get creatives sizes');
+                            creatives.errorHandler(result);
+                        }
+                    }, creatives.errorHandler);
+            },
+
+            fetchAdFormats: function () {
+                $scope.creativeSizeData.adFormats = [
+                    {id: 1, name: 'Display',    active: true},
+                    {id: 2, name: 'Video',      active: false},
+                    {id: 3, name: 'Rich Media', active: true},
+                    {id: 4, name: 'Social',     active: false}
+                ];
+                //default value
+                $scope.adData.adFormat = 'Display';
+            },
+
+            errorHandler: function (errData) {
+                console.log(errData);
+            }
+        };
+
         if ($location.path() === '/creative/add') {
             $scope.isAddCreativePopup = true;
-            $('.main_navigation').find('.active').removeClass('active').end().find('#creative_nav_link').addClass('active');
+            $('.main_navigation')
+                .find('.active')
+                .removeClass('active')
+                .end()
+                .find('#creative_nav_link')
+                .addClass('active');
         }
 
         $scope.textConstants = constants;
@@ -19,6 +59,7 @@ var angObj = angObj || {};
         $scope.IncorrectTag = false;
         $scope.disableCancelSave = false;
         $scope.campaignId = $routeParams.campaignId;
+
         /*AD Format Type*/
         $scope.formatLabel = 'Display';
         if ($scope.campaignId) {
@@ -26,6 +67,7 @@ var angObj = angObj || {};
         } else {
             $scope.adPage = false;
         }
+
         $scope.$on('adFormatChanged', function (event, adType) {
             $scope.formatLabel = adType;
         });
@@ -41,47 +83,17 @@ var angObj = angObj || {};
         $scope.resetAlertMessage = function () {
             $rootScope.setErrAlertMessage('', 0);
         };
-        
-        var creatives = {
-            /*Function to get creatives sizes*/
-            getCreativeSizes: function () {
-                workflowService.getCreativeSizes().then(function (result) {
-                    if (result.status === 'OK' || result.status === 'success') {
-                        var responseData = result.data.data;
-                        $scope.creativeSizeData.creativeSize = responseData;
-                    }
-                    else {
-                        console.log('failed to get creatives sizes');
-                        creatives.errorHandler(result);
-                    }
-                }, creatives.errorHandler);
-            },
 
-            fetchAdFormats: function () {
-                $scope.creativeSizeData.adFormats = [
-                    {id: 1, name: 'Display', active: true},
-                    {id: 2,name: 'Video',active: false},
-                    {id: 3, name: 'Rich Media', active: true},
-                    {id: 4, name: 'Social', active: false}
-                ];
-                //default value
-                $scope.adData.adFormat = 'Display';
-            },
-
-            errorHandler: function (errData) {
-                console.log(errData);
-            }
-        };
-  
         $scope.prarentHandler = function (clientId, clientName, advertiserId, advertiserName) {
-            $scope.campaignId = clientId;
-            $scope.advertiserId = advertiserId;
             var campaignData = {
                 'advertiserId': advertiserId,
                 'advertiserName': advertiserName,
                 'clientId': clientId,
                 'clientName': clientName
             };
+
+            $scope.campaignId = clientId;
+            $scope.advertiserId = advertiserId;
             localStorage.setItem('campaignData', JSON.stringify(campaignData));
             creatives.getCreativeSizes(clientId, advertiserId);
             creatives.fetchAdFormats();
@@ -93,7 +105,8 @@ var angObj = angObj || {};
                     formData,
                     PatternOutside,
                     PatternInside,
-                    tagLower;
+                    tagLower,
+                    postCrDataObj;
 
                 $scope.$broadcast('show-errors-check-validity');
                 if ($scope.$parent.TrackingIntegrationsSelected) {
@@ -102,7 +115,7 @@ var angObj = angObj || {};
                     formData = _.object(_.pluck(formData, 'name'), _.pluck(formData, 'value'));
                     // if (formData.name && formData.creativeFormat && formData.creativeSize) {
                     if (formData.name && formData.creativeSize) {
-                        var postCrDataObj = {};
+                        postCrDataObj = {};
                         postCrDataObj.name = formData.name;
                         postCrDataObj.tag = '%%TRACKER%%';
                         postCrDataObj.sizeId = formData.creativeSize;
@@ -120,7 +133,7 @@ var angObj = angObj || {};
                     }
                 } else {
                     if ($scope.formCreativeCreate.$valid) {
-                        formElem = $('#formCreativeCreate'),
+                        formElem = $('#formCreativeCreate');
                         formData = formElem.serializeArray();
                         formData = _.object(_.pluck(formData, 'name'), _.pluck(formData, 'value'));
                         PatternOutside = new RegExp(/<script.*>.*(https:).*<\/script>.*/);
@@ -128,14 +141,14 @@ var angObj = angObj || {};
                         tagLower = formData.tag.toLowerCase().replace(' ', '').replace(/(\r\n|\n|\r)/gm, '');
                         if (tagLower.match(PatternOutside)) {
                             if ( (tagLower.indexOf('%%tracker%%') > -1)) {
-                                $scope.creativesave(formData);
+                                $scope.creativeSave(formData);
                             } else {
                                 $scope.IncorrectTag = true;
                                 $scope.incorrectTagMessage = $scope.textConstants.WF_INVALID_CREATIVE_TAG_TRACKER;
                             }
                         } else if (tagLower.match(PatternInside)) {
                             if ( (tagLower.indexOf('%%tracker%%') > -1)) {
-                                $scope.creativesave(formData);
+                                $scope.creativeSave(formData);
                             } else{
                                 $scope.IncorrectTag = true;
                                 $scope.incorrectTagMessage = $scope.textConstants.WF_INVALID_CREATIVE_TAG_TRACKER;
@@ -151,7 +164,7 @@ var angObj = angObj || {};
             });
         });
 
-        $scope.creativesave = function (formData) {
+        $scope.creativeSave = function (formData) {
             var postCrDataObj = {};
 
             $scope.IncorrectTag = false;
@@ -159,31 +172,36 @@ var angObj = angObj || {};
             postCrDataObj.tag = formData.tag;
             postCrDataObj.sizeId = formData.creativeSize;
             if ($scope.campaignId) {
-                postCrDataObj.creativeFormat=$scope.formatLabel.replace(/\s+/g, '').toUpperCase();
+                postCrDataObj.creativeFormat = $scope.formatLabel.replace(/\s+/g, '').toUpperCase();
             } else {
-                postCrDataObj.creativeFormat = formData.creativeFormat && formData.creativeFormat.replace(/\s+/g, '').toUpperCase();
+                postCrDataObj.creativeFormat =
+                    formData.creativeFormat && formData.creativeFormat.replace(/\s+/g, '').toUpperCase();
             }
             postCrDataObj.creativeType = formData.creativeType;
             postCrDataObj.sslEnable = 'true';
             $scope.CrDataObj = postCrDataObj;
-            workflowService.saveCreatives($scope.campaignId, $scope.advertiserId, postCrDataObj).then(function (result) {
-                if (result.status === 'OK' || result.status === 'success') {
-                    $scope.addedSuccessfully = true;
-                    $scope.Message = 'Creative Added Successfully';
-                    workflowService.setNewCreative(result.data.data);
-                    $scope.cancelBtn();// redirect user after successful saving
-                    $rootScope.setErrAlertMessage($scope.textConstants.CREATIVE_SAVE_SUCCESS,0);
-                    localStorage.setItem( 'topAlertMessage', $scope.textConstants.CREATIVE_SAVE_SUCCESS);
-                } else if (result.data.data.message = 'Creative with this tag already exists. If you still want to save, use force save') {
-                    $('.popup-holder').css('display', 'block');
-                    $scope.addedSuccessfully = false;
-                    $scope.disableCancelSave = true;
-                } else {
-                    $scope.addedSuccessfully = true;
-                    $scope.Message = 'Unable to create Creatives';
-                    console.log(result);
-                }
-            });
+            workflowService
+                .saveCreatives($scope.campaignId, $scope.advertiserId, postCrDataObj)
+                .then(function (result) {
+                    if (result.status === 'OK' || result.status === 'success') {
+                        $scope.addedSuccessfully = true;
+                        $scope.Message = 'Creative Added Successfully';
+                        workflowService.setNewCreative(result.data.data);
+                        // redirect user after successful saving
+                        $scope.cancelBtn();
+                        $rootScope.setErrAlertMessage($scope.textConstants.CREATIVE_SAVE_SUCCESS,0);
+                        localStorage.setItem( 'topAlertMessage', $scope.textConstants.CREATIVE_SAVE_SUCCESS);
+                    } else if (result.data.data.message ===
+                        'Creative with this tag already exists. If you still want to save, use force save') {
+                        $('.popup-holder').css('display', 'block');
+                        $scope.addedSuccessfully = false;
+                        $scope.disableCancelSave = true;
+                    } else {
+                        $scope.addedSuccessfully = true;
+                        $scope.Message = 'Unable to create Creatives';
+                        console.log(result);
+                    }
+                });
         };
 
         $scope.$on('closeAddCreativePage', function () {
@@ -196,19 +214,25 @@ var angObj = angObj || {};
             if ($location.path() === '/creative/add') {
                 $location.url('/creative/list');
             } else {
-                $('.newCreativeSlide .popCreativeLib').delay(300).animate({
-                    left: '100%',
-                    marginLeft: '0px'
-                }, 'slow', function () {
-                    $(this).hide();
-                });
-                $('#creative').delay(300).animate({height: 'auto'}, 'slow');
+                $('.newCreativeSlide .popCreativeLib')
+                    .delay(300)
+                    .animate({
+                        left: '100%',
+                        marginLeft: '0px'
+                    }, 'slow', function () {
+                        $(this).hide();
+                    });
+                $('#creative')
+                    .delay(300)
+                    .animate({
+                        height: 'auto'
+                    }, 'slow');
             }
         });
 
         $scope.cancelBtn = function () {
-            $scope.$broadcast('closeAddCreativePage');
             var winHeight = $(window).height() - 126;
+            $scope.$broadcast('closeAddCreativePage');
             $('.adStepOne .tab-pane').css('min-height', winHeight-30+'px');
         };
 
@@ -217,22 +241,28 @@ var angObj = angObj || {};
         };
 
         $scope.saveDuplicate = function () {
-            workflowService.forceSaveCreatives($scope.campaignId, $scope.advertiserId, $scope.CrDataObj).then(function (result) {
-                if (result.status === 'OK' || result.status === 'success') {
-                    $('.popup-holder').css('display', 'none');
-                    $scope.disableCancelSave = false;
-                    workflowService.setNewCreative(result.data.data);
-                    $scope.cancelBtn();
-                } else {
-                    $scope.addedSuccessfully = true;
-                    $scope.Message = 'Unable to create Creatives';
-                }
-            });
-        }
-;
+            workflowService
+                .forceSaveCreatives($scope.campaignId, $scope.advertiserId, $scope.CrDataObj)
+                .then(function (result) {
+                    if (result.status === 'OK' || result.status === 'success') {
+                        $('.popup-holder').css('display', 'none');
+                        $scope.disableCancelSave = false;
+                        workflowService.setNewCreative(result.data.data);
+                        $scope.cancelBtn();
+                    } else {
+                        $scope.addedSuccessfully = true;
+                        $scope.Message = 'Unable to create Creatives';
+                    }
+                });
+        };
+
         $scope.dropBoxItemSelected =  function (item, type, event) {
-            $('#listBoxSize').parents('.dropdown').removeClass('open');
-            $('[name="creativeFormat"]').prev('.dropdown').removeClass('open');
+            $('#listBoxSize')
+                .parents('.dropdown')
+                .removeClass('open');
+            $('[name="creativeFormat"]')
+                .prev('.dropdown')
+                .removeClass('open');
             $scope.adData[type] = item;
             $scope.newData[type]='';
 
@@ -242,33 +272,46 @@ var angObj = angObj || {};
             $('.popup-holder').css('display', 'none');
             $scope.addedSuccessfully = true;
             $scope.Message = 'Unable to create Creatives';
-            /*enable cancel, save button on cancel duplicate*/
+            // enable cancel, save button on cancel duplicate
             $scope.disableCancelSave = false;
 
         };
 
         $scope.toggleBtn = function (event) {
-            var target = $(event.target);
-            var parentElem =  target.parents('.miniToggle');
-            parentElem.find('label').removeClass('active');
+            var target = $(event.target),
+                parentElem =  target.parents('.miniToggle');
+
+            parentElem
+                .find('label')
+                .removeClass('active');
             target.parent().addClass('active');
             target.attr('checked', 'checked');
         };
 
         $('.dropdown-menu li a').click(function () {
             var selText = $(this).text();
-            $(this).parents('.btn-group').find('.dropdown-toggle').html('<span>'+selText+'</span> <span class="caret"></span>');
+
+            $(this)
+                .parents('.btn-group')
+                .find('.dropdown-toggle')
+                .html('<span>' + selText + '</span> <span class="caret"></span>');
         });
         
         // DDL Search Input Prevent Default
-        $('.dropdown-menu').find('input').click(function (e) {
-            e.stopPropagation();
-        });
+        $('.dropdown-menu')
+            .find('input')
+            .click(function (e) {
+                e.stopPropagation();
+            });
         
         // DDL with Search
         $('.dropdown-menu-search li a').click(function () {
             var selText = $(this).text();
-            $(this).parents('.btn-group').find('.dropdown-toggle-search').attr('value='+selText);
+
+            $(this)
+                .parents('.btn-group')
+                .find('.dropdown-toggle-search')
+                .attr('value=' + selText);
         });
     });
 })();
