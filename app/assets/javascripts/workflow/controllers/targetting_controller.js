168564a8cd2c1ec165584ebe47e7877b66c3d220
Abhimanyu <abhimanyus@collective.com>
2016-02-16 00:27:16 +0530

    fixed day part issue

--- app/assets/javascripts/workflow/controllers/targetting_controller.js
+++ app/assets/javascripts/workflow/controllers/targetting_controller.js
@@ -51,7 +51,7 @@ var angObj = angObj || {};
             }
 
             if (targetingName === 'Daypart') {
-                if(fetchedObj && fetchedObj.targets.adDaypartTargets && $scope.mode === 'edit') {
+                if(fetchedObj && fetchedObj.targets.adDaypartTargets && _.size(fetchedObj.targets.adDaypartTargets) > 0 && $scope.mode === 'edit') {
                     $timeout(function() {
                         $scope.$broadcast("updateDayPart", true);
                     }, 2000)
