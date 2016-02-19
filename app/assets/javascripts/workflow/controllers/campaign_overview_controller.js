63fa01ba3e5e5c84fe2e1ea111219833f7bded00
Shrujan Shetty <shrujan@Collectives-MacBook-Pro-6.local>
2016-02-19 11:44:54 +0530

    changed campaign overview page controller scope variables

--- app/assets/javascripts/workflow/controllers/campaign_overview_controller.js
+++ app/assets/javascripts/workflow/controllers/campaign_overview_controller.js
@@ -7,35 +7,34 @@ var angObj = angObj || {};
         $(".main_navigation_holder").find('.active_tab').removeClass('active_tab') ;
         $(".main_navigation").find('.active').removeClass('active').end().find('#campaigns_nav_link').addClass('active');
         $(".bodyWrap").addClass('bodyWrapOverview');
-        if( $('.adGroupSelectionWrap').length ) { $("html").css({'background-color':'#eef5fc'}); };
+        if( $('.adGroupSelectionWrap').length ) { $("html").css({'background-color':'#eef5fc'}); }
+        var sizeString = "";
         $scope.textConstants = constants;
         $scope.workflowData = {};
-        $scope.workflowData['getADsForGroupData'] = {}
+        $scope.workflowData.getADsForGroupData = {};
         $scope.disablePushBtn = true;
-        $scope.notPushed = false;
-        $scope.sizeString = "";
+        //$scope.notPushed = false; // this is not used anywhere
         $scope.showHideToggle = false;
         $scope.showIndividualAds = false;
         $scope.showCreateAdGrp=false;
-        $scope.createGroupMessage=false;
         $scope.createGroupMessage=false;
         $scope.showPushAdsLoader = false;
         $scope.brand=[];
         $scope.performance=[];
         $scope.redirectFlag = false;
         localStorage.setItem('campaignData','');
-        $scope.moreThenThree = '';
+        //$scope.moreThenThree = '';// not used
         $scope.campaignArchiveLoader = false;
         $scope.editCampaign=function(workflowcampaignData){
             $location.url('/mediaplan/'+workflowcampaignData.id+'/edit');
-        }
+        };
         $scope.utcToLocalTime=function(date, format){
             return momentService.utcToLocalTime(date,format);
-        }
+        };
         $scope.resetAlertMessage = function(){
             localStorage.removeItem('topAlertMessage');
             $rootScope.setErrAlertMessage("",0);
-        }
+        };
 
         //show selected targets in ads card
         $scope.displaySelectedTargets = function (adsData) {
@@ -50,27 +49,26 @@ var angObj = angObj || {};
                 }
 
                 if ((adsData.targets.segmentTargets.segmentList && adsData.targets.segmentTargets.segmentList.length > 0)) {
-                    if(selectedStr != ''){
+                    if(selectedStr !== ''){
                         selectedStr += ', Audience';
                     }
                     else{
                         selectedStr += 'Audience';
-
                     }
                 }
 
                 if (adsData.targets.adDaypartTargets.schedule && adsData.targets.adDaypartTargets.schedule.length > 0) {
-                    if(selectedStr != ''){
+                    if(selectedStr !== ''){
                         selectedStr += ', Daypart';
                     }
                     else{
                         selectedStr += 'Daypart';
-
                     }
                 }
 
-                if(selectedStr == '')
+                if(selectedStr === ''){
                     selectedStr = constants.WF_NOT_SET;
+                }
             }
 
             return selectedStr;
@@ -344,14 +342,14 @@ var angObj = angObj || {};
             var creativeSizeArr = []
             if (typeof creative != 'undefined' && creative.length>0) {
                 if (creative.length == 1) {
-                    $scope.sizeString = creative[0].size.size;
+                    sizeString = creative[0].size.size;
                 } else if (creative.length > 1) {
-                    $scope.sizeString = "";
+                    sizeString = "";
                     for (var i in creative) {
-                        //$scope.sizeString += creative[i].size.size + ", ";
+                        //sizeString += creative[i].size.size + ", ";
                         creativeSizeArr.push(creative[i].size.size)
                     }
-                    $scope.sizeString = creativeSizeArr;
+                    sizeString = creativeSizeArr;
                     var arr = creativeSizeArr;
                     var result = noRepeat(arr);
 
@@ -359,16 +357,16 @@ var angObj = angObj || {};
                         var creativeSizeLimit = result[0].splice(0,3);
                         var remainingCreativeSize = result[0].join(", ");
                         var amountLeft = result[0].length;
-                        $scope.sizeString = creativeSizeLimit.join(', ').replace(/X/g, 'x')  + ' <span class="blueTxt" title="'+remainingCreativeSize+'" >+' + amountLeft + '</span>';
+                        sizeString = creativeSizeLimit.join(', ').replace(/X/g, 'x')  + ' <span class="blueTxt" title="'+remainingCreativeSize+'" >+' + amountLeft + '</span>';
 
                     }
                     else {
-                        $scope.sizeString = result[0] && result[0].join(', ');
+                        sizeString = result[0] && result[0].join(', ');
                     }
                 }
 
             } else {
-                $scope.sizeString = constants.WF_NOT_SET;
+                sizeString = constants.WF_NOT_SET;
             }
 
 
@@ -390,7 +388,7 @@ var angObj = angObj || {};
                 return [a, b];
             }
 
-            return $scope.sizeString;
+            return sizeString;
         }
 
 
