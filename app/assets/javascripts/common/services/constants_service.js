7c4427749ca34e4cd95c996bb1513b9c70f39d99
Jayesh Patel <jpatel@collective.com>
2016-02-23 16:40:41 -0500

    Text updates

--- app/assets/javascripts/common/services/constants_service.js
+++ app/assets/javascripts/common/services/constants_service.js
@@ -549,7 +549,7 @@
         this.WF_PAUSE="Pause";
         this.WF_RESUME="Resume";
         this.WF_MOVE_TO="Move to"
-        this.WF_COPY_TO="Copy to"
+        this.WF_CLONE="Clone"
         this.WF_ARCHIVE="Archive"
         this.WF_NOT_SET = "Not Set"
         this.WF_AD_SAVE_CLOSE = "Save & Close";
