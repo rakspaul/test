d4185bdae993025d571a64157977ff31ad7fbbbf
Rupan Paul <rupanpaul99@gmail.com>
2016-02-29 23:50:31 +0530

    Adding define to the common/directive folder.

--- angularAMD-sample/app/scripts/common/directives/data_not_found.js
+++ angularAMD-sample/app/scripts/common/directives/data_not_found.js
@@ -1,9 +1,9 @@
-(function(){
-  'use strict';
+define(['angularAMD'],function (angularAMD) {
+    'use strict';
   //This is a common directive to handle no data scenarios
   //TODO: Had planned to define directive dataNotFound. But, when I define that the directive is not rendering (<data-not-found>)
   //So, defined it as notFound.
-  commonModule.directive('notFound',function(){
+    angularAMD.directive('notFound',function(){
       return{
         restrict:'EAC',
         templateUrl:'/assets/html/data_not_found.html',
@@ -13,4 +13,4 @@
         }
       }
   });
-}());
+});
