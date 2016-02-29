6a767f75414c7cd6a6692abca6fc9bee6c8d3608
Rupan Paul <rupanpaul99@gmail.com>
2016-02-29 17:17:57 +0530

    Adding define(function(){...}).....

--- angularAMD-sample/app/scripts/reporting/controllers/inventory_controller.js
+++ angularAMD-sample/app/scripts/reporting/controllers/inventory_controller.js
@@ -2,15 +2,16 @@ var angObj = angObj || {};
 define(['angularAMD','reporting/kpiSelect/kpi_select_model', 'reporting/campaignSelect/campaign_select_model', 'reporting/strategySelect/strategy_select_model',
         'reporting/common/charts/column_line', 'common/services/data_service', 'common/services/constants_service',
         'reporting/timePeriod/time_period_model', 'login/login_model', 'reporting/advertiser/advertiser_model',
-        'reporting/brands/brands_model', 'common/services/url_service'
+        'reporting/brands/brands_model', 'common/services/url_service', 'common/services/analytics_service',
+        'reporting/models/domain_reports'
     ],
 
     function (angularAMD) {  'use strict';
-        angularAMD.controller('InventoryController', function (kpiSelectModel, campaignSelectModel, strategySelectModel,
+        angularAMD.controller('InventoryController', function ($scope, kpiSelectModel, campaignSelectModel, strategySelectModel,
                                                                columnline, dataService, constants,
                                                                timePeriodModel, loginModel, advertiserModel,
                                                                brandsModel, urlService, analytics,
-                                                               $scope, domainReports) {
+                                                               domainReports) {
 
       $scope.textConstants = constants;
 
--- angularAMD-sample/app/scripts/reporting/controllers/viewability_controller.js
+++ angularAMD-sample/app/scripts/reporting/controllers/viewability_controller.js
@@ -1,7 +1,16 @@
 var angObj = angObj || {};
-(function () {
+define(['angularAMD','reporting/kpiSelect/kpi_select_model', 'reporting/campaignSelect/campaign_select_model', 'reporting/strategySelect/strategy_select_model',
+        'common/services/data_service', 'reporting/models/domain_reports', 'common/services/constants_service',
+        'reporting/timePeriod/time_period_model', 'login/login_model', 'common/services/url_service',
+        'common/services/analytics_service', 'reporting/advertiser/advertiser_model', 'reporting/brands/brands_model'
+    ],
+
+    function (angularAMD) {
     'use strict';
-    angObj.controller('ViewabilityController', function ($scope, campaignSelectModel,kpiSelectModel, strategySelectModel, dataService, domainReports, constants, timePeriodModel, loginModel, urlService, analytics, advertiserModel, brandsModel) {
+    angObj.controller('ViewabilityController', function ($scope, kpiSelectModel, campaignSelectModel, strategySelectModel,
+                                                                 dataService, domainReports, constants,
+                                                                 timePeriodModel, loginModel, urlService,
+                                                                 analytics, advertiserModel, brandsModel) {
         $scope.textConstants = constants;
 
         //highlight the header menu - Dashborad, Campaigns, Reports
@@ -247,4 +256,4 @@ var angObj = angObj || {};
     });
 
 
-}());
+});
