9c0d9493e3f9bb7c12d491d1771c5bb81eb04f9e
anandp <anandp504@gmail.com>
2016-03-04 02:33:54 +0530

    Deployment copy error fix

--- scripts/crptui.sh
+++ scripts/crptui.sh
@@ -10,6 +10,7 @@ if [ $CDESK_ENV == "ewr_dev" ]
 then
   echo "Its EWR DEV environment !!!"
   #cp ${CONF_EWR_QA_FILE} ${CONF_FILE}
+  mkdir /tmp/crpt-ui-grunt/bin
   cp /tmp/crpt-ui-grunt/scripts/crpt-ui-runit.sh  /tmp/crpt-ui-grunt/bin/
   cp -r /tmp/crpt-ui-grunt/* /home/amp/crpt-ui-grunt/
   rm -Rvf /tmp/crpt-ui-grunt
