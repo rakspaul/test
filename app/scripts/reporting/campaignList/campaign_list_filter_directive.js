4e912e77c81fe44f8d1fbda5ce9fd926968c82ea
cruze300 <cruze300@gmail.com>
2016-03-15 16:48:04 -0400

    quick filter right slide

--- app/scripts/reporting/campaignList/campaign_list_filter_directive.js
+++ app/scripts/reporting/campaignList/campaign_list_filter_directive.js
@@ -12,11 +12,21 @@ define(['angularAMD','../../common/services/constants_service'],function (angula
 
             link: function ($scope, element, attrs) {
                 $scope.constants = constants;
+                quickFilters = $(".sliding_dropdown_container_quick");
                 $(".sliding_dropdown_btn , .close_sliding_dropdown ").click( function() {
-                    $('.sliding_dropdown_container').toggle('slide', { direction: "right" }, 500);
-                }) ;
+                    $('.sliding_dropdown_container_quick').toggle('slide', { direction: "right" }, 500);
+                });
+                
+                $(document).mouseup(function (e){
+                    if (quickFilters.is(':visible') == true) {
+                        var container = $(".sliding_dropdown_container_quick");
+                    
+                        if (!container.is(e.target) // if the target of the click isn't the container...
+                            && container.has(e.target).length === 0) // ... nor a descendant of the container
+                        { container.toggle('slide', { direction: "right" }, 500); }
+                    }
+                });
             }
         };
     });
-
 });
