43d478e5c3c550443cdd1c0fb4f8cb09405706c3
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2016-03-18 11:14:26 +0530

    Fix for CW-2770. Uploading new domain list is NOW SHOWN on selected grid

--- app/scripts/workflow/controllers/inventory_filters_controller.js
+++ app/scripts/workflow/controllers/inventory_filters_controller.js
@@ -99,8 +99,12 @@ define(['angularAMD', 'workflow/services/workflow_service', 'workflow/services/f
 
                 response.data.domainNamesDisplay = 'collapsed';
 
+                response.data.checked = true;
+
                 if ($scope.inventoryCreate) {
                     inventoryData.push(response.data);
+                    selectedLists.push(response.data);
+
                     _.each(selectedLists, function (val) {
                         if ($scope.workflowData.whiteListsSelected) {
                             val.domainAction = 'INCLUDE';
@@ -109,13 +113,9 @@ define(['angularAMD', 'workflow/services/workflow_service', 'workflow/services/f
                         }
                     });
 
-                    // TODO: Display the newly loaded list
-
                     // Reset the flag variable
                     $scope.inventoryCreate = false;
                 } else {
-                    response.data.checked = true;
-
                     _.each(inventoryData, function(obj, idx) {
                         if (obj.id === response.data.id) {
                             inventoryData[idx] = response.data;
@@ -128,13 +128,13 @@ define(['angularAMD', 'workflow/services/workflow_service', 'workflow/services/f
                         }
                     });
 
-                    if (response.data.domainAction === 'INCLUDE') {
-                        $scope.workflowData.selectedWhiteLists = selectedLists;
-                    } else {
-                        $scope.workflowData.selectedBlackLists = selectedLists;
-                    }
-
                     $scope.adData.inventory = response.data;
+                }
+
+                if (response.data.domainAction === 'INCLUDE') {
+                    $scope.workflowData.selectedWhiteLists = selectedLists;
+                } else {
+                    $scope.workflowData.selectedBlackLists = selectedLists;
                 }
 
                 $scope.domainUploadInProgress = false;
