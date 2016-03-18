fa66df28250c27246b24330cf2d58af4c30d6287
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2016-03-18 11:14:26 +0530

    Fix for CW-2770. Uploading new domain list is NOW SHOWN on selected grid

--- app/scripts/workflow/controllers/inventory_filters_controller.js
+++ app/scripts/workflow/controllers/inventory_filters_controller.js
@@ -118,7 +118,6 @@ define(['angularAMD', 'workflow/services/workflow_service', 'workflow/services/f
                         }
                     });
 
-                    // TODO: Display the newly loaded list
                     // Reset the flag variable
                     $scope.inventoryCreate = false;
                 } else {
