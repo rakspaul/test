8f3834786d1cb26e1bf97e6b97d6b56c72771c19
Emmanuel Cruz <cruze300@gmail.com>
2016-04-04 20:58:46 -0400

    Media Plan Create "Add Item List"

--- app/scripts/workflow/controllers/campaign_create_controller.js
+++ app/scripts/workflow/controllers/campaign_create_controller.js
@@ -1092,5 +1092,40 @@ define(['angularAMD', 'common/services/constants_service', 'workflow/services/wo
 
             }
         })
+        
+        // Search show / hide
+        $scope.searchShowInput = function () {
+            var searchInputForm = $('.searchInputForm');
+
+            $('.searchInputBtn').hide();
+            $('.searchInputBtnInline').show();
+            searchInputForm.show();
+            searchInputForm.animate({width: '400px'}, 'fast');
+            setTimeout(function () {
+                $('.searchClearInputBtn').fadeIn();
+            }, 300);
+        };
+
+        $scope.searchHideInput = function () {
+            var inputSearch = $('.searchInputForm input');
+
+            $('.searchInputBtn').show();
+            $('.searchClearInputBtn, .searchInputBtnInline').hide();
+            $('.searchInputForm').animate({width: '34px'}, 'fast');
+            setTimeout(function () {
+                $('.searchInputForm').hide();
+            }, 100);
+        };
+
+        $scope.searchClearInput = function () {
+            var inputSearch = $('.searchInputForm input');
+            inputSearch.val('');
+        };
+
+        $('body').click(function(e) {
+            if ($(e.target).closest('.searchInput').length === 0) {
+                $scope.searchHideInput();
+            }
+        });
     });
 });
