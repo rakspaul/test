5b497167b76d33b07d50a43eca1611157f74c145
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2016-04-05 16:35:05 +0530

    Fixed issue CW-3119 -- total count is now showing correctly for all different filters.

--- app/scripts/reporting/campaignList/campaign_list_model.js
+++ app/scripts/reporting/campaignList/campaign_list_model.js
@@ -127,6 +127,7 @@ define(['angularAMD','reporting/campaignList/campaign_list_service', 'common/ser
                         this.sortParam = 'start_date';
                         this.sortDirection = 'desc';
                         this.dashboard.quickFilterSelectedCount = 0;
+                        this.dashboard.quickFilterSelectedCount = 0;
                         //this.brandId = brandsModel.getSelectedBrand().id;
                         this.resetCostBreakdown.call(this);
                     };
@@ -203,7 +204,6 @@ define(['angularAMD','reporting/campaignList/campaign_list_service', 'common/ser
                         fetchCampaigns = function () {
                             var self,
                                 url;
-
                             // check scroller only inside container
                             findScrollerFromContainer.call(this);
                             if ((!this.performanceParams.lastPage && (this.dashboard.filterTotal > 0) ||
@@ -219,7 +219,6 @@ define(['angularAMD','reporting/campaignList/campaign_list_service', 'common/ser
 
                                     // The total count is now returned as part of the main result set
                                     if (data && data[0] && data[0].count) {
-                                        self.dashboard.filterTotal = data[0].count;
                                         self.dashboard.quickFilterSelectedCount = data[0].count;
                                         // This stores the original total count on first load
                                         if (!self.dashboard.originalFilterTotal) {
