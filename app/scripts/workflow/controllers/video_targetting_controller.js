9f9cbd53895a7db8bce35ed970543d201469e5fd
cruze300 <cruze300@gmail.com>
2016-04-13 12:55:29 -0400

    Video Targeting

---
+++ app/scripts/workflow/controllers/video_targetting_controller.js
@@ -0,0 +1,8 @@
+define(['angularAMD', 'workflow/services/workflow_service', 'common/services/constants_service'], function (angularAMD) {
+  angularAMD.controller('VideoTargettingController', function ($scope, audienceService, workflowService, constants) {
+
+    var _videoTargetting = this;
+
+  });
+});
+
