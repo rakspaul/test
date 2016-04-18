718c6b45bd5e59cc415e9585b375d4dbe6200569
Priya Harish <priyah@collective.com>
2016-04-18 17:06:25 +0530

    Ad Total budget is mandatory field to save ad CTR should be a % as well Impressions in left hand pane should be Camel case (right now, IMPRESSIONS) If ‘Calculate’ button is not relevant, hide the text + button. Leave the Total Ad Budget in the same position in UI Total Ad Budget – validate this against the unallocated budget in Ad Group / Campaign. No need to validate the media_cost or Impressions (budget_value) Primary KPI, KPI-Value, unit cost, total_ad budget and Media Cost (or Imps) is mandatory. don’t enable save button till all are entered The header (campaign summary) in Ad Create / Edit page needs to be updated to match new design

--- app/scripts/workflow/controllers/budget_delivery_controller.js
+++ app/scripts/workflow/controllers/budget_delivery_controller.js
@@ -51,6 +51,8 @@ define(['angularAMD', 'common/services/constants_service', 'common/moment_utils'
               $scope.adBudgetExceedUnallocated=true;
           }else{
               $scope.adBudgetExceedUnallocated=false;
+
+
           }
       }
       $scope.checkBudgetExceed=function(){
