27d7cdce0e51bd86782acea0d9b2aa2e801f5cb2
Abhimanyu <abhimanyus@collective.com>
2016-04-22 19:06:43 +0530

    fixed buying platform issues and video targeting first cut.

--- app/scripts/workflow/controllers/video_targetting_controller.js
+++ app/scripts/workflow/controllers/video_targetting_controller.js
@@ -1,10 +1,27 @@
 define(['angularAMD', 'workflow/services/workflow_service', 'common/services/constants_service','workflow/services/dimension_service'], function (angularAMD) {
-    angularAMD.controller('VideoTargettingController', function ($scope, audienceService, workflowService, constants,dimensionService) {
+    angularAMD.controller('VideoTargettingController', function ($scope, audienceService, workflowService, constants,videoService) {
 
         $scope.additionalDimension = [];
 
+        $scope.selectedDimesnion = [];
+        $scope.tagsSize = [];
+        $scope.tagsPosition = [] ;
+        $scope.tagsPlayback = [] ;
+
+
+
 
         var _videoTargetting = {
+            init : function() {
+                $scope.adData.isVideoSelected = null;
+                $scope.additionalDimension = [];
+                $scope.selectedDimesnion = [];
+                $scope.dimensionArr = [];
+                $scope.dimensionArr.push({key : 'sizes', 'value': 'Player Size', active : true});
+                $scope.dimensionArr.push({key : 'positions', 'value': 'Position', active : true});
+                $scope.dimensionArr.push({key : 'playback_methods', 'value': 'Playback Method', active : true});
+            },
+
             showBox : function() {
                 $("#videoTargeting").show().animate({ marginLeft: "0px", left: "0px", opacity: "1"}, 800 );
             },
@@ -13,97 +30,123 @@ define(['angularAMD', 'workflow/services/workflow_service', 'common/services/con
                 $("#videoTargeting").animate({ marginLeft: "0px", left: "1000px", opacity: "0"},function () {
                     $(this).hide();
                 } );
+            },
+
+            removeSelectedDimension : function() {
+                if ($scope.selectedDimesnion && $scope.selectedDimesnion.length > 0) {
+                    _.each($scope.dimensionArr, function (obj, index) {
+                        if (_.indexOf($scope.selectedDimesnion, obj.key) !== -1) {
+                            $scope.dimensionArr[index].active = false;
+                        }
+                    })
+                }
+            },
+
+            addAdditionalDimension : function(event) {
+                $scope.additionalDimension.push({
+                    key: "",
+                    name: "",
+                    value: "",
+                    hide: true
+                });
+
+
             }
         }
 
-        $scope.selectedDimesnion = [];
-
-        $scope.dimensionArr = [];
-        $scope.dimensionArr.push({key : 'player', 'value': 'Player Size', active : true});
-        $scope.dimensionArr.push({key : 'position', 'value': 'Position', active : true});
-        $scope.dimensionArr.push({key : 'playback', 'value': 'Playback Method', active : true});
-
-        var removeSelectedDimension = function() {
-            if($scope.selectedDimesnion && $scope.selectedDimesnion.length >0) {
-                _.each($scope.dimensionArr, function(obj, index) {
-                    if(_.indexOf($scope.selectedDimesnion, obj.key) !== -1) {
-                        $scope.dimensionArr[index].active = false;
-                    }
-                })
-            }
-        }
-
-        $scope.addAdditionalDimension = function(event) {
-            $scope.additionalDimension.push({
-                key: "",
-                name: "",
-                value: "",
-                hide: true
-            });
-
-            if(event) {
-                removeSelectedDimension();
-            }
-            if( $scope.additionalDimension.length == 3 ) {
-                $(".video-dimension").find(".clickable-txt").hide();
-            }
-        }
-
+        _videoTargetting.init();
 
         $scope.hideVideoTargeting = function() {
             _videoTargetting.hideBox();
+            _videoTargetting.init();
+            if($scope.mode =='edit') {
+            } else {
+                _videoTargetting.addAdditionalDimension();
+            }
         }
 
         $scope.$on('triggerVideo', function () {
             _videoTargetting.showBox();
         });
 
-        $scope.selectOption = function(event,type, dimensionStatus) {
 
-            if(!dimensionStatus) {
-                event.stopImmediatePropagation();
+
+
+        $scope.sizesLabels  = [];
+        $scope.selectOption = function(event, index, type, dimensionStatus) {
+
+            if(event && !dimensionStatus) {
+                return false;
             }
 
             var elem = $(event.target);
             elem.closest(".each-video-dimension").find(".multiselectTagOptions").hide() ;
             elem.closest(".each-video-dimension").find("#selectSizeDropdown").hide() ;
-            if( type == "player" ) {
+            if( type == "sizes" ) {
                 elem.closest(".each-video-dimension").find("#selectSizeDropdown").show();
             } else {
-                elem.closest(".each-video-dimension").find("#" + type + "InputBox").show() ;
-            }
-            if(_.indexOf($scope.selectedDimesnion, type) === -1) {
-                $scope.selectedDimesnion.push(type);
+                elem.closest(".each-video-dimension").find("#" + type + "_input_box").show() ;
             }
 
-            if( $scope.additionalDimension.length == 3 ) {
-                removeSelectedDimension();
+            if(_.indexOf($scope.selectedDimesnion, type) === -1) {
+                $scope.selectedDimesnion.push(type);
+                workflowService.getVideoTargetsType(type).then(function(results) {
+                    console.log("results", results);
+                    console.log("index", index);
+                    if(results.status === 'success' && results.data.statusCode === 200) {
+                        var data = results.data.data;
+                        switch (type) {
+                            case "sizes" :
+                                    $scope.sizesLabels[index] = _.keys(data);
+                                    videoService.setPlayerSize(data);
+                                break;
+
+                            case "positions" :
+                                    videoService.setPosition(data);
+                                break;
+
+
+                            case "playback_methods" :
+                                    videoService.setPlaybackMethods(data);
+                                break;
+                        }
+                    }
+                })
+            }
+
+            if($scope.selectedDimesnion.length < 3) {
+                _videoTargetting.addAdditionalDimension(event);
+            }
+
+            if(event && $scope.selectedDimesnion.length <= 3) { //call this function we have selected options manually.
+                _videoTargetting.removeSelectedDimension();
             }
         }
 
         $scope.selectSize = function(event,type) {
             var elem = $(event.target);
-            if(type == "specific") {
-                elem.closest(".each-video-dimension").find("#playerInputBox").show() ;
+            if(type == "Specific Size") {
+                elem.closest(".each-video-dimension").find("#sizes_input_box").show() ;
             } else {
                 elem.closest(".each-video-dimension").find(".multiselectTagOptions").hide() ;
             }
         }
 
-        $scope.tagsSize = [];
-        $scope.tagsPosition = [] ;
-        $scope.tagsPlayback = [] ;
 
         $scope.loadSize = function(query) {
-            return dimensionService.specificSizeOption(query);
+            return videoService.getPlayerSize(query);
         }
 
         $scope.loadPositions = function(query) {
-            return dimensionService.positionOption(query);
+            return videoService.getPositions(query);
         }
         $scope.loadPlayback = function(query) {
-            return dimensionService.playbackOption(query);
+            return videoService.getPlaybackMethods(query);
         }
+
+        $(function() {
+            _videoTargetting.addAdditionalDimension();
+        })
     });
 });
 
