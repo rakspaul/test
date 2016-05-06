79b7af5539e00987722f35b411294a325590a448
Sapna <sapna@techjini.com>
2016-05-06 16:31:27 +0530

    Code cleaning & optimization

--- app/scripts/reporting/controllers/platform_controller.js
+++ app/scripts/reporting/controllers/platform_controller.js
@@ -272,7 +272,7 @@ define(['angularAMD','reporting/kpiSelect/kpi_select_model', 'reporting/campaign
             extractAdFormats();
             $scope.selectedStrategy.id = selectedStrategyObj.id;
             $scope.selectedStrategy.name = selectedStrategyObj.name;
-            $scope.strategyHeading = Number($scope.selectedStrategy.id) === constants.ALL_STRATEGIES_OBJECT.id ? 'Media Plan total*:' : 'Ad Group total*:';
+            $scope.strategyHeading = Number($scope.selectedStrategy.id) === constants.ALL_STRATEGIES_OBJECT.id ? constants.MEDIA_PLAN_TOTAL : constants.AD_GROUP_TOTAL;
             $scope.isStrategyDataEmpty = false;
             $scope.resetVariables();
             $scope.strategyChangeHandler();
