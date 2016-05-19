944c8203d3e788fbf1f6e8fb931dba1ccf3a6b33
Shrujan Shetty <shrujan@Collectives-MacBook-Pro-6.local>
2016-05-19 16:47:20 +0530

    archive campaign done

--- app/scripts/workflow/controllers/campaign_create_controller.js
+++ app/scripts/workflow/controllers/campaign_create_controller.js
@@ -845,6 +845,39 @@ define(['angularAMD', 'common/services/constants_service', 'workflow/services/wo
         //    }
         //});
 
+        // archive campaign
+        $scope.cancelArchiveCampaign = function () {
+            $scope.campaignArchive = !$scope.campaignArchive;
+        };
+
+        //Archive save func more
+        $scope.archiveCampaign = function (event) {
+            var campaignId = $scope.campaignId,
+                campaignArchiveErrorHandler = function () {
+                    $scope.campaignArchive = false;
+                    $scope.campaignArchiveLoader = false;
+                    $rootScope.setErrAlertMessage();
+                };
+
+            $scope.campaignArchiveLoader = true;
+            event.preventDefault();
+
+            workflowService
+                .deleteCampaign(campaignId)
+                .then(function (result) {
+                    var campaignName;
+
+                    if (result.status === 'OK' || result.status === 'success') {
+                        $scope.campaignArchive = false;
+                        $scope.campaignArchiveLoader = false;
+                        campaignName = $scope.workflowData.campaignData.name;
+                        localStorage.setItem('topAlertMessage', campaignName + ' has been archived');
+                        $location.url(vistoconfig.MEDIA_PLANS_LINK);
+                    } else {
+                        campaignArchiveErrorHandler();
+                    }
+                }, campaignArchiveErrorHandler);
+        };
 
         $(function () {
             $(".masterContainer").on('click', '.leftNavLink', function (event) {
