61f3487921b4c632cc7c1d79f358bddb8273400c
Emmanuel Cruz <cruze300@gmail.com>
2016-06-07 14:16:55 -0400

    name

--- app/scripts/workflow/controllers/creative_list_controller.js
+++ app/scripts/workflow/controllers/creative_list_controller.js
@@ -409,6 +409,8 @@ define(['angularAMD','common/services/constants_service','workflow/services/work
         };
         
         $scope.previewCreative = function (creativeData) {
+            $scope.previewCreativeId = creativeData.id;
+            $scope.previewCreativeName = creativeData.name;
             $location.url("/creative/"+creativeData.id+"/preview");
         };
 
