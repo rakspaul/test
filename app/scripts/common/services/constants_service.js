3fba5710f46be5f72956a7787b540bb0d2ae0c13
Rupan Paul <rupanpaul99@gmail.com>
2016-06-22 15:36:42 +0530

    Showing alert message if the custom code is empty.

--- app/scripts/common/services/constants_service.js
+++ app/scripts/common/services/constants_service.js
@@ -1222,6 +1222,7 @@ define(['angularAMD'], function(angularAMD) {
         this.INVOICE_DROPDWON_UPLOAD = "The Invoice Report requires modification by Uploaded <br> System of Record (SOR) Results";
         this.INVOICE_CONFIRM_CLOSE = "Do you want to close the invoice permanently.";
         this.INVOICE_CONFIRM_UPLOAD = "Once the invoice status changed to upload, user must upload the file to change the status.";
+        this.CODE_FIELD_EMPTY = "Please add code";
 
     });
 });
