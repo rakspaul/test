d292912b0d1e734168e357c539f48b08f207b55d
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2016-07-04 15:36:58 +0530

    Sprint: 1625, ticket: CW-4103. Fixed time comparison datatype error and further code cleaning for some more controllers.

--- app/scripts/workflow/controllers/creative_preview_controller.js
+++ app/scripts/workflow/controllers/creative_preview_controller.js
@@ -1,58 +1,58 @@
-define(['angularAMD', 'workflow/services/workflow_service',
-    'login/login_model'], function (angularAMD) {
+define(['angularAMD', 'workflow/services/workflow_service', 'login/login_model'], // jshint ignore:line
+    function (angularAMD) {
     angularAMD.controller('CreativePreviewController', function ($scope, $rootScope, $routeParams, $location,
                                                          workflowService, loginModel) {
+        var creativeId = $routeParams.creativeId,
 
-        var creativeId = $routeParams.creativeId;
-        $scope.creativePreviewUrl = true
+            extractCreativeTag =  function(data) {
+                var tag = data.tag,
+                    iframe,
+                    html;
 
-        function loadScript(url, callback) {
+                if( tag) {
+                    iframe = document.createElement('iframe');
+                    html = '<body>' + tag + '</body>';
+                    iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+                    iframe.width = '1000';
+                    iframe.height = '1000';
+                    iframe.frameBorder='0';
 
-            var script = document.createElement("script")
-            script.type = "text/javascript";
+                    $('#creativePreviewContainer').append(iframe);
 
-            if (script.readyState) { //IE
+                    $.getScript($scope.creativePreviewUrl, function(res) {
+                        console.log('script loaded', res);
+                    });
+                }
+            },
+
+            buildCreativeTagPreviewContainer = function(creativeData) {
+                extractCreativeTag(creativeData);
+            };
+
+        $scope.creativePreviewUrl = true;
+
+        function loadScript(url, callback) { // jshint ignore:line
+            var script = document.createElement('script');
+
+            script.type = 'text/javascript';
+
+            if (script.readyState) {
+                //IE
                 script.onreadystatechange = function () {
-                    if (script.readyState == "loaded" || script.readyState == "complete") {
+                    if (script.readyState === 'loaded' || script.readyState === 'complete') {
                         script.onreadystatechange = null;
                         callback();
                     }
                 };
-            } else { //Others
+            } else {
+                //Others
                 script.onload = function () {
                     callback();
                 };
             }
 
             script.src = url;
-            document.getElementsByTagName("head")[0].appendChild(script);
-        }
-
-        var extractCreativeTag =  function(data) {
-            var tag = data.tag,
-                type = data.creativeType;
-                if( tag) {
-
-                    var iframe = document.createElement('iframe');
-                    var html = "<body>"+tag+"</body>";
-                    iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
-                    iframe.width = '1000';
-                    iframe.height = '1000';
-                    iframe.frameBorder="0"
-
-                    $('#creativePreviewContainer').append(iframe);
-                    console.log('iframe.contentWindow =', iframe.contentWindow);
-
-
-                    $.getScript($scope.creativePreviewUrl, function(res) {
-                        console.log("script loaded", res)
-                    })
-                }
-            //}
-        }
-
-        var buildCreativeTagPreviewContainer = function(creativeData) {
-            extractCreativeTag(creativeData);
+            document.getElementsByTagName('head')[0].appendChild(script);
         }
 
         workflowService
@@ -61,7 +61,7 @@ define(['angularAMD', 'workflow/services/workflow_service',
                 if (result.status === 'OK' || result.status === 'success') {
                     $scope.creativePreviewData = result.data.data;
                     console.log($scope.creativePreviewData);
-                    buildCreativeTagPreviewContainer($scope.creativePreviewData)
+                    buildCreativeTagPreviewContainer($scope.creativePreviewData);
                 } else {
                     console.log('No data Available to edit');
                 }
