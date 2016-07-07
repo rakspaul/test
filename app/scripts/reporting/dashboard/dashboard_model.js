c12897d67e04deb8bc2ca010ce0d4a0d7a07bc3b
Sapna <sapna@techjini.com>
2016-07-07 11:51:10 +0530

    Fixed client id - but server throwing error - inprogress

--- app/scripts/reporting/dashboard/dashboard_model.js
+++ app/scripts/reporting/dashboard/dashboard_model.js
@@ -33,19 +33,9 @@ define(['angularAMD', 'login/login_model', 'reporting/advertiser/advertiser_mode
 
         return dataService.fetch(url).then(function (response) {
             var searchCriteria = utils.typeaheadParams;
-            searchCriteria.clientId = loginModel.getSelectedClient().id;
-            searchCriteria.advertiserId = advertiserModel.getAdvertiser().hasOwnProperty('selectedAdvertiser') ? advertiserModel.getAdvertiser().selectedAdvertiser.id : -1;
+            searchCriteria.clientId = clientId;
+            searchCriteria.advertiserId = advertiserId ? advertiserId : -1;
             return brandsModel.getBrands(function() {
-                /*var ready = response.data.data.ready, draft = response.data.data.draft, paused = response.data.data.paused;
-                var totalCampaigns = response.data.data.active.total + response.data.data.completed.total + response.data.data.na.total + ready + draft + paused;
-                dashboardData.totalCampaigns = totalCampaigns;
-                var mediaPlanText = 'Media Plan' + (dashboardData.totalCampaigns > 1 ? 's' : '');
-                if (advertiserId > 0 && brandId == -1) {
-                    dashboardData.toolTip = 'Showing data for ' + dashboardData.totalCampaigns + ' ' + mediaPlanText + ' across ' + brandsModel.totalBrands() + ' brand' + (Number(brandsModel.totalBrands()) > 1 ? 's' : '');
-                } else {
-                    dashboardData.toolTip = 'Showing data for ' + dashboardData.totalCampaigns + ' ' + mediaPlanText;
-                }*/
-
                 var totalCamapigns = response.data.data.total_campaigns;
                 var mediaPlanText = 'Media Plan' + (totalCamapigns > 1 ? 's' : '');
                 dashboardData.toolTip = 'Showing data for ' + totalCamapigns + ' ' + mediaPlanText;
