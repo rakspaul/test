7f876bee54ade69c4949ef5b7cb4740210a014f1
Priya Harish <priyah@collective.com>
2016-07-25 13:52:06 +0530

    only if associated ads count is greater than0, check for same type in creative templates

--- app/scripts/workflow/creative/creative_controller.js
+++ app/scripts/workflow/creative/creative_controller.js
@@ -420,12 +420,14 @@ define(['angularAMD', '../../common/services/constants_service', 'workflow/servi
 
                         /*In creative library page, in edit mode, if ads count for a creative>0,
                          allow template change between same type. if its pushed, disable the template selection*/
-                        if($scope.creativeMode=='edit' && !$scope.adPage){
+                        if($scope.creativeMode=='edit' && !$scope.adPage && $scope.associatedAdCount > 0){
                             var tempArr=_.filter(responseData,function (obj) {
                                 return obj.templateType==$scope.creativeEditData.creativeTemplate.templateType;
                             })
+                            $scope.creativeTemplates = tempArr;
+                        }else{
+                            $scope.creativeTemplates=responseData;
                         }
-                        $scope.creativeTemplates = tempArr;
 
                     } else {
                         console.log('failed to get Channels');
