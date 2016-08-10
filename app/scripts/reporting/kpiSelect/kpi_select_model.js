85430c8361d8615f9ac20ef2bfffd1a51e9f982a
Rupan Paul <rupanpaul99@gmail.com>
2016-08-10 16:38:55 +0530

    kip drop issue.

--- app/scripts/reporting/kpiSelect/kpi_select_model.js
+++ app/scripts/reporting/kpiSelect/kpi_select_model.js
@@ -13,13 +13,28 @@ define(['angularAMD', 'common/services/vistoconfig_service'], function (angularA
         kpiData.kpiDropDown = vistoconfig.kpiDropDown;
         kpiData.newkpiDropDownAlt = vistoconfig.newkpiDropDownAlt;
 
+        var getKpiVale = function(_kpi){
+            _kpi = _kpi.toLowerCase();
+            if(_kpi === 'post click cpa'){
+                _kpi = 'pc_cpa';
+            }else if(_kpi === 'suspicious activity rate'){
+                _kpi = 'suspicious_impressions_perc';
+            }else if(_kpi === 'viewable impressions'){
+                _kpi = 'suspicious_impressions_perc';
+            }else if(_kpi === 'viewable rate'){
+                _kpi = 'viewable_impressions_perc';
+            }
+            return _kpi;
+        }
         kpiData.setSelectedKpi = function (_kpi) {
+            _kpi = getKpiVale(_kpi);
             _kpi = _kpi.replace(/ /g, '_');
             localStorage.setItem('selectedKpi', _kpi);
             kpiData.selectedKpi = _kpi;
         };
 
         kpiData.setSelectedKpiAlt = function (_kpi) {
+            _kpi = getKpiVale(_kpi);
             _kpi = _kpi.replace(/ /g, '_');
             localStorage.setItem('selectedKpiAlt', _kpi);
             kpiData.selectedKpiAlt = _kpi;
