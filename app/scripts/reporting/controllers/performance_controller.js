e295b7d2235a5f8f6f4b504af7daade6a9e14bc0
Venkat G <venkateswararao.g@gmail.com>
2016-08-25 12:53:46 +0530

    Changed the text for discrepancy report #CRPT-5774

--- app/scripts/reporting/controllers/performance_controller.js
+++ app/scripts/reporting/controllers/performance_controller.js
@@ -286,6 +286,7 @@ define(['angularAMD','reporting/kpiSelect/kpi_select_model', 'reporting/campaign
                                 _.each($scope.groupThem, function(item) {
                                     item.perf_metrics = _customCtrl.removeDuplicateSOR(item.perf_metrics);
                                 });
+                                $scope.lastSyncedOn = _.first($scope['strategyPerfDataBy' + tab]).sync_date;
 
                                 _.each($scope['strategyPerfDataBy' + tab], function (item) {
                                     var vendorName = (item.nodes.length === 1) ? item.nodes[0].name : item.category;
