74aa905e4b8a7b481aac2809e35a7af62b8803ff
Saudamini M <saudamini@techjini.com>
2016-09-01 15:03:58 +0530

    Performance screen styling done

--- app/scripts/reporting/controllers/performance_controller.js
+++ app/scripts/reporting/controllers/performance_controller.js
@@ -260,6 +260,9 @@ define(['angularAMD','kpi-select-model', 'campaign-select-model',
                                         if(item.dimension === 'Line Item Totals') {
                                             item.sepratorCls = 'sepratorCls';
                                         }
+                                        if(item.dimension === 'Ad Item Totals') {
+                                            item.sepratorCls = 'sepratorLineCls';
+                                        }
                                         return item.ad_id !== -1 && item.ad_group_id !== -1;
                                     });
 
