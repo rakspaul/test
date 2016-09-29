b0879bbd3de5fc8da40620a4821f77e562ba1408
Vishal Verma <vishalv@collective.com>
2016-09-29 15:17:55 +0530

    fix regex

--- app/scripts/workflow/services/audience_service.js
+++ app/scripts/workflow/services/audience_service.js
@@ -59,7 +59,7 @@ define(['angularAMD'],
                         url += '&query=';
 
                         for (i = 0; i < keywords.length; i++) {
-                            if(/^[a-zA-Z0-9- ]*/.test(keywords[i]) === false) {
+                            if(/^[a-zA-Z0-9- ]*$/.test(keywords[i]) === false) {
                                 url += encodeURIComponent(keywords[i]);
                             } else {
                                 url += keywords[i];
